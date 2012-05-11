621fb14d3072c30e80b97d94136fd5a0bbb0af0d
Thomas Quintana <quintana.thomas@gmail.com>
2012-05-11 13:20:06 -0400

    append to last commit.

---
+++ src/main/java/org/mobicents/servlet/sip/restcomm/interpreter/tagstrategy/FaxTagStrategy.java
@@ -0,0 +1,37 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.mobicents.servlet.sip.restcomm.interpreter.tagstrategy;
+
+import org.mobicents.servlet.sip.restcomm.annotations.concurrency.NotThreadSafe;
+import org.mobicents.servlet.sip.restcomm.interpreter.RcmlInterpreter;
+import org.mobicents.servlet.sip.restcomm.interpreter.RcmlInterpreterContext;
+import org.mobicents.servlet.sip.restcomm.interpreter.TagStrategyException;
+import org.mobicents.servlet.sip.restcomm.xml.rcml.RcmlTag;
+
+/**
+ * @author quintana.thomas@gmail.com (Thomas Quintana)
+ */
+@NotThreadSafe public final class FaxTagStrategy extends RcmlTagStrategy {
+  public FaxTagStrategy() {
+    super();
+  }
+  
+  @Override public void execute(final RcmlInterpreter interpreter, final RcmlInterpreterContext context,
+      final RcmlTag tag) throws TagStrategyException {
+    
+  }
+}
