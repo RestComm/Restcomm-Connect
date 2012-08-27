29e8832ae440891f9cf7463700a652ff0c7efd63
Thomas Quintana <quintana.thomas@gmail.com>
2012-08-27 12:33:24 -0400

    Fixed issue where the SQL initialization script declares a field NOT NULL that should be allowed to be NULL.

--- restcomm.core/src/main/webapp/WEB-INF/scripts/sql/init.sql
+++ restcomm.core/src/main/webapp/WEB-INF/scripts/sql/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE "restcomm_accounts" (
 "date_updated" DATE NOT NULL,
 "email_address" LONGVARCHAR NOT NULL,
 "friendly_name" VARCHAR(64) NOT NULL,
-"account_sid" VARCHAR(34) NOT NULL,
+"account_sid" VARCHAR(34),
 "type" VARCHAR(8) NOT NULL,
 "status" VARCHAR(16) NOT NULL,
 "auth_token" VARCHAR(32) NOT NULL,
