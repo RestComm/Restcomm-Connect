10e37974b34319af103bc35e15005faed3b23a14
gvagenas <gvagenas@gmail.com>
2012-11-17 19:53:21 +0200

    Fixes for tests

---
+++ restcomm.testsuite/MediaTests/src/test/java/org/mobicents/servlet/sip/restcomm/Testsuite.java
@@ -0,0 +1,17 @@
+package org.mobicents.servlet.sip.restcomm;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author <a href="mailto:gvagenas@gmail.com">George Vagenas</a>
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+	MediaTest.class,
+	OutgoingCallTest.class
+})
+public class Testsuite {
+
+}
