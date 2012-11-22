4425a9d639dffa44a95c944662e2957db4c620b7
Thomas Quintana <thomas@localhost.localdomain>
2012-11-22 01:11:03 -0500

    Removed second primary key from gateways table.

--- restcomm.core/src/main/java/org/mobicents/servlet/sip/restcomm/http/GatewaysEndpoint.java
+++ restcomm.core/src/main/java/org/mobicents/servlet/sip/restcomm/http/GatewaysEndpoint.java
@@ -1,12 +1,18 @@
 package org.mobicents.servlet.sip.restcomm.http;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 
 import org.mobicents.servlet.sip.restcomm.ServiceLocator;
 import org.mobicents.servlet.sip.restcomm.annotations.concurrency.ThreadSafe;
 import org.mobicents.servlet.sip.restcomm.dao.DaoManager;
 import org.mobicents.servlet.sip.restcomm.dao.GatewaysDao;
+import org.mobicents.servlet.sip.restcomm.entities.Gateway;
+import org.mobicents.servlet.sip.restcomm.entities.RestCommResponse;
+import org.mobicents.servlet.sip.restcomm.http.converter.GatewayConverter;
+import org.mobicents.servlet.sip.restcomm.http.converter.GatewayListConverter;
+import org.mobicents.servlet.sip.restcomm.http.converter.RestCommResponseConverter;
 
 @ThreadSafe public final class GatewaysEndpoint extends AbstractEndpoint {
   private final GatewaysDao dao;
@@ -17,7 +23,15 @@ import org.mobicents.servlet.sip.restcomm.dao.GatewaysDao;
     super();
     final ServiceLocator services = ServiceLocator.getInstance();
     dao = services.get(DaoManager.class).getGatewaysDao();
-    gson = null;
-    xstream = null;
+    final GatewayConverter converter = new GatewayConverter(configuration);
+    final GsonBuilder builder = new GsonBuilder();
+    builder.registerTypeAdapter(Gateway.class, converter);
+    builder.setPrettyPrinting();
+    gson = builder.create();
+    xstream = new XStream();
+    xstream.alias("RestcommResponse", RestCommResponse.class);
+    xstream.registerConverter(converter);
+    xstream.registerConverter(new GatewayListConverter(configuration));
+    xstream.registerConverter(new RestCommResponseConverter(configuration));
   }
 }
