f570e9466804eb23cbef444fd58449dba0419b37
George <gvagenas@gmail.com>
2013-02-08 13:15:16 +0200

    Update issue 156

--- restcomm.commons/src/main/java/org/mobicents/servlet/sip/restcomm/util/HttpUtils.java
+++ restcomm.commons/src/main/java/org/mobicents/servlet/sip/restcomm/util/HttpUtils.java
@@ -21,38 +21,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
-
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.mobicents.servlet.sip.restcomm.annotations.concurrency.ThreadSafe;
 
 /**
  * @author quintana.thomas@gmail.com (Thomas Quintana)
  */
 @ThreadSafe public final class HttpUtils {
-  private HttpUtils() {
-    super();
-  }
-  
-  public static Map<String, String> toMap(final HttpEntity entity)
-      throws IllegalStateException, IOException {
-    final List<NameValuePair> parameters = URLEncodedUtils.parse(entity);
-    final Map<String, String> map = new HashMap<String, String>();
-    for(final NameValuePair parameter : parameters) {
-      map.put(parameter.getName(), parameter.getValue());
-    }
-    return map;
-  }
-  
-  public static String toString(final Header[] headers) {
-    final List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-    for(final Header header : headers) {
-      parameters.add(new BasicNameValuePair(header.getName(), header.getValue()));
-    }
-    return URLEncodedUtils.format(parameters, "UTF-8");
-  }
+	private HttpUtils() {
+		super();
+	}
+
+	public static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
+
+	public static Map<String, String> toMap(final HttpEntity entity)
+			throws IllegalStateException, IOException {
+
+		String contentType = null;
+		String charset = null;
+
+		contentType = EntityUtils.getContentMimeType(entity);
+		charset = EntityUtils.getContentCharSet(entity);
+
+		List<NameValuePair> parameters = null;
+		if (contentType != null && contentType.equalsIgnoreCase(CONTENT_TYPE)) {
+			parameters = URLEncodedUtils.parse(entity);
+		} else {
+			final String content = EntityUtils.toString(entity, HTTP.ASCII);
+			if (content != null && content.length() > 0) {
+				parameters = new ArrayList <NameValuePair>();
+				URLEncodedUtils.parse(parameters, new Scanner(content), charset);
+			}  
+		}
+
+		final Map<String, String> map = new HashMap<String, String>();
+		for(final NameValuePair parameter : parameters) {
+			map.put(parameter.getName(), parameter.getValue());
+		}
+		return map;
+	}
+
+	public static String toString(final Header[] headers) {
+		final List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+		for(final Header header : headers) {
+			parameters.add(new BasicNameValuePair(header.getName(), header.getValue()));
+		}
+		return URLEncodedUtils.format(parameters, "UTF-8");
+	}
 }
