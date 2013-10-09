b3f8e0abebda12ee8b146f620c4856ec31ff807f
George Vagenas <gvagenas@gmail.com>
2013-10-09 21:07:25 +0300

    fixes issue #127

---
+++ restcomm/restcomm.http/src/test/java/org/mobicents/servlet/restcomm/http/AccountsEndpointTest.java
@@ -0,0 +1,153 @@
+package org.mobicents.servlet.restcomm.http;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.jboss.arquillian.container.test.api.Deployer;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.archive.ShrinkWrapMaven;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.gson.JsonObject;
+
+/**
+ * @author <a href="mailto:gvagenas@gmail.com">gvagenas</a>
+ */
+
+@RunWith(Arquillian.class)
+public class AccountsEndpointTest{
+	
+	private static final String version = "6.1.2-TelScale-SNAPSHOT";
+
+	@ArquillianResource
+	private Deployer deployer;
+	@ArquillianResource
+	URL deploymentUrl;
+
+	private String adminUsername = "administrator@company.com";
+	private String adminAccountSid = "ACae6e420f425248d6a26948c17a9e2acf";
+	private String adminAuthToken = "77f8c12cc7b8f8423e5c38b035249166";
+	private String userEmailAddress = "gvagenas@restcomm.org";
+	private String userPassword = "1234";
+
+	@Test
+	public void testCreateAccount(){
+		JsonObject createAccountResponse = RestcommAccountsTool.getInstance().createAccount(deploymentUrl.toString(), adminUsername, adminAuthToken, userEmailAddress, userPassword);
+		JsonObject getAccountResponse = RestcommAccountsTool.getInstance().getAccount(deploymentUrl.toString(), adminUsername , adminAuthToken, userEmailAddress);
+
+		String usernameHashed = "AC"+(new Md5Hash(userEmailAddress).toString());
+		assertTrue(createAccountResponse.get("sid").getAsString().equals(usernameHashed));
+		
+		assertTrue(createAccountResponse.get("auth_token").equals(getAccountResponse.get("auth_token")));
+		String userPasswordHashed = new Md5Hash(userPassword).toString();
+		assertTrue(getAccountResponse.get("auth_token").getAsString().equals(userPasswordHashed));
+	}
+	
+	@Test
+	public void testGetAccounts(){
+		//Create account
+		RestcommAccountsTool.getInstance().createAccount(deploymentUrl.toString(), adminUsername, adminAuthToken, userEmailAddress, userPassword);
+		//Get Account using admin email address and user email address
+		JsonObject account1 = RestcommAccountsTool.getInstance().getAccount(deploymentUrl.toString(), adminUsername , adminAuthToken, userEmailAddress);
+		//Get Account using admin account sid and user sid
+		JsonObject account2 = RestcommAccountsTool.getInstance().getAccount(deploymentUrl.toString(), adminAccountSid , adminAuthToken, account1.get("sid").getAsString());
+
+		assertTrue(account1.toString().equals(account2.toString()));
+		
+	}
+
+
+	@Deployment(name="ClientsEndpointTest", managed=true, testable=false)
+	public static WebArchive createWebArchiveNoGw() {
+		final WebArchive archive = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.application:war:" + version)
+				.withoutTransitivity().asSingle(WebArchive.class);
+		JavaArchive dependency = ShrinkWrapMaven.resolver()
+				.resolve("commons-configuration:commons-configuration:jar:1.7")
+				.offline().withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("jain:jain-mgcp-ri:jar:1.0")
+				.offline().withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("org.mobicents.media.client:mgcp-driver:jar:3.0.0.Final")
+				.offline().withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("joda-time:joda-time:jar:2.0")
+				.offline().withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.iSpeech:iSpeech:jar:1.0.1")
+				.offline().withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.commons:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.dao:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.asr:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.fax:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.tts.acapela:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.tts.api:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.mgcp:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.http:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.interpreter:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.sms.api:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.sms:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.telephony.api:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		dependency = ShrinkWrapMaven.resolver()
+				.resolve("com.telestax.servlet:restcomm.telephony:jar:" + version)
+				.withoutTransitivity().asSingle(JavaArchive.class);
+		archive.addAsLibrary(dependency);
+		archive.delete("/WEB-INF/sip.xml");
+		archive.delete("/WEB-INF/conf/restcomm.xml");
+		archive.delete("/WEB-INF/data/hsql/restcomm.script");
+		archive.addAsWebInfResource("sip.xml");
+		archive.addAsWebInfResource("restcomm.xml", "conf/restcomm.xml");
+		archive.addAsWebInfResource("restcomm.script", "data/hsql/restcomm.script");
+		return archive;
+	}
+
+}
