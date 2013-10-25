253181c6697732db26b0a6d66e5c543d1a3e310c
Jean Deruelle <jean.deruelle@telestax.com>
2013-10-25 18:19:43 +0200

    ref issue #132 Adding non regression test for parsing of SIP Noun

--- restcomm/restcomm.interpreter/src/test/java/org/mobicents/servlet/restcomm/interpreter/rcml/ParserTest.java
+++ restcomm/restcomm.interpreter/src/test/java/org/mobicents/servlet/restcomm/interpreter/rcml/ParserTest.java
@@ -15,6 +15,7 @@ package org.mobicents.servlet.restcomm.interpreter.rcml;
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
@@ -26,7 +27,9 @@ import java.io.InputStream;
 import java.util.List;
 
 import org.junit.AfterClass;
+
 import static org.junit.Assert.*;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -36,70 +39,108 @@ import static org.mobicents.servlet.restcomm.interpreter.rcml.Verbs.*;
  * @author quintana.thomas@gmail.com (Thomas Quintana)
  */
 public final class ParserTest {
-  private static ActorSystem system;
-  
-  public ParserTest() {
-    super();
-  }
-  
-  @BeforeClass public static void before() throws Exception {
-    system = ActorSystem.create();
-  }
+    private static ActorSystem system;
 
-  @AfterClass public static void after() throws Exception {
-    system.shutdown();
-  }
-  
-  private ActorRef parser(final InputStream input) {
-    return system.actorOf(new Props(new UntypedActorFactory() {
-		private static final long serialVersionUID = 1L;
-		@Override public UntypedActor create() throws Exception {
-          return new Parser(input);
-		}
-    }));
-  }
+    public ParserTest() {
+        super();
+    }
 
-  @Test public void testParser() {
-    final InputStream input = getClass().getResourceAsStream("/rcml.xml");
-    new JavaTestKit(system) {{
-        final ActorRef observer = getRef();
-    	// Create a new parser.
-        final GetNextVerb next = GetNextVerb.instance();
-        final ActorRef parser = parser(input);
-        // Start consuming verbs until the end of the document.
-        parser.tell(next, observer);
-        Tag verb = expectMsgClass(Tag.class);
-        assertTrue(record.equals(verb.name()));
-        assertTrue(verb.attribute("action").value().equals("https://127.0.0.1:8080/restcomm/demos/hello-world.jsp"));
-        assertTrue(verb.attribute("method").value().equals("GET"));
-        assertTrue(verb.attribute("maxLength").value().equals("60"));
-        assertTrue(verb.attribute("timeout").value().equals("5"));
-        assertTrue(verb.attribute("finishOnKey").value().equals("#"));
-        assertTrue(verb.attribute("transcribe").value().equals("true"));
-        assertTrue(verb.attribute("transcribeCallback").value().equals("transcribe.jsp"));
-        assertTrue(verb.attribute("playBeep").value().equals("false"));
-        parser.tell(next, observer);
-        verb = expectMsgClass(Tag.class);
-        assertTrue(gather.equals(verb.name()));
-        assertTrue(verb.attribute("timeout").value().equals("30"));
-        assertTrue(verb.attribute("finishOnKey").value().equals("#"));
-        assertTrue(verb.hasChildren());
-        final List<Tag> children = verb.children();
-        Tag child = children.get(0);
-        assertTrue(say.equals(child.name()));
-        assertTrue(child.attribute("voice").value().equals("man"));
-        assertTrue(child.attribute("language").value().equals("en"));
-        assertTrue(child.attribute("loop").value().equals("1"));
-        assertTrue(child.text().equals("Hello World!"));
-        child = children.get(1);
-        assertTrue(play.equals(child.name()));
-        assertTrue(child.attribute("loop").value().equals("1"));
-        assertTrue(child.text().equals("https://127.0.0.1:8080/restcomm/audio/hello-world.wav"));
-        child = children.get(2);
-        assertTrue(pause.equals(child.name()));
-        assertTrue(child.attribute("length").value().equals("1"));
-        parser.tell(next, observer);
-        expectMsgClass(End.class);
-    }};
-  }
+    @BeforeClass
+    public static void before() throws Exception {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void after() throws Exception {
+        system.shutdown();
+    }
+
+    private ActorRef parser(final InputStream input) {
+        return system.actorOf(new Props(new UntypedActorFactory() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public UntypedActor create() throws Exception {
+                return new Parser(input);
+            }
+        }));
+    }
+
+    @Test
+    public void testParser() {
+        final InputStream input = getClass().getResourceAsStream("/rcml.xml");
+        new JavaTestKit(system) {{
+            final ActorRef observer = getRef();
+            // Create a new parser.
+            final GetNextVerb next = GetNextVerb.instance();
+            final ActorRef parser = parser(input);
+            // Start consuming verbs until the end of the document.
+            parser.tell(next, observer);
+            Tag verb = expectMsgClass(Tag.class);
+            assertTrue(record.equals(verb.name()));
+            assertTrue(verb.attribute("action").value().equals("https://127.0.0.1:8080/restcomm/demos/hello-world.jsp"));
+            assertTrue(verb.attribute("method").value().equals("GET"));
+            assertTrue(verb.attribute("maxLength").value().equals("60"));
+            assertTrue(verb.attribute("timeout").value().equals("5"));
+            assertTrue(verb.attribute("finishOnKey").value().equals("#"));
+            assertTrue(verb.attribute("transcribe").value().equals("true"));
+            assertTrue(verb.attribute("transcribeCallback").value().equals("transcribe.jsp"));
+            assertTrue(verb.attribute("playBeep").value().equals("false"));
+            parser.tell(next, observer);
+            verb = expectMsgClass(Tag.class);
+            assertTrue(gather.equals(verb.name()));
+            assertTrue(verb.attribute("timeout").value().equals("30"));
+            assertTrue(verb.attribute("finishOnKey").value().equals("#"));
+            assertTrue(verb.hasChildren());
+            final List<Tag> children = verb.children();
+            Tag child = children.get(0);
+            assertTrue(say.equals(child.name()));
+            assertTrue(child.attribute("voice").value().equals("man"));
+            assertTrue(child.attribute("language").value().equals("en"));
+            assertTrue(child.attribute("loop").value().equals("1"));
+            assertTrue(child.text().equals("Hello World!"));
+            child = children.get(1);
+            assertTrue(play.equals(child.name()));
+            assertTrue(child.attribute("loop").value().equals("1"));
+            assertTrue(child.text().equals("https://127.0.0.1:8080/restcomm/audio/hello-world.wav"));
+            child = children.get(2);
+            assertTrue(pause.equals(child.name()));
+            assertTrue(child.attribute("length").value().equals("1"));
+            parser.tell(next, observer);
+            expectMsgClass(End.class);
+        }};
+    }
+
+    @Test
+    // Test for SIP Noun Parsing for Issue
+    // https://bitbucket.org/telestax/telscale-restcomm/issue/132/implement-twilio-sip-out
+    public void testParserDialSIP() {
+        final InputStream input = getClass().getResourceAsStream("/rcml-sip.xml");
+        new JavaTestKit(system) {{
+            final ActorRef observer = getRef();
+            // Create a new parser.
+            final GetNextVerb next = GetNextVerb.instance();
+            final ActorRef parser = parser(input);
+            // Start consuming verbs until the end of the document.
+            parser.tell(next, observer);
+            Tag verb = expectMsgClass(Tag.class);
+            assertTrue(dial.equals(verb.name()));
+            assertTrue(verb.attribute("record").value().equals("true"));
+            assertTrue(verb.attribute("timeout").value().equals("10"));
+            assertTrue(verb.attribute("hangupOnStar").value().equals("true"));
+            assertTrue(verb.attribute("callerId").value().equals("bob"));
+            assertTrue(verb.attribute("method").value().equals("POST"));
+            assertTrue(verb.attribute("action").value().equals("/handle_post_dial"));
+            final List<Tag> children = verb.children();
+            Tag child = children.get(0);
+            assertTrue(Nouns.SIP.equals(child.name()));
+            assertTrue(child.attribute("username").value().equals("admin"));
+            assertTrue(child.attribute("password").value().equals("1234"));
+            assertTrue(child.attribute("method").value().equals("POST"));
+            assertTrue(child.attribute("url").value().equals("/handle_screening_on_answer"));
+            assertTrue(child.text().equals("sip:kate@example.com?customheader=foo"));
+            parser.tell(next, observer);
+            expectMsgClass(End.class);
+        }};
+    }
 }
