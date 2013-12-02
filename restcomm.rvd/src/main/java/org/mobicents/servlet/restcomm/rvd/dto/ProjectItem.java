a7d00e309e68a5877f55ee367adc8834f07c92ee
Orestis Tsakiridis <otsakir@gmail.com>
2013-12-02 21:08:38 +0200

    Added a fixed 'list projects' service returning ProjectItems

---
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/dto/ProjectItem.java
@@ -0,0 +1,22 @@
+package org.mobicents.servlet.restcomm.rvd.dto;
+
+
+public class ProjectItem {
+	
+	private String name;
+	private String startupUrl;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStartupUrl() {
+		return startupUrl;
+	}
+	public void setStartupUrl(String startupUrl) {
+		this.startupUrl = startupUrl;
+	}
+}
