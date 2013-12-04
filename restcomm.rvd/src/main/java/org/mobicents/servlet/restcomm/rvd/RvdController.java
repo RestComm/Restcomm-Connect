b849d0bd08c4cbd7f1cd7b63e4798d9f5f442714
Orestis Tsakiridis <otsakir@gmail.com>
2013-12-04 23:07:56 +0200

    Added basic interpreter functionality. Added xstream library too

--- restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdController.java
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdController.java
@@ -1,11 +1,83 @@
 package org.mobicents.servlet.restcomm.rvd;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.io.FileUtils;
+import org.mobicents.servlet.restcomm.rvd.dto.ProjectState;
+import org.mobicents.servlet.restcomm.rvd.interpreter.Target;
+import org.mobicents.servlet.restcomm.rvd.model.RcmlResponse;
+import org.mobicents.servlet.restcomm.rvd.model.RcmlStep;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+
+@Path("/apps/{appname}/controller")
 public class RvdController {
 
-	public RvdController() {
-		// TODO Auto-generated constructor stub
+	// configuration parameters 
+	//private static final String workspaceDirectoryName = "workspace";
+	//private static final String protoDirectoryName = "_proto"; // the prototype project directory name
+	
+	private String workspaceBasePath;
+	
+	@Context ServletContext servletContext;
+	private ProjectService projectService; // use a proper way to initialize this in init()
+	Gson gson;
+	
+	@PostConstruct
+	void init() {
+		//workspaceBasePath = servletContext.getRealPath(File.separator) + workspaceDirectoryName;
+		gson = new Gson();
+		projectService = new ProjectService(servletContext);
 	}
 	
+	@GET @Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listProjects(@PathParam("appname") String appname) {
+		Gson gson  = new Gson();
+		return Response.ok(gson.toJson("asdf " + appname)).build();
+	}
 	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response controller( @PathParam("appname") String appname, @QueryParam("target") String targetParam ) {
+
+		if ( !projectService.projectExists(appname) )
+			return Response.status(Status.NOT_FOUND).build();
+		
+		String projectBasePath = projectService.getWorkspaceBasePath() + File.separator + appname;
+		System.out.println( "Working on project path " + projectBasePath);
+		
+		Interpreter interpreter = new Interpreter();
+		
+		String rcmlResponse = interpreter.interpret(targetParam, projectBasePath);
+		//interpreter.interpret()
+		
+		System.out.println("result: " + rcmlResponse);
+		
+			
+	//	System.out.println( "from service. Workspacebasepath: " + projectService.getWorkspaceBasePath() );
+		
+		return Response.ok("Running application " + appname + ". workspaceBasePath " + workspaceBasePath).build();
+	}
 	
+		
 }
--- restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/model/RcmlGatherStep.java
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/model/RcmlGatherStep.java
@@ -1,5 +1,16 @@
 package org.mobicents.servlet.restcomm.rvd.model;
 
 public class RcmlGatherStep extends RcmlStep {
+	private String action;
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+	
+	
 
 }
