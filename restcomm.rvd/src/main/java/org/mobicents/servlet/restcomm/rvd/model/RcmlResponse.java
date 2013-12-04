f8fa9e03cd3cb90b38d71a66aa1ec52328d2389f
Orestis Tsakiridis <otsakir@gmail.com>
2013-12-04 21:45:04 +0200

    Added model classes for RCML generation and a new package to host interpret (json project->RCML) functionality

---
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/model/RcmlResponse.java
@@ -0,0 +1,9 @@
+package org.mobicents.servlet.restcomm.rvd.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RcmlResponse {
+	public List<RcmlStep> steps = new ArrayList<RcmlStep>();	
+}
---
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/model/RcmlSayStep.java
@@ -0,0 +1,13 @@
+package org.mobicents.servlet.restcomm.rvd.model;
+
+public class RcmlSayStep extends RcmlStep {
+	private String phrase;
+
+	public String getPhrase() {
+		return phrase;
+	}
+
+	public void setPhrase(String phrase) {
+		this.phrase = phrase;
+	}
+}
