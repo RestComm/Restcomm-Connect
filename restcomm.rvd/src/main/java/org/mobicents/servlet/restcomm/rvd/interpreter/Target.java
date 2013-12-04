4eaed2e3e45ce0f1faac88ad0da6deba957e938b
Orestis Tsakiridis <otsakir@gmail.com>
2013-12-04 21:45:04 +0200

    Added model classes for RCML generation and a new package to host interpret (json project->RCML) functionality

---
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/interpreter/Target.java
@@ -0,0 +1,27 @@
+package org.mobicents.servlet.restcomm.rvd.interpreter;
+
+public class Target {
+	String nodename;
+	String stepname;
+	String action;
+	public String getNodename() {
+		return nodename;
+	}
+	public void setNodename(String nodename) {
+		this.nodename = nodename;
+	}
+	public String getStepname() {
+		return stepname;
+	}
+	public void setStepname(String stepname) {
+		this.stepname = stepname;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	
+	
+}
