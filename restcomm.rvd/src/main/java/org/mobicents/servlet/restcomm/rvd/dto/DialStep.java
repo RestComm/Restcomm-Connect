c37826b03a04176af49115e94fb94b530c6409f4
Orestis Tsakiridis <otsakir@gmail.com>
2013-12-05 23:05:08 +0200

    Added support for <Dial/> verb

--- restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/dto/DialStep.java
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/dto/DialStep.java
@@ -1,7 +1,78 @@
 package org.mobicents.servlet.restcomm.rvd.dto;
 
 public class DialStep extends Step {
+	private String dialType;
+	private String number;
+	private String client;
+	private String conference;
+	private String sipuri;
+	
+	private String action;
+	private String method;
+	private Integer timeout;
+	private Integer timeLimit;
+	private String callerId;
 	
 	
-
+	public String getDialType() {
+		return dialType;
+	}
+	public void setDialType(String dialType) {
+		this.dialType = dialType;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getClient() {
+		return client;
+	}
+	public void setClient(String client) {
+		this.client = client;
+	}
+	public String getConference() {
+		return conference;
+	}
+	public void setConference(String conference) {
+		this.conference = conference;
+	}
+	public String getSipuri() {
+		return sipuri;
+	}
+	public void setSipuri(String sipuri) {
+		this.sipuri = sipuri;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getMethod() {
+		return method;
+	}
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	public Integer getTimeout() {
+		return timeout;
+	}
+	public void setTimeout(Integer timeout) {
+		this.timeout = timeout;
+	}
+	public Integer getTimeLimit() {
+		return timeLimit;
+	}
+	public void setTimeLimit(Integer timeLimit) {
+		this.timeLimit = timeLimit;
+	}
+	public String getCallerId() {
+		return callerId;
+	}
+	public void setCallerId(String callerId) {
+		this.callerId = callerId;
+	}
 }
+
---
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/model/RcmlDialStep.java
@@ -0,0 +1,34 @@
+package org.mobicents.servlet.restcomm.rvd.model;
+
+public class RcmlDialStep extends RcmlStep {
+	private String number;
+	private String client;
+	private String conference;
+	private String sipuri;
+	
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getClient() {
+		return client;
+	}
+	public void setClient(String client) {
+		this.client = client;
+	}
+	public String getConference() {
+		return conference;
+	}
+	public void setConference(String conference) {
+		this.conference = conference;
+	}
+	public String getSipuri() {
+		return sipuri;
+	}
+	public void setSipuri(String sipuri) {
+		this.sipuri = sipuri;
+	}
+}
