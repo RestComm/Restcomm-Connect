30816fc5048eee17fe1a94c498e5aa4f7adc07a1
Orestis Tsakiridis <otsakir@gmail.com>
2013-12-08 17:43:52 +0200

    Added default target (startup module) project setting when target is not defined in URL. Allows for launching an application without specifying a target in the URL

--- restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdController.java
+++ restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdController.java
@@ -2,11 +2,13 @@ package org.mobicents.servlet.restcomm.rvd;
 
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
@@ -17,7 +19,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.mobicents.servlet.restcomm.rvd.exceptions.BadWorkspaceDirectoryStructure;
+import org.mobicents.servlet.restcomm.rvd.exceptions.UndefinedTarget;
 import org.mobicents.servlet.restcomm.rvd.interpreter.Interpreter;
+import org.mobicents.servlet.restcomm.rvd.interpreter.exceptions.RVDUnsupportedHandlerVerb;
 
 import com.google.gson.Gson;
 
@@ -45,7 +49,44 @@ public class RvdController {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public Response controller( @PathParam("appname") String appname, @QueryParam("target") String targetParam,  @Context HttpServletRequest httpRequest ) {
+	public Response controllerGet( @PathParam("appname") String appname, @QueryParam("target") String targetParam,  @Context HttpServletRequest httpRequest ) {
+		
+		try {
+			if ( !projectService.projectExists(appname) )
+				return Response.status(Status.NOT_FOUND).build();
+		} catch (BadWorkspaceDirectoryStructure e) {
+			e.printStackTrace(); // TODO Auto-generated catch block
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+		
+		String projectBasePath = projectService.getWorkspaceBasePath() + File.separator + appname;		
+		Interpreter interpreter = new Interpreter();
+		
+		String rcmlResponse;
+		try {
+			rcmlResponse = interpreter.interpret(targetParam, projectBasePath, appname, httpRequest);
+		} catch (RVDUnsupportedHandlerVerb e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		} catch (UndefinedTarget e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(); 
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+		
+		System.out.println(rcmlResponse);		
+		return Response.ok(rcmlResponse, MediaType.APPLICATION_XML).build();
+	}
+	
+	/*
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public Response controllerPost( @PathParam("appname") String appname, @QueryParam("target") String targetParam,  @Context HttpServletRequest httpRequest ) {
 		
 		try {
 			if ( !projectService.projectExists(appname) )
@@ -62,6 +103,7 @@ public class RvdController {
 		System.out.println(rcmlResponse);		
 		return Response.ok(rcmlResponse, MediaType.APPLICATION_XML).build();
 	}
+	*/
 	
 		
 }
