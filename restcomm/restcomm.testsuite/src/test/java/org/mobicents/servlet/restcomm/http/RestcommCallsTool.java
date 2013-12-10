b942b8784d1dceb37f926ac62474074b5a3779e0
George Vagenas <gvagenas@gmail.com>
2013-12-10 21:28:08 +0200

    fix issue #205 fix issue #234

--- restcomm/restcomm.testsuite/src/test/java/org/mobicents/servlet/restcomm/http/RestcommCallsTool.java
+++ restcomm/restcomm.testsuite/src/test/java/org/mobicents/servlet/restcomm/http/RestcommCallsTool.java
@@ -92,6 +92,30 @@ public class RestcommCallsTool {
         }
 
     }
+    
+    public JsonObject getCall(String deploymentUrl, String username, String authToken, String sid){
+        
+        Client jerseyClient = Client.create();
+        jerseyClient.addFilter(new HTTPBasicAuthFilter(username, authToken));
+
+        String url = getAccountsUrl(deploymentUrl, username, true);
+
+        WebResource webResource = jerseyClient.resource(url);
+
+        String response = null;
+
+        MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+        params.add("sid", String.valueOf(sid));
+        
+        response = webResource.queryParams(params).accept(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML)
+                .get(String.class);
+        
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = parser.parse(response).getAsJsonObject();
+
+        return jsonObject;
+
+    }
 
     public JsonObject getCallsUsingFilter(String deploymentUrl, String username, String authToken, Map<String, String> filters) {
 
