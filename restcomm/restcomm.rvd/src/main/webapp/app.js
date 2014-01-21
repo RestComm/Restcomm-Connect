1273243a2318c98de602ec1c2aba33903121d0e1
Orestis Tsakiridis <otsakir@gmail.com>
2014-01-21 17:50:51 +0200

    refers issue #275. Added UI controls for URL parameter pairs. Fixed routing controls appearance in chrome.

--- restcomm/restcomm.rvd/src/main/webapp/app.js
+++ restcomm/restcomm.rvd/src/main/webapp/app.js
@@ -25,7 +25,7 @@ App.factory('stepService', function($rootScope) {
 					number: {kind:'number', label:'number', title:'Number', numberToCall:'', sendDigits:'', numberUrl:''},
 					redirect: {kind:'redirect', label:'redirect', title:'redirect', next:''},
 					hungup: {kind:'hungup', label:'hang up', title:'hang up', next:''},
-					externalService: {kind:'externalService', label:'externalService', title:'external service', url:'', assignments:[], next:'', doRouting:false, nextType:'fixed', nextVariable:''},
+					externalService: {kind:'externalService', label:'externalService', title:'external service', url:'', urlParams:[], assignments:[], next:'', doRouting:false, nextType:'fixed', nextVariable:''},
 		},
 		stepNames: ['say','gather','dial','redirect','hungup','externalService'],
 		lastStepId: 0,
