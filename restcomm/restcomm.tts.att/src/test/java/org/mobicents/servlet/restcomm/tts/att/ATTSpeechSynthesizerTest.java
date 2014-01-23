492c123969b9a7f190ee7368e339ed725881a7ad
George Vagenas <gvagenas@gmail.com>
2014-01-24 01:47:07 +0200

    Working progress for issue #48

---
+++ restcomm/restcomm.tts.att/src/test/java/org/mobicents/servlet/restcomm/tts/att/ATTSpeechSynthesizerTest.java
@@ -0,0 +1,219 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.mobicents.servlet.restcomm.tts.att;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mobicents.servlet.restcomm.cache.DiskCache;
+import org.mobicents.servlet.restcomm.cache.DiskCacheRequest;
+import org.mobicents.servlet.restcomm.cache.DiskCacheResponse;
+import org.mobicents.servlet.restcomm.cache.HashGenerator;
+import org.mobicents.servlet.restcomm.tts.api.GetSpeechSynthesizerInfo;
+import org.mobicents.servlet.restcomm.tts.api.SpeechSynthesizerInfo;
+import org.mobicents.servlet.restcomm.tts.api.SpeechSynthesizerRequest;
+import org.mobicents.servlet.restcomm.tts.api.SpeechSynthesizerResponse;
+
+import scala.concurrent.duration.FiniteDuration;
+import akka.actor.Actor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.actor.UntypedActorFactory;
+import akka.testkit.JavaTestKit;
+
+/**
+ * @author quintana.thomas@gmail.com (Thomas Quintana)
+ */
+public final class ATTSpeechSynthesizerTest {
+    private ActorSystem system;
+    private ActorRef tts;
+    private ActorRef cache;
+    private String tempSystemDirectory;
+
+    public ATTSpeechSynthesizerTest() throws ConfigurationException {
+        super();
+    }
+
+    @Before
+    public void before() throws Exception {
+        system = ActorSystem.create();
+        final URL input = getClass().getResource("/att.xml");
+        final XMLConfiguration configuration = new XMLConfiguration(input);
+        tts = tts(configuration);
+        cache = cache("/tmp/cache", "http://127.0.0.1:8080/restcomm/cache");
+        // Fix for MacOS systems: only append "/" to temporary path if it doesnt end with it - hrosa
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        tempSystemDirectory = "file:" + tmpDir + (tmpDir.endsWith("/") ? "" : "/");
+    }
+
+    @After
+    public void after() throws Exception {
+        system.shutdown();
+    }
+
+    private ActorRef tts(final Configuration configuration) {
+        final String classpath = configuration.getString("[@class]");
+
+        return system.actorOf(new Props(new UntypedActorFactory() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Actor create() throws Exception {
+                return (UntypedActor) Class.forName(classpath).getConstructor(Configuration.class).newInstance(configuration);
+            }
+        }));
+    }
+
+    private ActorRef cache(final String path, final String uri) {
+        return system.actorOf(new Props(new UntypedActorFactory() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public UntypedActor create() throws Exception {
+                return new DiskCache(path, uri, true);
+            }
+        }));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testInfo() {
+        new JavaTestKit(system) {
+            {
+                final ActorRef observer = getRef();
+                tts.tell(new GetSpeechSynthesizerInfo(), observer);
+                final SpeechSynthesizerResponse<SpeechSynthesizerInfo> response = this.expectMsgClass(
+                        FiniteDuration.create(30, TimeUnit.SECONDS), SpeechSynthesizerResponse.class);
+                assertTrue(response.succeeded());
+                final Set<String> languages = response.get().languages();
+                assertTrue(languages.contains("ca"));
+                assertTrue(languages.contains("zh"));
+                assertTrue(languages.contains("zh-hk"));
+                assertTrue(languages.contains("zh-tw"));
+                assertTrue(languages.contains("da"));
+                assertTrue(languages.contains("nl"));
+                assertTrue(languages.contains("en-au"));
+                assertTrue(languages.contains("en-ca"));
+                assertTrue(languages.contains("en-gb"));
+                assertTrue(languages.contains("en-in"));
+                assertTrue(languages.contains("en"));
+                assertTrue(languages.contains("fi"));
+                assertTrue(languages.contains("fr-ca"));
+                assertTrue(languages.contains("fr"));
+                assertTrue(languages.contains("de"));
+                assertTrue(languages.contains("it"));
+                assertTrue(languages.contains("ja"));
+                assertTrue(languages.contains("ko"));
+                assertTrue(languages.contains("nb"));
+                assertTrue(languages.contains("pl"));
+                assertTrue(languages.contains("pt-br"));
+                assertTrue(languages.contains("pt"));
+                assertTrue(languages.contains("ru"));
+                assertTrue(languages.contains("es-mx"));
+                assertTrue(languages.contains("es"));
+                assertTrue(languages.contains("sv"));
+            }
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testSynthesisMan() {
+        new JavaTestKit(system) {
+            {
+                final ActorRef observer = getRef();
+                String gender = "man";
+                String woman = "woman";
+                String language = "en";
+                String message = "Hello TTS World! This is a test for AT&T TTS Engine using Man Voice";
+
+                String hash = HashGenerator.hashMessage(gender, language, message);
+                String womanHash = HashGenerator.hashMessage(woman, language, message);
+
+                assertTrue(!hash.equalsIgnoreCase(womanHash));
+
+                final SpeechSynthesizerRequest synthesize = new SpeechSynthesizerRequest(gender, language, message);
+                tts.tell(synthesize, observer);
+                final SpeechSynthesizerResponse<URI> response = this.expectMsgClass(
+                        FiniteDuration.create(30, TimeUnit.SECONDS), SpeechSynthesizerResponse.class);
+                assertTrue(response.succeeded());
+
+                DiskCacheRequest diskCacheRequest = new DiskCacheRequest(response.get());
+                cache.tell(diskCacheRequest, observer);
+
+                final DiskCacheResponse diskCacheResponse = this.expectMsgClass(FiniteDuration.create(30, TimeUnit.SECONDS),
+                        DiskCacheResponse.class);
+                assertTrue(diskCacheResponse.succeeded());
+
+                assertEquals(tempSystemDirectory + hash + ".wav", response.get().toString());
+                assertEquals("http://127.0.0.1:8080/restcomm/cache/" + hash + ".wav", diskCacheResponse.get().toString());
+
+                FileUtils.deleteQuietly(new File(response.get()));
+            }
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testSynthesisWoman() {
+        new JavaTestKit(system) {
+            {
+                final ActorRef observer = getRef();
+                String gender = "woman";
+                String language = "en";
+                String message = "Hello TTS World! This is a test for AT&T TTS Engine using Women Voice";
+
+                String hash = HashGenerator.hashMessage(gender, language, message);
+
+                final SpeechSynthesizerRequest synthesize = new SpeechSynthesizerRequest(gender, language, message);
+                tts.tell(synthesize, observer);
+                final SpeechSynthesizerResponse<URI> response = this.expectMsgClass(
+                        FiniteDuration.create(30, TimeUnit.SECONDS), SpeechSynthesizerResponse.class);
+                assertTrue(response.succeeded());
+
+                DiskCacheRequest diskCacheRequest = new DiskCacheRequest(response.get());
+                cache.tell(diskCacheRequest, observer);
+
+                final DiskCacheResponse diskCacheResponse = this.expectMsgClass(FiniteDuration.create(30, TimeUnit.SECONDS),
+                        DiskCacheResponse.class);
+                assertTrue(diskCacheResponse.succeeded());
+
+                assertEquals(tempSystemDirectory + hash + ".wav", response.get().toString());
+                assertEquals("http://127.0.0.1:8080/restcomm/cache/" + hash + ".wav", diskCacheResponse.get().toString());
+
+                FileUtils.deleteQuietly(new File(response.get()));
+            }
+        };
+    }
+
+}
