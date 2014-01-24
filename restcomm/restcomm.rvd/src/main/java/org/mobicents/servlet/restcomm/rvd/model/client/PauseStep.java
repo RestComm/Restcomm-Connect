8377e178cf9b0ba4e823aae84c47ac17a58016c6
Orestis Tsakiridis <otsakir@gmail.com>
2014-01-24 16:23:27 +0200

    refers issue #223. Added <Pause/> verb

---
+++ restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/model/client/PauseStep.java
@@ -0,0 +1,13 @@
+package org.mobicents.servlet.restcomm.rvd.model.client;
+
+public class PauseStep extends Step {
+    Integer length;
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+}
---
+++ restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/model/rcml/RcmlPauseStep.java
@@ -0,0 +1,13 @@
+package org.mobicents.servlet.restcomm.rvd.model.rcml;
+
+public class RcmlPauseStep extends RcmlStep {
+    Integer length;
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+}
