8e98cb23cd31df785eff26ed64ff76c0fbf690c0
Alexandre Mendonça <ammendonca>
2014-04-23 01:40:49 +0100

    RESTCOMM-399: Update to Bootstrap 3.1.1. Many other fixes and enhancements.

---
+++ restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/global.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var rcMod = angular.module('rcApp');
+
+// Numbers : Incoming : List ---------------------------------------------------
+
+rcMod.controller('MainCtrl', function ($rootScope, $scope, $dialog, $http, $location, SessionService, $log) {
+  $log.debug("Entered MainCtrl...");
+});
