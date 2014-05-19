7f8c8bbc0ea87fe01303eeff9879e49641a66bef
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2014-05-19 14:51:18 +0300

    RESTCOMM-369 #comment UI - Created angular factories for PackageInfo and ConfigOption classes

--- restcomm/restcomm.rvd/src/main/webapp/js/controllers/packaging.js
+++ restcomm/restcomm.rvd/src/main/webapp/js/controllers/packaging.js
@@ -1,29 +1,25 @@
 angular.module('Rvd')
-.controller('packagingCtrl', function ($scope, $routeParams) {
+.controller('packagingCtrl', function ($scope, $routeParams, PackageInfo, ConfigOption) {
 	$scope.protos = {
 			configOption: {name:'', label:'', type:'value', description:'', defaultValue:'', required: true }
 	}
 	$scope.projectName = $routeParams.projectName;
 	$scope.packageInfo = null;
+	$scope.ConfigOption = 
 	
 	$scope.getPackageInfo = function (projectName) {
 		// retrieve package information from server
 		// ...
-		$scope.packageInfo = {
-				config: {
-					options: []
-				}
-		};
+		$scope.packageInfo = new PackageInfo().init({config: {options: []} });
 	} 
 	
-	$scope.addConfigurationOption = function(selectedOption) {
+	$scope.addConfigurationOption = function(type) {
 		console.log("Adding configuration option");
-		if ( selectedOption == "Add value" )
-			$scope.packageInfo.config.options.push(angular.copy( $scope.protos.configOption ));
+		$scope.packageInfo.addOption( ConfigOption.getTypeByLabel(type));
 	}
 	
 	$scope.removeConfigurationOption = function (option) {
-		$scope.packageInfo.config.options.splice($scope.packageInfo.config.options.indexOf(option), 1);
+		$scope.packageInfo.removeOption(option);
 	}
 	
 	
@@ -31,5 +27,31 @@ angular.module('Rvd')
 	$scope.getPackageInfo($scope.projectName);
 	
 	console.log("Initializing packaging controller");
-});
+})
+.factory('ConfigOption', ['rvdModel', function (rvdModel) {
+	var types = ['value'];
+	var typesByLabel = {'Add value': 'value'};
+	
+	function ConfigOption() {};
+	ConfigOption.prototype = new rvdModel();
+	ConfigOption.prototype.constructor = ConfigOption;
+	ConfigOption.getTypeByLabel = function(type) { return typesByLabel[type];	}
+	ConfigOption.getTypeLabels = function() {	return labels;	}
+	return ConfigOption;
+}])
+.factory('PackageInfo', ['rvdModel', 'ConfigOption', function (rvdModel, ConfigOption) {
+	function PackageInfo() {
+		this.config = { options: []};
+	};
+	PackageInfo.prototype = new rvdModel();
+	PackageInfo.prototype.constructor = PackageInfo;
+	PackageInfo.prototype.addOption = function (type) {
+		this.config.options.push( new ConfigOption(type) );
+	}
+	PackageInfo.prototype.removeOption = function (option) {
+		this.config.options.splice(this.config.options.indexOf(option,1));
+	}
+	return PackageInfo;
+}])
+;
 
