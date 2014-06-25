15c00238b56840ae53b29be7ebbb4f8230cc5b73
George Vagenas <gvagenas@telestax.com>
2014-06-25 12:09:46 +0300

    RESTCOMM-476 #Resolved Fixed

--- release/config-scripts/as7-config-scripts/restcomm/utils/read-network-props.sh
+++ release/config-scripts/as7-config-scripts/restcomm/utils/read-network-props.sh
@@ -17,20 +17,20 @@ getPrivateIP() {
 ## Description: Gets the broadcast address of the instance 
 ## Parameters : none
 getBroadcastAddress() {
-  echo "$INET_DATA" | grep "Bcast:$IP_ADDRESS_PATTERN" | awk -F: '{print $2}'
+  echo "$INET_DATA" | grep "Bcast:$IP_ADDRESS_PATTERN" | awk '{print $3}' | awk -F: '{print $2}'
 }
 
 ## Description: Gets the Subnet Mask of the instance 
 ## Parameters : none
 getSubnetMask() {
-  /sbin/ifconfig $INTERFACE | grep "Mask:$IP_ADDRESS_PATTERN" | awk -F: '{print $2}'
+  /sbin/ifconfig $INTERFACE | grep "Mask:$IP_ADDRESS_PATTERN" | awk '{print $4}' | awk -F: '{print $2}'
 }
 
 ## Description: Gets the Network of the instance 
 ## Parameters : 1.Private IP
 ## 				2.Subnet Mask
 getNetwork() {
-  ipcalc -n $1 $2 | grep -i "Network" | awk -F= '{print $2}';
+  ipcalc -n $1 $2 | grep -i "Network" | awk '{print $2}' | awk -F/ '{print $1}';
 }
 
 # MAIN
