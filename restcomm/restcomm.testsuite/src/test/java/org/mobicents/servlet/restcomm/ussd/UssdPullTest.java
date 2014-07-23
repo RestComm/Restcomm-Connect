f6ae30cb8803d29d3ab4e2257076ff5c7fdf392b
George Vagenas <gvagenas@telestax.com>
2014-07-23 10:17:33 +0300

    Fixes for failing tests in the Jenkins job

--- restcomm/restcomm.testsuite/src/test/java/org/mobicents/servlet/restcomm/ussd/UssdPullTest.java
+++ restcomm/restcomm.testsuite/src/test/java/org/mobicents/servlet/restcomm/ussd/UssdPullTest.java
@@ -43,6 +43,7 @@ import org.cafesip.sipunit.SipStack;
 import org.jboss.arquillian.container.mss.extension.SipStackTool;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.archive.ShrinkWrapMaven;
 import org.junit.After;
@@ -208,9 +209,11 @@ public class UssdPullTest {
     @Deployment(name = "UssdPullTest", managed = true, testable = false)
     public static WebArchive createWebArchiveNoGw() {
         logger.info("Packaging Test App");
-        final WebArchive archive = ShrinkWrapMaven.resolver()
+        WebArchive archive = ShrinkWrap.create(WebArchive.class, "restcomm.war");
+        final WebArchive restcommArchive = ShrinkWrapMaven.resolver()
                 .resolve("com.telestax.servlet:restcomm.application:war:" + version).withoutTransitivity()
                 .asSingle(WebArchive.class);
+        archive = archive.merge(restcommArchive);
         archive.delete("/WEB-INF/sip.xml");
         archive.delete("/WEB-INF/conf/restcomm.xml");
         archive.delete("/WEB-INF/data/hsql/restcomm.script");
