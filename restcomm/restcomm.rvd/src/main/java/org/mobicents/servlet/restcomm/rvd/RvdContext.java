dcad7d22fa4b07910521d6361fdff66786cd44a7
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2014-07-28 06:55:41 +0300

    RESTCOMM-419 #comment WIP

--- restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdContext.java
+++ restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdContext.java
@@ -2,7 +2,9 @@ package org.mobicents.servlet.restcomm.rvd;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MultivaluedMap;
 
+import org.mobicents.servlet.restcomm.rvd.interpreter.Interpreter;
 import org.mobicents.servlet.restcomm.rvd.model.ModelMarshaler;
 import org.mobicents.servlet.restcomm.rvd.storage.FsProjectStorage;
 import org.mobicents.servlet.restcomm.rvd.storage.FsStorageBase;
@@ -10,18 +12,30 @@ import org.mobicents.servlet.restcomm.rvd.storage.ProjectStorage;
 
 public class RvdContext {
 
-    private ModelMarshaler marshaler;
-    private RvdSettings settings;
-    private ProjectStorage projectStorage;
-    private ServletContext servletContext;
-    private FsStorageBase storageBase;
+    protected ModelMarshaler marshaler;
+    protected RvdSettings settings;
+    protected ProjectStorage projectStorage;
+    protected ServletContext servletContext;
+    protected FsStorageBase storageBase;
+    protected HttpServletRequest request;
+
 
     public RvdContext(HttpServletRequest request,  ServletContext servletContext) {
         this.servletContext = servletContext;
+        this.request = request;
         this.marshaler = new ModelMarshaler();
         this.settings = RvdSettings.getInstance(servletContext);
         this.storageBase = new FsStorageBase(this.settings.getWorkspaceBasePath(), marshaler);
         this.projectStorage = new FsProjectStorage(storageBase, marshaler);
+
+    }
+
+    public Interpreter createInterpreter(String appName, MultivaluedMap<String, String> requestParams) {
+        throw new UnsupportedOperationException("You'll need a ProjectAwareRvdContext to use an interpreter");
+    }
+
+    public ProjectLogger getProjectLogger() {
+        throw new UnsupportedOperationException("You'll need a ProjectAwareRvdContext to use ProjectLogger");
     }
 
     public ModelMarshaler getMarshaler() {
@@ -43,6 +57,4 @@ public class RvdContext {
     public FsStorageBase getStorageBase() {
         return storageBase;
     }
-
-
 }
