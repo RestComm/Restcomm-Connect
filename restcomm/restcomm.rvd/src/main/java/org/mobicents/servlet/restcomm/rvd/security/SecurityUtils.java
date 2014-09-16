61a9a07be05088e851b4bd5f686703d467569430
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2014-09-16 15:32:28 +0300

    RESTCOMM-575 #comment Added username info in rvdticket cookie and displayed it in the top menu

--- restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/security/SecurityUtils.java
+++ restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/security/SecurityUtils.java
@@ -9,9 +9,17 @@ public class SecurityUtils {
     /*
      * Encapsulate rvdticket cookie creation. It is done both when logging in and when renewing the ticket (in any authenticated request)
      */
-    public static NewCookie createTicketCookie( String ticketId ) {
-        //return new NewCookie(RvdConfiguration.TICKET_COOKIE_NAME, ticketId, "/restcomm-rvd/services", null, null,1800, false );
-        return new NewCookie(RvdConfiguration.TICKET_COOKIE_NAME, ticketId, "/restcomm-rvd", null, null,1800, false );
+    public static NewCookie createTicketCookieRaw( String ticketCookie ) {
+        //return new newcookie(rvdconfiguration.ticket_cookie_name, ticketid, "/restcomm-rvd/services", null, null,1800, false );
+        return new NewCookie(RvdConfiguration.TICKET_COOKIE_NAME, ticketCookie, "/restcomm-rvd", null, null,1800, false );
+    }
+
+    public static NewCookie createTicketCookie( Ticket ticket ) {
+        //return new NewCookie(RvdConfiguration.TICKET_COOKIE_NAME, ticketId, "/restcomm-rvd/services", null, null,1800, false );.getTicketId()
+        if ( ticket == null )
+            return new NewCookie(RvdConfiguration.TICKET_COOKIE_NAME, "", "/restcomm-rvd", null, null,1800, false );
+        else
+            return new NewCookie(RvdConfiguration.TICKET_COOKIE_NAME, ticket.getUserId() + ":" + ticket.getTicketId(), "/restcomm-rvd", null, null,1800, false );
     }
 
 }
