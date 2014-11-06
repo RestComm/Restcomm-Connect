f4698b832de1b36f9710d853a01c05d4742b18ad
George Vagenas <gvagenas@gmail.com>
2014-11-06 17:32:47 +0200

    Add optimized versions of standalone.conf, mss-sip-stack.properties and mms/run.sh

--- release/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-media-server.sh
+++ release/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-media-server.sh
@@ -4,30 +4,30 @@
 
 configUdpManager() {
 	FILE=$MMS_HOME/deploy/server-beans.xml
-		
+
 	sed -e "s|<property name=\"bindAddress\">.*<\/property>|<property name=\"bindAddress\">$1<\/property>|" \
 	    -e "s|<property name=\"localBindAddress\">.*<\/property>|<property name=\"localBindAddress\">$1<\/property>|" \
 	    -e "s|<property name=\"localNetwork\">.*<\/property>|<property name=\"localNetwork\">$2<\/property>|" \
 	    -e "s|<property name=\"localSubnet\">.*<\/property>|<property name=\"localSubnet\">$3<\/property>|" \
 	    -e 's|<property name="useSbc">.*</property>|<property name="useSbc">true</property>|' \
 	    -e 's|<property name="dtmfDetectorDbi">.*</property>|<property name="dtmfDetectorDbi">0</property>|' \
-	    -e 's|<property name="lowestPort">.*</property>|<property name="lowestPort">64534</property>|' \
+	    -e 's|<property name="lowestPort">.*</property>|<property name="lowestPort">34534</property>|' \
 	    -e 's|<property name="highestPort">.*</property>|<property name="highestPort">65535</property>|' \
 	    $FILE > $FILE.bak
-	    
+
 	grep -q -e '<property name="lowestPort">.*</property>' $FILE.bak || sed -i '/rtpTimeout/ a\
-    <property name="lowestPort">64534</property>' $FILE.bak
-    
+    <property name="lowestPort">34534</property>' $FILE.bak
+
     grep -q -e '<property name="highestPort">.*</property>' $FILE.bak || sed -i '/rtpTimeout/ a\
     <property name="highestPort">65535</property>' $FILE.bak
-	
+
 	mv $FILE.bak $FILE
 	echo 'Configured UDP Manager'
 }
 
 configJavaOpts() {
     FILE=$MMS_HOME/bin/run.sh
-    
+
 	# Find total available memory on the instance
     TOTAL_MEM=$(free -m -t | grep 'Total:' | awk '{print $2}')
     # get 20 percent of available memory
@@ -38,9 +38,9 @@ configJavaOpts() {
     # use multiples of 64mb to know effective memory
     FINAL_MEM=$(echo "$MULTIPLIER * 64" | bc -l)
     MEM_UNIT='m'
-    
+
     MMS_OPTS="-Xms$FINAL_MEM$MEM_UNIT -Xmx$FINAL_MEM$MEM_UNIT -XX:MaxPermSize=256m -Dorg.jboss.resolver.warning=true -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
-    
+
 	sed -e "/# Setup MMS specific properties/ {
 		N; s|JAVA_OPTS=.*|JAVA_OPTS=\"$MMS_OPTS\"|
 	}" $FILE > $FILE.bak
@@ -51,7 +51,7 @@ configJavaOpts() {
 configLogDirectory() {
 	FILE=$MMS_HOME/conf/log4j.xml
 	DIRECTORY=$MMS_HOME/log
-	
+
 	sed -e "/<param name=\"File\" value=\".*server.log\" \/>/ s|value=\".*server.log\"|value=\"$DIRECTORY/server.log\"|" \
 	    $FILE > $FILE.bak
 	mv $FILE.bak $FILE
