773104d800ff560a211bf44739520ea999a744b1
George Vagenas <gvagenas@gmail.com>
2015-01-23 15:29:30 +0200

    WIP

--- restcomm/restcomm.telephony/src/main/java/org/mobicents/servlet/restcomm/telephony/Call.java
+++ restcomm/restcomm.telephony/src/main/java/org/mobicents/servlet/restcomm/telephony/Call.java
@@ -555,6 +555,7 @@ public final class Call extends UntypedActor {
                 } else if (muting.equals(state) || unmuting.equals(state)) {
                     fsm.transition(message, closingRemoteConnection);
                 } else if (closingRemoteConnection.equals(state)) {
+                    context().stop(remoteConn);
                     remoteConn = null;
                     if (internalLink != null) {
                         fsm.transition(message, closingInternalLink);
@@ -1221,6 +1222,7 @@ public final class Call extends UntypedActor {
             }
             if (remoteConn != null) {
                 gateway.tell(new DestroyConnection(remoteConn), source);
+                context().stop(remoteConn);
                 remoteConn = null;
             }
             // Explicitly invalidate the application session.
@@ -1300,6 +1302,7 @@ public final class Call extends UntypedActor {
         public void execute(final Object message) throws Exception {
             if (remoteConn != null) {
                 gateway.tell(new DestroyConnection(remoteConn), source);
+                context().stop(remoteConn);
                 remoteConn = null;
             }
             if (direction.equalsIgnoreCase(INBOUND))
@@ -1642,14 +1645,18 @@ public final class Call extends UntypedActor {
             }
             if (remoteConn != null) {
                 gateway.tell(new DestroyConnection(remoteConn), source);
+                context().stop(remoteConn);
                 remoteConn = null;
             }
             if (internalLink != null) {
                 gateway.tell(new DestroyLink(internalLink), source);
+                context().stop(internalLink);
+                context().stop(internalLinkEndpoint);
                 internalLink = null;
             }
             if (bridge != null) {
                 gateway.tell(new DestroyEndpoint(bridge), source);
+                context().stop(bridge);
                 bridge = null;
             }
             // Explicitly invalidate the application session.
