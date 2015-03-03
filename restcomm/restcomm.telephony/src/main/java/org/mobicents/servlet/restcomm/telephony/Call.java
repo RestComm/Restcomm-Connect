dfd3ed1c247a0382a865e47c35899998b869ef5a
George Vagenas <gvagenas@gmail.com>
2015-03-03 10:59:30 +0200

    RESTCOMM-691 #Comment SIP INFO for DTMF detection

--- restcomm/restcomm.telephony/src/main/java/org/mobicents/servlet/restcomm/telephony/Call.java
+++ restcomm/restcomm.telephony/src/main/java/org/mobicents/servlet/restcomm/telephony/Call.java
@@ -622,6 +622,8 @@ public final class Call extends UntypedActor {
                 }
             } else if ("BYE".equalsIgnoreCase(method)) {
                 fsm.transition(message, closingRemoteConnection);
+            } else if ("INFO".equalsIgnoreCase(method)) {
+                processInfo(request);
             }
         } else if (message instanceof SipServletResponse) {
             final SipServletResponse response = (SipServletResponse) message;
@@ -738,6 +740,25 @@ public final class Call extends UntypedActor {
             }
             group = getMediaGroup(message);
             sender.tell(new CallResponse<ActorRef>(group), self);
+        }
+    }
+
+    private void processInfo(final SipServletRequest request) throws IOException {
+        final SipServletResponse okay = request.createResponse(SipServletResponse.SC_OK);
+        okay.send();
+        String digits = null;
+        if (request.getContentType().equalsIgnoreCase("application/dtmf-relay")){
+            final String content = new String(request.getRawContent());
+            digits = content.split("\n")[0].replaceFirst("Signal=","").trim();
+        } else {
+        digits = new String(request.getRawContent());
+        }
+        if (digits != null) {
+            MediaGroupResponse<String> infoResponse = new MediaGroupResponse<String>(digits);
+            for (final ActorRef observer : observers) {
+                observer.tell(infoResponse, self());
+            }
+            group.tell(new Stop(), self());
         }
     }
 
