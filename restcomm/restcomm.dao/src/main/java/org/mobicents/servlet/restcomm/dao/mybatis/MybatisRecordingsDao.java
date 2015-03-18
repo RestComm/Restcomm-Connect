23404adaac05915833073711c71ede96f10c7238
George Vagenas <gvagenas@gmail.com>
2015-03-18 20:05:43 +0200

    RESTCOMM-699 #Comment work in progress

--- restcomm/restcomm.dao/src/main/java/org/mobicents/servlet/restcomm/dao/mybatis/MybatisRecordingsDao.java
+++ restcomm/restcomm.dao/src/main/java/org/mobicents/servlet/restcomm/dao/mybatis/MybatisRecordingsDao.java
@@ -27,13 +27,14 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-
 import org.joda.time.DateTime;
 
 import static org.mobicents.servlet.restcomm.dao.DaoUtils.*;
+
 import org.mobicents.servlet.restcomm.dao.RecordingsDao;
 import org.mobicents.servlet.restcomm.entities.Recording;
 import org.mobicents.servlet.restcomm.entities.Sid;
+import org.mobicents.servlet.restcomm.amazonS3.S3AccessTool;
 import org.mobicents.servlet.restcomm.annotations.concurrency.ThreadSafe;
 
 /**
@@ -43,14 +44,30 @@ import org.mobicents.servlet.restcomm.annotations.concurrency.ThreadSafe;
 public final class MybatisRecordingsDao implements RecordingsDao {
     private static final String namespace = "org.mobicents.servlet.sip.restcomm.dao.RecordingsDao.";
     private final SqlSessionFactory sessions;
+    private S3AccessTool s3AccessTool;
+    private String recordingPath;
 
     public MybatisRecordingsDao(final SqlSessionFactory sessions) {
         super();
         this.sessions = sessions;
     }
 
+    public MybatisRecordingsDao(final SqlSessionFactory sessions, final S3AccessTool s3AccessTool, final String recordingPath) {
+        super();
+        this.sessions = sessions;
+        this.s3AccessTool = s3AccessTool;
+        this.recordingPath = recordingPath;
+    }
+
     @Override
-    public void addRecording(final Recording recording) {
+    public void addRecording(Recording recording) {
+        if (s3AccessTool != null) {
+            URI s3Uri = s3AccessTool.uploadFile("/"+recordingPath+"/"+recording.getSid().toString()+".wav");
+            if (s3Uri != null) {
+                recording.builder().setUri(s3Uri);
+                recording = recording.builder().build();
+            }
+        }
         final SqlSession session = sessions.openSession();
         try {
             session.insert(namespace + "addRecording", toMap(recording));
