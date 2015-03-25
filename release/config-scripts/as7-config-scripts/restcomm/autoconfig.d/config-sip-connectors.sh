126db9fcd6d6084d7edfd052129ed2d0e2c1ba9c
George Vagenas <gvagenas@gmail.com>
2015-03-25 15:30:44 +0200

    Fixed minor issue for auto-config scripts

--- release/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-sip-connectors.sh
+++ release/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-sip-connectors.sh
@@ -12,7 +12,7 @@ configConnectors() {
 	proxy_address="$2"
 
 	if [ "$ACTIVE_PROXY" == "true" ] || [ "$ACTIVE_PROXY" == "TRUE" ]; then
-		sed -e "s|path-name=\"org.mobicents.ext\"  \(app-dispatcher-class=.*\)|path-name=\"org.mobicents.ha.balancing.only\" \1|" \
+		sed -e "s|path-name=\"org.mobicents.ext\" \(app-dispatcher-class=.*\)|path-name=\"org.mobicents.ha.balancing.only\" \1|" \
 		-e "s|<connector name=\"sip-udp\" .*/>|<connector name=\"sip-udp\" protocol=\"SIP/2.0\" scheme=\"sip\" socket-binding=\"sip-udp\" use-static-address=\"true\" static-server-address=\"$proxy_address\" static-server-port=\"5060\"/>|" \
 	        -e "s|<connector name=\"sip-tcp\" .*/>|<connector name=\"sip-tcp\" protocol=\"SIP/2.0\" scheme=\"sip\" socket-binding=\"sip-tcp\" use-static-address=\"true\" static-server-address=\"$proxy_address\" static-server-port=\"5060\"/>|" \
 	        -e "s|<connector name=\"sip-tls\" .*/>|<connector name=\"sip-tls\" protocol=\"SIP/2.0\" scheme=\"sip\" socket-binding=\"sip-tls\" use-static-address=\"true\" static-server-address=\"$proxy_address\" static-server-port=\"5061\"/>|" \
