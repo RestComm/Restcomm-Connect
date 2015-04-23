00afb684c34963718ffe066c15448c4f3e6ab07f
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-04-23 13:34:19 +0300

    Started adding keycloak support to adminUI. Refers #58.  - Disabled old authentication mechanism  - Added keycloak dependencies and 'stub' file (keycloak-app.js)
    Conflicts:
    
    	restcomm/restcomm.ui/src/main/webapp/index.html

---
+++ restcomm/restcomm.ui/src/main/webapp/keycloak-app.js
@@ -0,0 +1,80 @@
+// var module = angular.module('product', []);
+
+var auth = {};
+var logout = function(){
+    console.log('*** LOGOUT');
+    auth.loggedIn = false;
+    auth.authz = null;
+    window.location = auth.logoutUrl;
+};
+
+
+angular.element(document).ready(function ($http) {
+    var keycloakAuth = new Keycloak('keycloak.json');
+    auth.loggedIn = false;
+
+    keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
+        auth.loggedIn = true;
+        auth.authz = keycloakAuth;
+        auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/demo/tokens/logout?redirect_uri=http://192.168.1.29:8080/restcomm-management/index.html";
+        angular.module('rcApp').factory('Auth', function() {
+            return auth;
+        });
+        angular.bootstrap(document, ["rcApp"]);
+    }).error(function (a, b) {
+            window.location.reload();
+        });
+
+});
+
+angular.module('rcApp').factory('authInterceptor', function($q, Auth) {
+    return {
+        request: function (config) {
+            var deferred = $q.defer();
+            if (Auth.authz.token) {
+                Auth.authz.updateToken(5).success(function() {
+                    config.headers = config.headers || {};
+                    config.headers.Authorization = 'Bearer ' + Auth.authz.token;
+
+                    deferred.resolve(config);
+                }).error(function() {
+                        deferred.reject('Failed to refresh token');
+                    });
+            }
+            return deferred.promise;
+        }
+    };
+});
+
+
+
+
+angular.module('rcApp').config(function($httpProvider) {
+    $httpProvider.responseInterceptors.push('errorInterceptor');
+    $httpProvider.interceptors.push('authInterceptor');
+
+});
+
+angular.module('rcApp').factory('errorInterceptor', function($q) {
+    return function(promise) {
+        return promise.then(function(response) {
+            return response;
+        }, function(response) {
+            if (response.status == 401) {
+                console.log('session timeout?');
+                logout();
+            } else if (response.status == 403) {
+                alert("Forbidden");
+            } else if (response.status == 404) {
+                alert("Not found");
+            } else if (response.status) {
+                if (response.data && response.data.errorMessage) {
+                    alert(response.data.errorMessage);
+                } else {
+                    alert("An unexpected server error has occurred");
+                }
+            }
+            return $q.reject(response);
+        });
+    };
+});
