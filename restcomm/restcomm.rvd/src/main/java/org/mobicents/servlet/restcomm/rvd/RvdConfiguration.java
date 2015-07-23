f17b4b87eff17639f7235cfd197acd210a51bb1d
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-07-23 11:09:15 +0300

    New RVD project validation schema (1.4) defined. It contais the Email element. Old schema (1.3) restored. Refers #339.

--- restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdConfiguration.java
+++ restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/RvdConfiguration.java
@@ -24,7 +24,7 @@ public class RvdConfiguration {
     public static final String REST_SERVICES_PATH = "services"; // the "services" from the /restcomm-rvd/services/apps/... path
 
     public static final String WAVS_DIRECTORY_NAME = "wavs";
-    private static final String RVD_PROJECT_VERSION = "1.3"; // version for rvd project syntax
+    private static final String RVD_PROJECT_VERSION = "1.4"; // version for rvd project syntax
     private static final String PACKAGING_VERSION = "1.0";
     private static final String RAS_APPLICATION_VERSION = "2"; // version of the RAS application specification
     public static final String STICKY_PREFIX = "sticky_"; // a  prefix for rvd sticky variable names
