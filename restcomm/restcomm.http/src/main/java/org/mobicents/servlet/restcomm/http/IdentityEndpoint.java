68bec6a65e8e772b8aacb5b8346a0445eeb43e85
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-07-24 17:49:01 +0300

    Added ...identity/instance/register method. Improvements to the KeycloakClient wrapper class also included. Refers #355.

--- restcomm/restcomm.http/src/main/java/org/mobicents/servlet/restcomm/http/IdentityEndpoint.java
+++ restcomm/restcomm.http/src/main/java/org/mobicents/servlet/restcomm/http/IdentityEndpoint.java
@@ -1,16 +1,20 @@
 package org.mobicents.servlet.restcomm.http;
 
+import java.util.UUID;
+
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import org.keycloak.representations.AccessTokenResponse;
+import org.apache.commons.lang.StringUtils;
 import org.mobicents.servlet.restcomm.identity.KeycloakClient;
 import org.mobicents.servlet.restcomm.identity.KeycloakClient.KeycloakClientException;
 
 @Path("/instance")
 public class IdentityEndpoint extends AbstractEndpoint {
+
+    public static String IDENTITY_PROXY_URL = "https://identity.restcomm.com/instance-manager";
 
     public IdentityEndpoint() {
         // TODO Auto-generated constructor stub
@@ -18,18 +22,28 @@ public class IdentityEndpoint extends AbstractEndpoint {
 
     @POST
     @Path("/register")
-    public Response registerInstance(@FormParam("restcommBaseUrl") String baseUrl, @FormParam("authUrl") String authUrl, @FormParam("username") String username, @FormParam("password") String password) throws KeycloakClientException {
-        /*IdentityModeEntity modeEntity = new IdentityModeEntity();
-        modeEntity.setMode(keycloakConfigurator.getMode());
-        Gson gson = new Gson();
-        return Response.ok(gson.toJson(modeEntity),MediaType.APPLICATION_JSON).build();
-        */
+    public Response registerInstance(@FormParam("restcommBaseUrl") String baseUrl, @FormParam("authUrl") String authUrl, @FormParam("username") String username, @FormParam("password") String password, @FormParam("instanceSecret") String instanceSecret ) throws KeycloakClientException {
+        String instanceName = generateInstanceName(username);
+        if (StringUtils.isEmpty(instanceSecret))
+            instanceSecret = generateInstanceSecret();
 
         KeycloakClient keycloakClient = new KeycloakClient(username, password, "restcomm-identity-rest", "restcomm", authUrl);
-        AccessTokenResponse token = keycloakClient.getToken();
-        logger.info("token: " +  token.getToken() );
+        keycloakClient.getToken();
+        keycloakClient.addParam("name", instanceName); // what we put here??
+        keycloakClient.addParam("prefix", baseUrl);
+        keycloakClient.addParam("secret", instanceSecret); // generate a random secret for the instance/restcomm-rest client if none specified in the request
+        keycloakClient.makePostRequest(IDENTITY_PROXY_URL + "/api/instances"); // we assume that the identity proxy lives with the authorization server
 
+        logger.info( "User '" + username + "' registed this instance as '" + instanceName + "' at authorization server " + authUrl);
         return Response.ok().build();
+    }
+
+    protected String generateInstanceSecret() {
+        return UUID.randomUUID().toString();
+    }
+
+    protected String generateInstanceName(String username) {
+        return username + "-" + UUID.randomUUID().toString().split("-")[0];
     }
 
 }
