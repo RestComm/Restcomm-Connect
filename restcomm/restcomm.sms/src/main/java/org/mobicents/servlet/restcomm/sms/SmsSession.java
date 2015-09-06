35a7f41d963c3558cd13b030882bd9d89b8406bd
George Vagenas <gvagenas@gmail.com>
2015-09-06 21:02:11 +0300

    Refers to #283, Restcomm Monitoring Service work in progress

--- restcomm/restcomm.sms/src/main/java/org/mobicents/servlet/restcomm/sms/SmsSession.java
+++ restcomm/restcomm.sms/src/main/java/org/mobicents/servlet/restcomm/sms/SmsSession.java
@@ -43,6 +43,7 @@ import org.mobicents.servlet.restcomm.entities.Registration;
 import org.mobicents.servlet.restcomm.patterns.Observe;
 import org.mobicents.servlet.restcomm.patterns.Observing;
 import org.mobicents.servlet.restcomm.patterns.StopObserving;
+import org.mobicents.servlet.restcomm.telephony.TextMessage;
 
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
@@ -73,8 +74,10 @@ public final class SmsSession extends UntypedActor {
     private SmsSessionRequest initial;
     private SmsSessionRequest last;
 
+    private final ActorRef monitoringService;
+
     public SmsSession(final Configuration configuration, final SipFactory factory, final SipURI transport,
-            final DaoManager storage) {
+            final DaoManager storage, final ActorRef monitoringService) {
         super();
         this.configuration = configuration;
         this.factory = factory;
@@ -82,6 +85,7 @@ public final class SmsSession extends UntypedActor {
         this.transport = transport;
         this.attributes = new HashMap<String, Object>();
         this.storage = storage;
+        this.monitoringService = monitoringService;
     }
 
     private void inbound(final Object message) throws IOException {
@@ -187,6 +191,7 @@ public final class SmsSession extends UntypedActor {
             return;
         }
 
+        monitoringService.tell(new TextMessage(from, to, TextMessage.SmsState.OUTBOUND), self());
         final ClientsDao clients = storage.getClientsDao();
         final Client toClient = clients.getClient(to);
         Registration toClientRegistration = null;
