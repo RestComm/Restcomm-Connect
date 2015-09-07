e142303162f9e53659c60aab8d9382a08fd1ec8b
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-09-07 10:18:17 +0300

    Moved adapter file location from RVD to Restcomm /WEB-INF/conf/rvd directory. That way we eliminate stale configuration issues when hot-deploying RVD. Refers #425.

--- restcomm/restcomm.identity/src/main/java/org/mobicents/servlet/restcomm/identity/configuration/RvdConfigurationUpdateListener.java
+++ restcomm/restcomm.identity/src/main/java/org/mobicents/servlet/restcomm/identity/configuration/RvdConfigurationUpdateListener.java
@@ -1,6 +1,7 @@
 package org.mobicents.servlet.restcomm.identity.configuration;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.servlet.ServletContext;
 
@@ -13,38 +14,48 @@ import org.mobicents.servlet.restcomm.identity.configuration.IdentityConfigurato
 
 public class RvdConfigurationUpdateListener implements ConfigurationUpdateListener<IdentityConfigurator> {
 
+    // Relative path of the rvd keycloak adapter configuration parent directory based on Restcomm context root path
+    private static final String RVD_ADAPTER_PATH = "/WEB-INF/conf/rvd";
+
     protected Logger logger = Logger.getLogger(RvdConfigurationUpdateListener.class);
 
-    String rvdContextRealPath; // the root directory where RVD lives
+    private String absoluteRvdAdapterPath;
 
     public RvdConfigurationUpdateListener(ServletContext servletContext) {
-        rvdContextRealPath = servletContext.getRealPath("../restcomm-rvd.war");
+        absoluteRvdAdapterPath = servletContext.getRealPath(RVD_ADAPTER_PATH);
     }
 
     /**
-     * Updates RVD adapter configuration file, restcomm-rvd.war/WEB-INF/keycloak.json
-     * with with options built out of current restcomm configuration.
+     * Updates RVD adapter configuration file, restcomm.war/WEB-INF/conf/keycloak.json
+     * with options built out of current restcomm configuration.
      */
     @Override
     public void configurationUpdated(IdentityConfigurator configurator) {
-        // update rvd keycloak.json file
+        // Create the rvd configuration directory if it does not exist
+        File adapterParentDir = new File(absoluteRvdAdapterPath);
+        if ( ! adapterParentDir.exists() ) {
+            try {
+                FileUtils.forceMkdir(adapterParentDir);
+            } catch (IOException e1) {
+                logger.error("Could not create keycloak RVD adapter directory: '" + adapterParentDir + "'. RVD won't be properly secured.", e1);
+            }
+        }
 
-        String adapterConfigFilePath = rvdContextRealPath + "/WEB-INF/keycloak.json";
-
+        // remove adapter file if still in 'init' mode
+        String adapterConfigFilePath = absoluteRvdAdapterPath + "/keycloak.json";
         if (configurator.getMode() == IdentityMode.init) {
             FileUtils.deleteQuietly(new File(adapterConfigFilePath)); // remove RVD keycloak adapter to have a consistent setup
             return;
         }
 
+        // create adapter file - keycloak.json
         try {
             AdapterConfig adapterConfig = configurator.getRestcommRvdConfig();
             configurator.writeAdapterConfigToFile(adapterConfig, adapterConfigFilePath);
-            logger.info("Updated RVD keycloak adapter configuration.");
+            logger.info("Updated RVD keycloak adapter configuration file: '" + adapterConfigFilePath + "'");
         } catch (IdentityNotSet e) {
             logger.error("Restcomm not registered to an authorization server. There is probably a configuration error. RVD keycloak adapter configuration won't be updated.",e);
         }
-
-
     }
 
 }
