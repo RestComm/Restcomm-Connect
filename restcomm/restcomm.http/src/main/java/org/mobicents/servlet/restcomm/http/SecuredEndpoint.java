793adee2d0066b6714af17abc25e755f25d35311
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-09-17 17:02:44 +0300

    Introduced dual authentication mechanism for oauth and apikey types. Authentication is transparent and either verifies the keycloak oauth token or validates the Basic HTTP credentials against local accounts and roles. The new IdentityContext class was created to abstract these operations. Fixes #441.

--- restcomm/restcomm.http/src/main/java/org/mobicents/servlet/restcomm/http/SecuredEndpoint.java
+++ restcomm/restcomm.http/src/main/java/org/mobicents/servlet/restcomm/http/SecuredEndpoint.java
@@ -7,12 +7,13 @@ import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.Permission;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.authz.SimpleRole;
-import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.authz.permission.WildcardPermissionResolver;
-import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.representations.AccessToken;
 import org.mobicents.servlet.restcomm.entities.Account;
 import org.mobicents.servlet.restcomm.entities.shiro.ShiroResources;
+import org.mobicents.servlet.restcomm.identity.AccountKey;
+import org.mobicents.servlet.restcomm.identity.AuthOutcome;
+import org.mobicents.servlet.restcomm.identity.IdentityContext;
 import org.mobicents.servlet.restcomm.identity.configuration.IdentityConfigurator;
 import org.mobicents.servlet.restcomm.identity.configuration.IdentityResourceNames;
 
@@ -26,6 +27,7 @@ public abstract class SecuredEndpoint extends AbstractEndpoint {
     //protected KeycloakAdminClient keycloakClient;
     protected static RestcommRoles restcommRoles;
     protected IdentityConfigurator identityConfigurator;
+    protected IdentityContext identityContext;
 
     public SecuredEndpoint() {
         super();
@@ -36,25 +38,56 @@ public abstract class SecuredEndpoint extends AbstractEndpoint {
         ShiroResources shiroResources = ShiroResources.getInstance();
         restcommRoles = shiroResources.get(RestcommRoles.class);
         this.identityConfigurator = (IdentityConfigurator) context.getAttribute(IdentityConfigurator.class.getName());
-        //keycloakClient = new KeycloakAdminClient();
+        this.identityContext = new IdentityContext(identityConfigurator, request, accountsDao);
     }
 
-    // Throws an authorization exception in case the user does not have the permission OR does not own (or is a parent) the account
-    protected void secure(final Account account, final String permission) throws AuthorizationException {
-        secureKeycloak(account, permission, getKeycloakAccessToken());
+    // Authorize request by using either keycloak token or API key method. If any of them succeeds, request is allowed
+    /**
+     * High level authorization. It grants access to 'account' resources required by permission.
+     * It takes into account any Oauth token of API Key existing in the request.
+     * @param operatedAccount
+     * @param permission
+     * @throws AuthorizationException
+     */
+    protected void secure(final Account operatedAccount, final String permission) throws AuthorizationException {
+        if ( identityContext.getOauthToken() != null )
+            if ( secureKeycloak(operatedAccount, permission, identityContext.getOauthToken() ) == AuthOutcome.OK )
+                return;
+
+        if ( identityContext.getAccountKey() != null )
+            if ( secureApikey(operatedAccount, permission, identityContext.getAccountKey()) == AuthOutcome.OK )
+                return;
+
+        throw new AuthorizationException();
+    }
+
+    protected void secure (final String permission) {
+        Set<String> roleNames = null;
+        if ( identityContext.getOauthToken() != null )
+            roleNames = identityContext.getOauthToken().getResourceAccess(identityConfigurator.getClientName(IdentityResourceNames.RESTCOMM_REST)).getRoles();
+        else
+        if ( identityContext.getAccountKey() != null ) {
+            if ( identityContext.getAccountKey().isVerified() )
+                roleNames = identityContext.getAccountKey().getRoles();
+        }
+
+        if ( roleNames != null )
+            if ( secureApi(permission, roleNames) == AuthOutcome.OK )
+                return;
+
+        throw new AuthorizationException();
     }
 
     // check if the user with the roles in accessToken can access has the following permissions (on the API)
-    protected void secureApi(String neededPermissionString, final AccessToken accessToken) {
+    /**
+     * Checks if the a user with roles 'roleNames' is allowed to perform actions in 'neededPermissionString'
+     * @param neededPermissionString
+     * @param roleNames
+     * @return
+     */
+    private AuthOutcome secureApi(String neededPermissionString, Set<String> roleNames) {
         // normalize the permission string
         neededPermissionString = "domain:" + neededPermissionString;
-
-        Set<String> roleNames;
-        try {
-            roleNames = accessToken.getResourceAccess(identityConfigurator.getClientName(IdentityResourceNames.RESTCOMM_REST)).getRoles();
-        } catch (NullPointerException e) {
-            throw new UnauthorizedException("No access token present or no roles in it");
-        }
 
         WildcardPermissionResolver resolver = new WildcardPermissionResolver();
         Permission neededPermission = resolver.resolvePermission(neededPermissionString);
@@ -73,67 +106,96 @@ public abstract class SecuredEndpoint extends AbstractEndpoint {
                 for (Permission permission: permissions) {
                     if (permission.implies(neededPermission)) {
                         logger.debug("Granted access by permission " + permission.toString());
-                        return;
+                        return AuthOutcome.OK;
                     }
                 }
                 logger.debug("Role " + roleName + " does not allow " + neededPermissionString);
             }
         }
-        throw new AuthorizationException();
+        return AuthOutcome.FAILED;
     }
 
-    private void secureKeycloak(final Account account, final String neededPermissionString, final AccessToken accessToken) {
-        secureApi(neededPermissionString, accessToken);
+    /**
+     * Implements authorization using keycloak Oauth token
+     * @param account
+     * @param neededPermissionString
+     * @param accessToken
+     * @return
+     */
+    private AuthOutcome secureKeycloak(final Account account, final String neededPermissionString, final AccessToken accessToken) {
+        // both api-level and account-level checks should be satisfied
+        Set<String> roleNames = accessToken.getResourceAccess(identityConfigurator.getClientName(IdentityResourceNames.RESTCOMM_REST)).getRoles();
+        if ( secureApi(neededPermissionString, roleNames) == AuthOutcome.FAILED )
+            return AuthOutcome.FAILED;
         // check if the logged user has access to the account that is operated upon
-        secureByAccount(accessToken, account);
+        if ( secureAccountByUsername(accessToken.getPreferredUsername(), account) == AuthOutcome.FAILED )
+            return AuthOutcome.FAILED;
+
+        return AuthOutcome.OK;
+    }
+
+    /**
+     * Implements authorization using the API Key credentials i.e. Basic HTTP auth username:password and compares against local authToken and roles.
+     * @param account
+     * @param permission
+     * @param accountKey
+     * @return
+     */
+    private AuthOutcome secureApikey(final Account account, final String permission, final AccountKey accountKey) {
+        if ( ! accountKey.isVerified() )
+            return AuthOutcome.FAILED;
+
+        Set<String> roleNames = accountKey.getRoles();
+        if ( secureApi(permission, roleNames) == AuthOutcome.FAILED )
+            return AuthOutcome.FAILED;
+        // check if the logged user has access to the account that is operated upon
+        if ( secureAccount(accountKey.getAccount(), account) == AuthOutcome.FAILED )
+            return AuthOutcome.FAILED;
+
+        return AuthOutcome.OK;
     }
 
     // uses keycloak token
     protected String getLoggedUsername() {
-        KeycloakSecurityContext session = (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
-        if (session.getToken() != null) {
-            return session.getToken().getPreferredUsername();
+        AccessToken token = identityContext.getOauthToken();
+        if (token != null) {
+            return token.getPreferredUsername();
         }
         return null;
     }
 
-    /* make sure the token bearer can access data that belong to this account. In its simplest form this means that the username in the token
-     * is the same as the account username. When the organization concepts are implemented and hierarchical accounts are created a smarter
-     * approach that will allow parant users access the resources of their children should be employed.
+    /**
+     * Makes sure that User 'username' can access resources of the operatedAccount. An Account should
+     * be mapped to the specific User through its emailAddress property.
+     *
      */
-    protected void secureByAccount(final AccessToken accessToken, final Account account) {
+    protected AuthOutcome secureAccountByUsername(final String username, final Account operatedAccount) {
         // load logged user's account
-        Account loggedAccount = accountsDao.getAccount(accessToken.getPreferredUsername());
-        if ( loggedAccount != null && loggedAccount.getSid() != null ) {
-            // check if loggedAccount is the same as the checked account or is parent of it
-            if ( loggedAccount.getSid().equals(account.getSid()) || loggedAccount.getSid().equals(account.getAccountSid()) ) {
-                // no op
-            } else {
-                throw new UnauthorizedException("User cannot access resources for the specified account.");
-            }
-        }
+        Account loggedAccount = accountsDao.getAccount(username);
+        //logger.warn("User '" + username + "' was refused to account '" + account.getSid() + "'");
+        return secureAccount(loggedAccount, operatedAccount);
     }
 
-    // does the accessToken contain the role?
-    /*
-    protected void secureByRole(final AccessToken accessToken, String role) {
-        Set<String> roleNames;
-        try {
-            roleNames = accessToken.getRealmAccess().getRoles();
-        } catch (NullPointerException e) {
-            throw new UnauthorizedException("No access token present or no roles in it");
+    /**
+     * Makes sure a user authenticated against actorAccount can access operatedAccount. In practice allows access if actorAccount == operatedAccount
+     * OR if operatedAccount is a sub-account of actorAccount
+     * @param actorAccount
+     * @param operatedAccount
+     * @return
+     */
+    protected AuthOutcome secureAccount(Account actorAccount, final Account operatedAccount) {
+        if ( actorAccount != null && actorAccount.getSid() != null ) {
+            if ( actorAccount.getSid().equals(operatedAccount.getSid()) || actorAccount.getSid().equals(operatedAccount.getAccountSid()) ) {
+                return AuthOutcome.OK;
+            }
         }
-
-        if (roleNames.contains(role))
-            return;
-        else
-            throw new UnauthorizedException("Role "+role+" is missing from token");
-    }*/
+        //logger.warn("User '" + username + "' was refused to account '" + operatedAccount.getSid() + "'");
+        return AuthOutcome.FAILED;
+    }
 
     protected AccessToken getKeycloakAccessToken() {
-        KeycloakSecurityContext session = (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
-        AccessToken accessToken = session.getToken();
-        return accessToken;
+        AccessToken token = identityContext.getOauthToken();
+        return token;
     }
 
 }
---
+++ restcomm/restcomm.identity/src/main/java/org/mobicents/servlet/restcomm/identity/IdentityContext.java
@@ -0,0 +1,97 @@
+package org.mobicents.servlet.restcomm.identity;
+
+import java.nio.charset.Charset;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
+import org.keycloak.RSATokenVerifier;
+import org.keycloak.VerificationException;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.representations.AccessToken;
+import org.mobicents.servlet.restcomm.dao.AccountsDao;
+import org.mobicents.servlet.restcomm.entities.Account;
+import org.mobicents.servlet.restcomm.entities.Sid;
+import org.mobicents.servlet.restcomm.identity.configuration.IdentityConfigurator;
+
+/**
+ * A per-request security context providing access to Oauth tokens or Account API Keys.
+ * @author "Tsakiridis Orestis"
+ *
+ */
+public class IdentityContext {
+    private static Logger logger = Logger.getLogger(IdentityContext.class);
+
+    AccessToken oauthToken;
+    AccountKey accountKey;
+    Account effectiveAccount; // if oauthToken is set get the account that maps to it. Otherwise use account from accountKey
+
+    public IdentityContext(IdentityConfigurator configurator, HttpServletRequest request, AccountsDao accountsDao) {
+        this.oauthToken = extractOauthToken(request, configurator);
+        this.accountKey = extractAccountKey(request, accountsDao);
+        updateEffectiveAccount(accountsDao);
+    }
+
+    private void updateEffectiveAccount(AccountsDao dao) {
+        if (oauthToken != null) {
+            effectiveAccount = dao.getAccount(oauthToken.getPreferredUsername());
+        } else
+        if (accountKey != null) {
+            effectiveAccount = accountKey.getAccount();
+        }
+    }
+
+    private AccessToken extractOauthToken(HttpServletRequest request, IdentityConfigurator configurator) {
+        KeycloakDeployment deployment = configurator.getDeployment();
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null) {
+            String[] parts = authHeader.split(" ");
+            if (parts.length >= 2 && parts[0].equals("Bearer")) {
+                String tokenString = parts[1];
+                AccessToken token;
+                try {
+                    token = RSATokenVerifier.verifyToken(tokenString, deployment.getRealmKey(), deployment.getRealmInfoUrl());
+                } catch (VerificationException e) {
+                    logger.error("Cannot verity token.", e);
+                    return null;
+                }
+                return token;
+            }
+        }
+        return null;
+    }
+
+    private AccountKey extractAccountKey(HttpServletRequest request, AccountsDao dao) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null) {
+            String[] parts = authHeader.split(" ");
+            if (parts.length >= 2 && parts[0].equals("Basic")) {
+                String base64Credentials = parts[1].trim();
+                String credentials = new String(Base64.decodeBase64(base64Credentials), Charset.forName("UTF-8"));
+                // credentials = username:password
+                final String[] values = credentials.split(":",2);
+                if (values.length >= 2)
+                    if ( Sid.valid(values[0]) ) {
+                        AccountKey accountKey = new AccountKey(values[0], values[1], dao);
+                        return accountKey;
+                    }
+
+            }
+        }
+        return null;
+    }
+
+    public AccessToken getOauthToken() {
+        return oauthToken;
+    }
+
+    public AccountKey getAccountKey() {
+        return accountKey;
+    }
+
+    public Account getEffectiveAccount() {
+        return effectiveAccount;
+    }
+
+}
