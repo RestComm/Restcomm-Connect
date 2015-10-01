85bfb382908f8aa22554217a5b562634ea2887c4
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-10-01 16:26:11 +0300

    Added 'SslMode' configuration option to RVD. It controls https host verification behaviour. Refers #485. Also made some changes in RvdConfiguration class. Refers #486.

--- restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/security/AuthenticationService.java
+++ restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/security/AuthenticationService.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
-
 import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.log4j.Logger;
 import org.mobicents.servlet.restcomm.rvd.RvdConfiguration;
+import org.mobicents.servlet.restcomm.rvd.commons.http.CustomHttpClientBuilder;
 import org.mobicents.servlet.restcomm.rvd.security.exceptions.RvdSecurityException;
 
 public class AuthenticationService {
@@ -34,7 +34,7 @@ public class AuthenticationService {
         //String restcommAuthUrl = "http://" + restcommIp + ":" + rvdSettings.getEffectiveRestcommPort(request);
         String restcommAuthUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
 
-        CloseableHttpClient client = HttpClients.createDefault();
+        CloseableHttpClient client = CustomHttpClientBuilder.buildHttpClient();
         URI url;
         try {
             URIBuilder uriBuilder = new URIBuilder(restcommAuthUrl);
