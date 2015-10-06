7d8bbaedffdd221a7ea9abe03e12e6e039256235
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-10-06 11:35:33 +0300

    Switched default SslMode configuration option to 'strict'. Also added copyright notices and author comments. Refers #485.

--- restcomm/restcomm.commons/src/main/java/org/mobicents/servlet/restcomm/configuration/sets/MainConfigurationSet.java
+++ restcomm/restcomm.commons/src/main/java/org/mobicents/servlet/restcomm/configuration/sets/MainConfigurationSet.java
@@ -1,3 +1,23 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
 package org.mobicents.servlet.restcomm.configuration.sets;
 
 import org.mobicents.servlet.restcomm.annotations.concurrency.Immutable;
@@ -5,11 +25,21 @@ import org.mobicents.servlet.restcomm.configuration.sources.ConfigurationSource;
 import org.mobicents.servlet.restcomm.http.SslMode;
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * Provides a typed interface to a set of configuration options retrieved from a 
+ * configuration source. 
+ * 
+ * To add a new option in this set define its name as static fields and then initialize,
+ * validate it in the constructor.
+ *  
+ * @author orestis.tsakiridis@telestax.com (Orestis Tsakiridis)
+ *
+ */
 @Immutable
 public class MainConfigurationSet extends ConfigurationSet {
 
     private static final String SSL_MODE_KEY = "http-client.ssl-mode";
-    private static final SslMode SSL_MODE_DEFAULT = SslMode.allowall;
+    private static final SslMode SSL_MODE_DEFAULT = SslMode.strict;
     private final SslMode sslMode;
 
     public MainConfigurationSet(ConfigurationSource source) {
