afcfc46c1e20a20260b185f0338d2c9e3781bf5b
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-10-06 11:35:33 +0300

    Switched default SslMode configuration option to 'strict'. Also added copyright notices and author comments. Refers #485.

--- restcomm/restcomm.commons/src/main/java/org/mobicents/servlet/restcomm/configuration/RestcommConfiguration.java
+++ restcomm/restcomm.commons/src/main/java/org/mobicents/servlet/restcomm/configuration/RestcommConfiguration.java
@@ -1,3 +1,23 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
 package org.mobicents.servlet.restcomm.configuration;
 
 import java.util.Map;
@@ -8,6 +28,13 @@ import org.mobicents.servlet.restcomm.configuration.sets.ConfigurationSet;
 import org.mobicents.servlet.restcomm.configuration.sets.MainConfigurationSet;
 import org.mobicents.servlet.restcomm.configuration.sources.ApacheConfigurationSource;
 
+/**
+ * Singleton like class that provides access to ConfigurationSets.
+ * Use get+() functions to access configuration sets.
+ * 
+ * @author orestis.tsakiridis@telestax.com (Orestis Tsakiridis)
+ *
+ */
 public class RestcommConfiguration {
 
     private final Map<String,ConfigurationSet> sets = new ConcurrentHashMap<String,ConfigurationSet>();
@@ -26,17 +53,19 @@ public class RestcommConfiguration {
         sets.put(setKey, set);
     }
     public <T extends ConfigurationSet> T get(String key, Class <T> type) {
-        return type.cast(sets.get("key"));
+        return type.cast(sets.get(key));
     }
 
     public MainConfigurationSet getMain() {
         return (MainConfigurationSet) sets.get("main");
     }
+    /*
     public void reloadMain() {
         MainConfigurationSet oldMain = getMain();
         MainConfigurationSet newMain = new MainConfigurationSet(oldMain.getSource());
         sets.put("main", newMain);
     }
+    */
 
     // define getters  for additional ConfigurationSets here
     // ...
