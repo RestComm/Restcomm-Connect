aa3f01b915e7ca9646c02fef726db55a69e69b9c
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-10-08 13:08:25 +0300

    Various UI/UX fixes in SSO/AdminUI

---
+++ restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/unlinked.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Controller definition for the '/unlinked' view. It covers the case when a user is granted
+ * access to an instance but is not linked to a Restcomm account.
+ */
+angular.module('rcApp').controller('UnlinkedCtrl', function ($scope,AuthService) {
+});
