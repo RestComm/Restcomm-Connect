d29317b519bdc2be601f29ba6a674a06976fa525
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-11-06 13:09:35 +0200

    Provided unit tests for RestcommIdentityApi class
    Refers #619

---
+++ restcomm/restcomm.identity/src/test/java/org/mobicents/servlet/restcomm/identity/RestcommIdentityApiTest.java
@@ -0,0 +1,39 @@
+package org.mobicents.servlet.restcomm.identity;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mobicents.servlet.restcomm.endpoints.Outcome;
+import org.mobicents.servlet.restcomm.identity.RestcommIdentityApi.RestcommIdentityApiException;
+import org.mobicents.servlet.restcomm.identity.RestcommIdentityApi.UserEntity;
+
+public class RestcommIdentityApiTest {
+
+    static String authServerBaseUrl = "https://identity.restcomm.com";
+    static String username = "test_user";
+    static String password = "password";
+    static String realm = "restcomm";
+
+    static RestcommIdentityApi api;
+
+    public RestcommIdentityApiTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @BeforeClass
+    public static void createInstance() throws RestcommIdentityApiException {
+        api = new RestcommIdentityApi(authServerBaseUrl, username, password, realm);
+        String instanceId = api.createInstance("http://localhost", "my-secret").instanceId;
+        api.bindInstance(instanceId);
+    }
+
+    @Test
+    public void testCreateAndInviteUser() {
+        UserEntity user = new UserEntity("test_invited_user",null, "Test Invited User", null, "invited_password");
+        assertEquals(api.createUser(user), Outcome.OK);
+        assertTrue(api.inviteUser("test_invited_user"));
+        // TODO remove user
+    }
+
+}
