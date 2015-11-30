0052b9aff841fea0a785e526017f43ecb2ab9dc4
hrosa <hdavidrosa@gmail.com>
2015-11-30 18:56:08 +0000

    #682 Added load tests for hello-play, hello-play-one-minute, simple-recording, voicemail and conference applications.

---
+++ load_tests/sipp-3.4/src/prepare_pcap.c
@@ -0,0 +1,201 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Author : Guillaume TEISSIER from FTR&D 02/02/2006
+ */
+#include <pcap.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#if defined(__HPUX) || defined(__CYGWIN) || defined(__FreeBSD__)
+#include <netinet/in_systm.h>
+#endif
+#include <netinet/ip.h>
+#ifndef __CYGWIN
+#include <netinet/ip6.h>
+#endif
+#include <string.h>
+
+#include "prepare_pcap.h"
+#include "screen.hpp"
+
+/* We define our own structures for Ethernet Header and IPv6 Header as they are not available on CYGWIN.
+ * We only need the fields, which are necessary to determine the type of the next header.
+ * we could also define our own structures for UDP and IPv4. We currently use the structures
+ * made available by the platform, as we had no problems to get them on all supported platforms.
+ */
+
+typedef struct _ether_hdr {
+    char ether_dst[6];
+    char ether_src[6];
+    u_int16_t ether_type; /* we only need the type, so we can determine, if the next header is IPv4 or IPv6 */
+} ether_hdr;
+
+typedef struct _ipv6_hdr {
+    char dontcare[6];
+    u_int8_t nxt_header; /* we only need the next header, so we can determine, if the next header is UDP or not */
+    char dontcare2[33];
+} ipv6_hdr;
+
+
+#ifdef __HPUX
+int check(u_int16_t *buffer, int len)
+{
+#else
+inline int check(u_int16_t *buffer, int len)
+{
+#endif
+    int sum;
+    int i;
+    sum = 0;
+
+    for (i=0; i<(len&~1); i+= 2)
+        sum += *buffer++;
+
+    if (len & 1) {
+        sum += htons( (*(const u_int8_t *)buffer) << 8);
+    }
+    return sum;
+}
+
+#ifdef __HPUX
+u_int16_t checksum_carry(int s)
+{
+#else
+inline u_int16_t checksum_carry(int s)
+{
+#endif
+    int s_c = (s >> 16) + (s & 0xffff);
+    return (~(s_c + (s_c >> 16)) & 0xffff);
+}
+
+char errbuf[PCAP_ERRBUF_SIZE];
+
+/* prepare a pcap file
+ */
+int prepare_pkts(char *file, pcap_pkts *pkts)
+{
+    pcap_t *pcap;
+    struct pcap_pkthdr *pkthdr = NULL;
+    u_char *pktdata = NULL;
+    int n_pkts = 0;
+    u_long max_length = 0;
+    u_int16_t base = 0xffff;
+    u_long pktlen;
+    pcap_pkt *pkt_index;
+    ether_hdr *ethhdr;
+    struct iphdr *iphdr;
+    ipv6_hdr *ip6hdr;
+    struct udphdr *udphdr;
+
+    pkts->pkts = NULL;
+
+    pcap = pcap_open_offline(file, errbuf);
+    if (!pcap)
+        ERROR("Can't open PCAP file '%s'", file);
+
+#if HAVE_PCAP_NEXT_EX
+    while (pcap_next_ex (pcap, &pkthdr, (const u_char **) &pktdata) == 1) {
+#else
+#ifdef __HPUX
+    pkthdr = (pcap_pkthdr *) malloc (sizeof (*pkthdr));
+#else
+    pkthdr = malloc (sizeof (*pkthdr));
+#endif
+    if (!pkthdr)
+        ERROR("Can't allocate memory for pcap pkthdr");
+    while ((pktdata = (u_char *) pcap_next (pcap, pkthdr)) != NULL) {
+#endif
+        ethhdr = (ether_hdr *)pktdata;
+        if (ntohs(ethhdr->ether_type) != 0x0800 /* IPv4 */
+                && ntohs(ethhdr->ether_type) != 0x86dd) { /* IPv6 */
+            fprintf(stderr, "Ignoring non IP{4,6} packet!\n");
+            continue;
+        }
+        iphdr = (struct iphdr *)((char *)ethhdr + sizeof(*ethhdr));
+        if (iphdr && iphdr->version == 6) {
+            //ipv6
+            pktlen = (u_long) pkthdr->len - sizeof(*ethhdr) - sizeof(*ip6hdr);
+            ip6hdr = (ipv6_hdr *)(void *) iphdr;
+            if (ip6hdr->nxt_header != IPPROTO_UDP) {
+                fprintf(stderr, "prepare_pcap.c: Ignoring non UDP packet!\n");
+                continue;
+            }
+            udphdr = (struct udphdr *)((char *)ip6hdr + sizeof(*ip6hdr));
+        } else {
+            //ipv4
+            if (iphdr->protocol != IPPROTO_UDP) {
+                fprintf(stderr, "prepare_pcap.c: Ignoring non UDP packet!\n");
+                continue;
+            }
+#if defined(__DARWIN) || defined(__CYGWIN) || defined(__FreeBSD__)
+            udphdr = (struct udphdr *)((char *)iphdr + (iphdr->ihl << 2) + 4);
+            pktlen = (u_long)(ntohs(udphdr->uh_ulen));
+#elif defined ( __HPUX)
+            udphdr = (struct udphdr *)((char *)iphdr + (iphdr->ihl << 2));
+            pktlen = (u_long) pkthdr->len - sizeof(*ethhdr) - sizeof(*iphdr);
+#else
+            udphdr = (struct udphdr *)((char *)iphdr + (iphdr->ihl << 2));
+            pktlen = (u_long)(ntohs(udphdr->len));
+#endif
+        }
+        if (pktlen > PCAP_MAXPACKET) {
+            ERROR("Packet size is too big! Recompile with bigger PCAP_MAXPACKET in prepare_pcap.h");
+        }
+        pkts->pkts = (pcap_pkt *) realloc(pkts->pkts, sizeof(*(pkts->pkts)) * (n_pkts + 1));
+        if (!pkts->pkts)
+            ERROR("Can't re-allocate memory for pcap pkt");
+        pkt_index = pkts->pkts + n_pkts;
+        pkt_index->pktlen = pktlen;
+        pkt_index->ts = pkthdr->ts;
+        pkt_index->data = (unsigned char *) malloc(pktlen);
+        if (!pkt_index->data)
+            ERROR("Can't allocate memory for pcap pkt data");
+        memcpy(pkt_index->data, udphdr, pktlen);
+
+#if defined(__HPUX) || defined(__DARWIN) || (defined __CYGWIN) || defined(__FreeBSD__)
+        udphdr->uh_sum = 0 ;
+#else
+        udphdr->check = 0;
+#endif
+
+        // compute a partial udp checksum
+        // not including port that will be changed
+        // when sending RTP
+#if defined(__HPUX) || defined(__DARWIN) || (defined __CYGWIN) || defined(__FreeBSD__)
+        pkt_index->partial_check = check((u_int16_t *) &udphdr->uh_ulen, pktlen - 4) + ntohs(IPPROTO_UDP + pktlen);
+#else
+        pkt_index->partial_check = check((u_int16_t *) &udphdr->len, pktlen - 4) + ntohs(IPPROTO_UDP + pktlen);
+#endif
+        if (max_length < pktlen)
+            max_length = pktlen;
+#if defined(__HPUX) || defined(__DARWIN) || (defined __CYGWIN) || defined(__FreeBSD__)
+        if (base > ntohs(udphdr->uh_dport))
+            base = ntohs(udphdr->uh_dport);
+#else
+        if (base > ntohs(udphdr->dest))
+            base = ntohs(udphdr->dest);
+#endif
+        n_pkts++;
+    }
+    pkts->max = pkts->pkts + n_pkts;
+    pkts->max_length = max_length;
+    pkts->base = base;
+    fprintf(stderr, "In pcap %s, npkts %d\nmax pkt length %ld\nbase port %d\n", file, n_pkts, max_length, base);
+    pcap_close(pcap);
+
+    return 0;
+}
+
---
+++ load_tests/sipp-3.4/src/watchdog.cpp
@@ -0,0 +1,126 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Author : Richard GAYRAUD - 04 Nov 2003
+ *           Marc LAMBERTON
+ *           Olivier JACQUES
+ *           Herve PELLAN
+ *           David MANSUTTI
+ *           Francois-Xavier Kowalski
+ *           Gerard Lyonnaz
+ *           From Hewlett Packard Company.
+ *           F. Tarek Rogers
+ *           Peter Higginson
+ *           Vincent Luba
+ *           Shriram Natarajan
+ *           Guillaume Teissier from FTR&D
+ *           Clement Chen
+ *           Wolfgang Beck
+ *           Charles P Wright from IBM Research
+ */
+#include "sipp.hpp"
+
+void watchdog::dump()
+{
+    WARNING("Watchdog Task: interval = %d, major_threshold = %d (%d triggers left), minor_threshold = %d (%d triggers left)", interval, major_threshold, major_maxtriggers, minor_threshold, minor_maxtriggers);
+}
+
+watchdog::watchdog(int interval, int reset_interval, int major_threshold, int major_maxtriggers, int minor_threshold, int minor_maxtriggers)
+{
+    this->interval = interval;
+    this->reset_interval = reset_interval;
+    this->major_threshold = major_threshold;
+    this->major_maxtriggers = major_maxtriggers;
+    this->minor_threshold = minor_threshold;
+    this->minor_maxtriggers = minor_maxtriggers;
+    major_triggers = 0;
+    minor_triggers = 0;
+    last_trigger = last_fire = getmilliseconds();
+}
+
+bool watchdog::run()
+{
+  getmilliseconds();
+
+  int expected_major_trigger_time = last_fire + this->major_threshold;
+  int expected_minor_trigger_time = last_fire + this->minor_threshold;
+
+  bool major_watchdog_tripped = clock_tick > expected_major_trigger_time;
+  bool minor_watchdog_tripped = clock_tick > expected_minor_trigger_time;
+
+    // Check if either watchdog has taken longer than expected to run,
+    // and if so, warn that we are overloaded.
+    if (major_watchdog_tripped) {
+        major_triggers++;
+        CStat::globalStat(CStat::E_WATCHDOG_MAJOR);
+        last_trigger = clock_tick;
+        WARNING("Overload warning: the major watchdog timer %dms has been tripped (%d), %d trips remaining.",
+                major_threshold,
+                clock_tick - last_fire,
+                major_maxtriggers - major_triggers);
+    } else if (minor_watchdog_tripped) {
+        minor_triggers++;
+        last_trigger = clock_tick;
+        CStat::globalStat(CStat::E_WATCHDOG_MINOR);
+        WARNING("Overload warning: the minor watchdog timer %dms has been tripped (%d), %d trips remaining.",
+                minor_threshold,
+                clock_tick - last_fire,
+                minor_maxtriggers - minor_triggers);
+    }
+
+    bool major_watchdog_failure = ((this->major_maxtriggers != -1) &&
+                                   (major_triggers > this->major_maxtriggers));
+    bool minor_watchdog_failure = ((this->minor_maxtriggers != -1) &&
+                                   (minor_triggers > this->minor_maxtriggers));
+
+    // If the watchdogs have tripped too many times, end the SIPp run.
+    if (major_watchdog_failure) {
+        ERROR("Overload error: the watchdog timer has tripped the major threshold of %dms too many times (%d out of %d allowed) (%d out of %d minor %dms timeouts tripped)\n",
+              major_threshold,
+              major_triggers,
+              major_maxtriggers,
+              minor_triggers,
+              minor_maxtriggers,
+              minor_threshold);
+    } else if (minor_watchdog_failure) {
+        ERROR("Overload error: the watchdog timer has tripped the minor threshold of %dms too many times (%d out of %d allowed) (%d out of %d major %dms timeouts tripped)\n",
+              minor_threshold,
+              minor_triggers,
+              minor_maxtriggers,
+              major_triggers,
+              major_maxtriggers,
+              major_threshold);
+    }
+
+
+
+    if ((reset_interval > 0) &&
+        (major_triggers || minor_triggers) &&
+        (clock_tick > (last_trigger + reset_interval))) {
+      WARNING("Resetting watchdog timer trigger counts, as it has not been triggered in over %dms.",
+              clock_tick - last_trigger);
+      major_triggers = minor_triggers = 0;
+    }
+
+    last_fire = clock_tick;
+    setPaused(); // Return this task to a paused state
+    return true;
+}
+
+// Returns the clock_tick when this task should next run
+unsigned int watchdog::wake()
+{
+    return last_fire + interval;
+}
