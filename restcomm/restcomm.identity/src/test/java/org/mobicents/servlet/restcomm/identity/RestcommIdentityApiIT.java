933334b6ba20c35a10df13f6ef3bbf0f99f0673c
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2015-12-17 16:19:35 +0200

    Configured basic integration testing using a wildfly-swarm packaged keycloak. Currently experimenting in restcomm.identity module.
    Refers #738

---
+++ restcomm/restcomm.identity/src/test/java/org/mobicents/servlet/restcomm/identity/RestcommIdentityApiIT.java
@@ -0,0 +1,19 @@
+package org.mobicents.servlet.restcomm.identity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nando on 12/17/15.
+ */
+
+public class RestcommIdentityApiIT {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+
+}
+
