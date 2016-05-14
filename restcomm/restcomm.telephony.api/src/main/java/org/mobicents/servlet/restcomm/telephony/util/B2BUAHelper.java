009f3f6f7c2eb0328185bce223be17ebf8f096cf
George <gvagenas@gmail.com>
2016-05-14 21:57:23 +0300

    Work in progress for issue #1068

--- restcomm/restcomm.telephony.api/src/main/java/org/mobicents/servlet/restcomm/telephony/util/B2BUAHelper.java
+++ restcomm/restcomm.telephony.api/src/main/java/org/mobicents/servlet/restcomm/telephony/util/B2BUAHelper.java
@@ -109,7 +109,7 @@
                  // create and send the outgoing invite and do the session linking
                  incomingSession.setAttribute(B2BUA_LAST_REQUEST, request);
                  SipServletRequest outRequest = sipFactory.createRequest(request.getApplicationSession(), request.getMethod(),
-                         request.getFrom().getURI(), request.getTo().getURI());
+                         from, to);
                  outRequest.setRequestURI(to);
 
                  if (request.getContent() != null) {
