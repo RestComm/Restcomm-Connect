86765a98f446bc80d585e65b814abda3b4c9ffb8
Maria Farooq <maria.farooq@seamless.se>
2016-05-14 23:54:51 +0500

    updated conference state in case of conference state changed to COMPLETED

--- restcomm/restcomm.interpreter/src/main/java/org/mobicents/servlet/restcomm/interpreter/VoiceInterpreter.java
+++ restcomm/restcomm.interpreter/src/main/java/org/mobicents/servlet/restcomm/interpreter/VoiceInterpreter.java
@@ -689,6 +689,12 @@ public final class VoiceInterpreter extends BaseVoiceInterpreter {
                     conferenceState = event.state();
                     conferenceStateModeratorPresent(message);
                     break;
+                case COMPLETED:
+                    conferenceState = event.state();
+                    // At this point i think we should call finishConferencing,
+                    // who will tell conference center to destroy the conference.
+                    // fsm.transition(message, finishConferencing);
+                    break;
                 default:
                     break;
             }
