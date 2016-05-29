cafa9af26dcb28e6cb37af2054e4353725144c1c
George Vagenas <gvagenas@gmail.com>
2016-05-29 21:10:17 +0300

    Add GC class histogram before and after GC.run

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/collect_jmap.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/collect_jmap.sh
@@ -41,8 +41,11 @@ fi
 if [[ -z "$RESTCOMM_PID" ]]; then
     echo "Please make sure that RestComm is running..."
  else
+    jcmd $RESTCOMM_PID GC.class_histogram  | grep org.mobicents.servlet.restcomm > restcomm_before_GC
     jcmd $RESTCOMM_PID GC.run
-    sleep 2
+    sleep 5
+    jcmd $RESTCOMM_PID GC.class_histogram  | grep org.mobicents.servlet.restcomm > restcomm_after_GC
+    echo "GC ran"
     jmap -dump:format=b,file=restcomm_jmap_$DATE.bin $RESTCOMM_PID
     mv restcomm_jmap_$DATE.bin $JMAP_DIR
  fi
