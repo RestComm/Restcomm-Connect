a370cddf056d6bf9ba9e6bb352c5fa815da1e371
George Vagenas <gvagenas@gmail.com>
2016-05-30 11:08:06 +0300

    Changed fodler of the restcomm_mem file

--- load_tests/collect_jmap.sh
+++ load_tests/collect_jmap.sh
@@ -9,7 +9,7 @@
 DATE=$(date +%F_%H_%M)
 DIR_NAME=restcomm_$DATE
 c=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-JMAP_DIR=$BASEDIR/$DIR_NAME
+JMAP_DIR=$BASEDIR/results
 
 JMAP="true"
 DTAR="true"
@@ -41,23 +41,23 @@ fi
 if [[ -z "$RESTCOMM_PID" ]]; then
     echo "Please make sure that RestComm is running..."
  else
-    echo "****************************************************************" > $BASEDIR/restcomm_mem
-    echo "GC Histogram before GC.run" >> $BASEDIR/restcomm_mem
-    echo "****************************************************************" >> $BASEDIR/restcomm_mem
-    jcmd $RESTCOMM_PID GC.class_histogram  | grep org.mobicents.servlet.restcomm >> $BASEDIR/restcomm_mem
+    echo "****************************************************************" > $JMAP_DIR/restcomm_mem
+    echo "GC Histogram before GC.run" >> $JMAP_DIR/restcomm_mem
+    echo "****************************************************************" >> $JMAP_DIR/restcomm_mem
+    jcmd $RESTCOMM_PID GC.class_histogram  | grep org.mobicents.servlet.restcomm >> $JMAP_DIR/restcomm_mem
 
     jcmd $RESTCOMM_PID GC.run
     sleep 5
 
-    echo "****************************************************************" >> $BASEDIR/restcomm_mem
-    echo "GC Histogram after GC.run" >> $BASEDIR/restcomm_mem
-    echo "****************************************************************" >> $BASEDIR/restcomm_mem
-    jcmd $RESTCOMM_PID GC.class_histogram  | grep org.mobicents.servlet.restcomm >> $BASEDIR/restcomm_mem
+    echo "****************************************************************" >> $JMAP_DIR/restcomm_mem
+    echo "GC Histogram after GC.run" >> $JMAP_DIR/restcomm_mem
+    echo "****************************************************************" >> $JMAP_DIR/restcomm_mem
+    jcmd $RESTCOMM_PID GC.class_histogram  | grep org.mobicents.servlet.restcomm >> $JMAP_DIR/restcomm_mem
 
-    echo "****************************************************************" >> $BASEDIR/restcomm_mem
-    echo "JVMTop" >> $BASEDIR/restcomm_mem
-    echo "****************************************************************" >> $BASEDIR/restcomm_mem
-    ./jvmtop.sh -n 1 >> $BASEDIR/restcomm_mem
+    echo "****************************************************************" >> $JMAP_DIR/restcomm_mem
+    echo "JVMTop" >> $JMAP_DIR/restcomm_mem
+    echo "****************************************************************" >> $JMAP_DIR/restcomm_mem
+    ./jvmtop.sh -n 1 >> $JMAP_DIR/restcomm_mem
 
     jmap -dump:format=b,file=restcomm_jmap_$DATE.bin $RESTCOMM_PID
     mv restcomm_jmap_$DATE.bin $JMAP_DIR
