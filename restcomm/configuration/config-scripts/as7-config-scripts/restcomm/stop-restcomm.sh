d2e71cc48402227385c0047d28d34839048f3193
lefty <eleftherios.banos@telestax.com>
2016-06-27 18:46:48 +0300

    Fix stop-script.sh for OSx

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/stop-restcomm.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/stop-restcomm.sh
@@ -6,6 +6,36 @@
 
 echo 'shutting down telscale restcomm...'
 
+
+getPIDKill(){
+   RESTCOMM_PID=" "
+   RMS_PID=""
+
+   RESTCOMM_PID=$(jps | grep jboss-modules.jar | cut -d " " -f 1)
+
+   while read -r line
+   do
+    if  ps -ef | grep $line | grep -q  mediaserver
+    then
+          RMS_PID=$line
+   fi
+   done < <(jps | grep Main | cut -d " " -f 1)
+
+   if [ -n "$RESTCOMM_PID" ]; then
+        echo "RESTCOMM_PID: $RESTCOMM_PID"
+        kill -9 $RESTCOMM_PID
+   fi
+
+    if [ -n "$RMS_PID" ]; then
+        echo "RMS_PID: $RMS_PID"
+        kill -9 $RMS_PID
+   fi
+}
+
+#Main
+#Kill RMS and RC
+getPIDKill
+
 # stop Media Server if necessary
 if screen -ls | grep -q 'mms'; then
 	screen -S 'mms' -p 0 -X 'quit'
