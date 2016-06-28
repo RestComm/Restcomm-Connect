f0e1959e17814351db35235c98d56218f0a3c2fe
lefty <eleftherios.banos@telestax.com>
2016-06-28 16:16:05 +0300

    Final Fixes (comments, MGMT fix,etc ).

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/start-restcomm.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/start-restcomm.sh
@@ -14,12 +14,20 @@
 startRestcomm() {
 	run_mode="$1"
 	bind_address="$2"
+	ExtraOpts="-Djboss.bind.address.management=127.0.0.1"
 
 	# Check if RestComm is already running
 	if screen -list | grep -q 'restcomm'; then
 		echo 'TelScale RestComm is already running on screen session "restcomm"!'
 		exit 1;
 	fi
+
+    if [ -n "$MGMT_PASS" ] && [ -n "$MGMT_USER" ]; then
+	    echo "MGMT_PASS, MGMT_USER is set will be added to MGMNT configuration"
+        grep -q "$MGMT_USER" $RESTCOMM_HOME/standalone/configuration/mgmt-users.properties || $RESTCOMM_HOME/bin/add-user.sh "$MGMT_USER" "$MGMT_PASS" -s
+        #Management bind address
+        ExtraOpts='-Djboss.bind.address.management=$bind_address'
+    fi
 
 	case $run_mode in
 		'standalone'*)
@@ -28,22 +36,22 @@ startRestcomm() {
 			echo 'TelScale RestComm started running on standalone mode. Screen session: restcomm.'
 			echo "Using IP Address: $BIND_ADDRESS"
 			if [[ "$RUN_DOCKER" == "true" || "$RUN_DOCKER" == "TRUE" ]]; then
-				$RESTCOMM_HOME/bin/standalone.sh -b $bind_address
+				$RESTCOMM_HOME/bin/standalone.sh -b $bind_address "${ExtraOpts}"
 			else
-				screen -dmS 'restcomm' $RESTCOMM_HOME/bin/standalone.sh -b $bind_address
+				screen -dmS 'restcomm' $RESTCOMM_HOME/bin/standalone.sh -b $bind_address "${ExtraOpts}"
 			fi
 			;;
 		'domain'*)
 			# start restcomm on standalone mode
 			chmod +x $RESTCOMM_HOME/bin/domain.sh
-			screen -dmS 'restcomm' $RESTCOMM_HOME/bin/domain.sh -b $bind_address
+			screen -dmS 'restcomm' $RESTCOMM_HOME/bin/domain.sh -b $bind_address "${ExtraOpts}"
 			echo 'TelScale RestComm started running on domain mode. Screen session: restcomm.'
 			echo "Using IP Address: $BIND_ADDRESS"
 			;;
 		*)
 			# start restcomm on standalone mode
 			chmod +x $RESTCOMM_HOME/bin/standalone.sh
-			screen -dmS 'restcomm' $RESTCOMM_HOME/bin/standalone.sh -b $bind_address
+			screen -dmS 'restcomm' $RESTCOMM_HOME/bin/standalone.sh -b $bind_address "${ExtraOpts}"
 			echo 'TelScale RestComm started running on standalone mode. Screen session: restcomm.'
 			echo "Using IP Address: $BIND_ADDRESS"
 			;;
