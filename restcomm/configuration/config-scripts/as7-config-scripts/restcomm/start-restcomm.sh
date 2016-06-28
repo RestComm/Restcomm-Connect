a73072cb913359d8e983d98a6a1ab9514b8b543d
lefty <eleftherios.banos@telestax.com>
2016-06-28 16:27:05 +0300

    Final Fixes.

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/start-restcomm.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/start-restcomm.sh
@@ -26,7 +26,7 @@ startRestcomm() {
 	    echo "MGMT_PASS, MGMT_USER is set will be added to MGMNT configuration"
         grep -q "$MGMT_USER" $RESTCOMM_HOME/standalone/configuration/mgmt-users.properties || $RESTCOMM_HOME/bin/add-user.sh "$MGMT_USER" "$MGMT_PASS" -s
         #Management bind address
-        ExtraOpts='-Djboss.bind.address.management=$bind_address'
+        ExtraOpts="-Djboss.bind.address.management=$bind_address"
     fi
 
 	case $run_mode in
