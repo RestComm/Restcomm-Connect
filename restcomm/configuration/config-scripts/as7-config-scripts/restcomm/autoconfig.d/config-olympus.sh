64b56b28ddb936dd3267ff0bde80a372ac53576c
lefty <eleftherios.banos@telestax.com>
2016-06-30 14:43:46 +0300

    Fix port for olympus login page  #1242

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-olympus.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-olympus.sh
@@ -19,17 +19,19 @@ FILE=$BASEDIR/standalone/deployments/olympus.war/resources/js/controllers/regist
 
  #Check for Por Offset
     if (( $PORT_OFFSET > 0 )); then
-		local SIP_PORT_WS=$((SIP_PORT_WS + PORT_OFFSET))
-		local SIP_PORT_WSS=$((SIP_PORT_WSS + PORT_OFFSET))
+      local SIP_PORT_WS=$((SIP_PORT_WS + PORT_OFFSET))
+      local SIP_PORT_WSS=$((SIP_PORT_WSS + PORT_OFFSET))
 
-		#change port due to Port-Offset
-		sed -i "s|5082|${SIP_PORT_WS}|" $FILE
-	fi
+      #change port due to Port-Offset
+      sed -i "s|\$scope.serverAddress + ':[0-9][0-9]*'|\$scope.serverAddress + ':${SIP_PORT_WS}'|" $FILE
+      sed -i "s|\$scope.serverPort = '[0-9][0-9]*';|\$scope.serverPort = '${SIP_PORT_WS}';|" $FILE
+   fi
 
-    if [ -n "$SECURESSL" ]; then
-        sed -i "s|ws:|wss:|" $FILE
-        sed -i "s|${SIP_PORT_WS}|${SIP_PORT_WSS}|" $FILE
-    fi
+   if [ -n "$SECURESSL" ]; then
+       sed -i "s|ws:|wss:|" $FILE
+       sed -i "s|\$scope.serverAddress + ':[0-9][0-9]*'|\$scope.serverAddress + ':${SIP_PORT_WSS}'|" $FILE
+       sed -i "s|\$scope.serverPort = '[0-9][0-9]*';|\$scope.serverPort = '${SIP_PORT_WSS}';|" $FILE
+   fi
 }
 
 
