2f9b4d29315f67cd54cc277e2699bed87e60f6ea
Orestis <orestis.tsakiridis@telestax.com>
2016-09-13 15:53:45 +0300

    Rewrote AccountDao unit test using temporary storage directory.
    Refers #1349

--- restcomm/restcomm.dao/src/test/java/org/mobicents/servlet/restcomm/dao/mybatis/AccountsDaoTest.java
+++ restcomm/restcomm.dao/src/test/java/org/mobicents/servlet/restcomm/dao/mybatis/AccountsDaoTest.java
@@ -1,14 +1,20 @@
 package org.mobicents.servlet.restcomm.dao.mybatis;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URL;
 
+import junit.framework.Assert;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mobicents.servlet.restcomm.dao.AccountsDao;
+import org.mobicents.servlet.restcomm.entities.Account;
+import org.mobicents.servlet.restcomm.entities.Sid;
 
-public class AccountsDaoTest {
+public class AccountsDaoTest extends DaoTest {
     private static MybatisDaoManager manager;
 
     public AccountsDaoTest() {
@@ -17,7 +23,12 @@ public class AccountsDaoTest {
 
     @Before
     public void before() throws Exception {
-        final InputStream data = getClass().getResourceAsStream("/mybatis.xml");
+        sandboxRoot = createTempDir("accountsTest");
+        String mybatisFilesPath = getClass().getResource("/accountsDao").getFile();
+        setupSandbox(mybatisFilesPath, sandboxRoot);
+
+        String mybatisXmlPath = sandboxRoot.getPath() + "/mybatis_updated.xml";
+        final InputStream data = new FileInputStream(mybatisXmlPath);
         final SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         final SqlSessionFactory factory = builder.build(data);
         manager = new MybatisDaoManager();
@@ -27,10 +38,14 @@ public class AccountsDaoTest {
     @After
     public void after() throws Exception {
         manager.shutdown();
+        removeTempDir(sandboxRoot.getAbsolutePath());
     }
 
     @Test
-    public void createReadUpdateDelete() {
-
+    public void readAccount() {
+        AccountsDao dao = manager.getAccountsDao();
+        Account account = dao.getAccount(new Sid("AC00000000000000000000000000000000"));
+        Assert.assertNotNull("Account not found",account);
     }
+
 }
