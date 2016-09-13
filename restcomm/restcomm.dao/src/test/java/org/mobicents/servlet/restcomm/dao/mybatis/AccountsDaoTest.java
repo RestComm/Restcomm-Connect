8858b43efa3be4dd4da500525bd51a0c2df45b96
Orestis <orestis.tsakiridis@telestax.com>
2016-09-13 18:25:23 +0300

    Algorithm for recursive sub-account retrieval and tests. - Implemented algorithm in MybatisAccountsDao. - Provided tests. - Added sample data for testing.
    Refers #1349

--- restcomm/restcomm.dao/src/test/java/org/mobicents/servlet/restcomm/dao/mybatis/AccountsDaoTest.java
+++ restcomm/restcomm.dao/src/test/java/org/mobicents/servlet/restcomm/dao/mybatis/AccountsDaoTest.java
@@ -3,6 +3,7 @@ package org.mobicents.servlet.restcomm.dao.mybatis;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 
 import junit.framework.Assert;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -46,6 +47,32 @@ public class AccountsDaoTest extends DaoTest {
         AccountsDao dao = manager.getAccountsDao();
         Account account = dao.getAccount(new Sid("AC00000000000000000000000000000000"));
         Assert.assertNotNull("Account not found",account);
+    }
+
+    @Test
+    public void nestedSubAccountRetrieval() {
+        // retrieve all sub-accounts of AC00000000000000000000000000000000
+        AccountsDao dao = manager.getAccountsDao();
+        Sid parentSid = new Sid("AC00000000000000000000000000000000");
+        List<String> sidList = dao.getSubAccountSidsRecursive(parentSid);
+        Assert.assertEquals("Invalid number of subaccounts returned",5, sidList.size());
+        // a parent with no sub-accounts should get zero
+        parentSid = new Sid("AC99999999999999999999999999999999");
+        sidList = dao.getSubAccountSidsRecursive(parentSid);
+        Assert.assertEquals("No sub-account sids should be returned", 0, sidList.size());
+        // check 2nd-level parents too
+        parentSid = new Sid("AC10000000000000000000000000000000");
+        sidList = dao.getSubAccountSidsRecursive(parentSid);
+        Assert.assertEquals("Invalid number of sub-account for 2nd level perent", 3, sidList.size());
+        // check third-level perent too
+        parentSid = new Sid("AC11000000000000000000000000000000");
+        sidList = dao.getSubAccountSidsRecursive(parentSid);
+        Assert.assertEquals("Invalid number of sub-account for 3rd level perent", 1, sidList.size());
+        // test behaviour for non-existing parents. An empty list should be returned
+        parentSid = new Sid("AC59494830204948392023934839392092"); // this does not exist
+        sidList = dao.getSubAccountSidsRecursive(parentSid);
+        Assert.assertEquals("Invalid number of sub-account for 3rd level perent", 0, sidList.size());
+
     }
 
 }
