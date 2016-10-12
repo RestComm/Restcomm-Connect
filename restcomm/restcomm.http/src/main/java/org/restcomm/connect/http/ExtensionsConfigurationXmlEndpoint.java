569b8a53bbe247019d9983698517f4cfacde0827
George Vagenas <gvagenas@gmail.com>
2016-10-12 17:45:06 +0300

    REST API for Extensions configuration, work in progress This refers to #1475

---
+++ restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/ExtensionsConfigurationXmlEndpoint.java
@@ -0,0 +1,34 @@
+/*
+ *  TeleStax, Open Source Cloud Communications
+ *  Copyright 2011-2016, Telestax Inc and individual contributors
+ *  by the @authors tag.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation; either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package org.restcomm.connect.http;
+
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+import org.apache.commons.configuration.Configuration;
+import org.restcomm.connect.dao.DaoManager;
+import org.restcomm.connect.dao.ExtensionsConfigurationDao;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by gvagenas on 12/10/2016.
+ */
+public class ExtensionsConfigurationXmlEndpoint {
+}
