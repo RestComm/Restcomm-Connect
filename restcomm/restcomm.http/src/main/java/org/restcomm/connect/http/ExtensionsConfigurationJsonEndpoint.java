abff6ff6c70dbf6c19699eb503ff57c73674a31f
George Vagenas <gvagenas@gmail.com>
2016-10-12 20:30:12 +0300

    REST API for Extensions configuration, work in progress This refers to #1475

--- restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/ExtensionsConfigurationJsonEndpoint.java
+++ restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/ExtensionsConfigurationJsonEndpoint.java
@@ -25,7 +25,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
