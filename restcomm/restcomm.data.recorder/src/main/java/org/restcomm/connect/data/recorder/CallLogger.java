5a81385064e01b3c5988296b71b3fb269ead71fc
Maria Farooq <maria-farooq@live.com>
2016-10-18 19:56:39 +0300

    created 2 modules for data recorder

--- restcomm/restcomm.data.recorder/src/main/java/org/restcomm/connect/data/recorder/CallLogger.java
+++ restcomm/restcomm.data.recorder/src/main/java/org/restcomm/connect/data/recorder/CallLogger.java
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
-package org.restcomm.connect.dao.logger;
+package org.restcomm.connect.data.recorder;
 
 import java.util.ArrayList;
 import java.util.Collections;
