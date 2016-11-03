23664b84f0198dc1a58bc49375ea6dcb23ddd3fd
hrosa <hdavidrosa@gmail.com>
2016-11-03 13:59:11 +0000

    #1526 Created scripts to start and stop Media Server.
    Created script to verify dependencies of auto-configuration mechanism.

---
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/stop-mediaserver.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+## Description: Stops Media Server running in a terminal session.
+## Author     : Henrique Rosa (henrique.rosa@telestax.com)
+
+local MS_HOME=$RESTCOMM_HOME/mediaserver
+
+$MS_HOME/stop-mediaserver.sh
