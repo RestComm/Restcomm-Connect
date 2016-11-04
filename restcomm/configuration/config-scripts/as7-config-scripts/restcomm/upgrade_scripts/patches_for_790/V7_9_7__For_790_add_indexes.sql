c7893e26574f0c15a994fb0f4b8761b9769bdbfb
Maria Farooq <maria-farooq@live.com>
2016-11-04 14:45:51 +0500

    safely created index only if they does not exists

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/upgrade_scripts/patches_for_790/V7_9_7__For_790_add_indexes.sql
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/upgrade_scripts/patches_for_790/V7_9_7__For_790_add_indexes.sql
@@ -4,9 +4,25 @@
 -- #To run the script use mysql client:
 -- #mysql -u yourusername -p yourpassword yourdatabase < sql_update_script.sql
 
-USE ${RESTCOMM_DBNAME};
-/* Create index on restcomm_call_detail_records on conference_sid column */
-CREATE INDEX idx_cdr_conference_sid ON restcomm_call_detail_records (conference_sid);
+use ${RESTCOMM_DBNAME};
+DELIMITER //
 
-/* Create index on restcomm_conference_detail_records on status column */
-CREATE INDEX idx_cdr_conference_status ON restcomm_conference_detail_records (status);
+DROP PROCEDURE IF EXISTS create_index_on_cdr_conference;
+CREATE PROCEDURE create_index_on_cdr_conference()
+BEGIN
+ IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = 'restcomm_call_detail_records' AND index_name = 'idx_cdr_conference_sid') = 0) 
+ THEN
+   CREATE INDEX idx_cdr_conference_sid ON restcomm_call_detail_records (conference_sid);
+ END IF;
+ 
+IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = 'restcomm_conference_detail_records' AND index_name = 'idx_cdr_conference_status') = 0) 
+ THEN
+   CREATE INDEX idx_cdr_conference_status ON restcomm_conference_detail_records (status);
+ END IF;
+ 
+END //
+
+DELIMITER ;
+CALL create_index_on_cdr_conference();
+drop procedure create_index_on_cdr_conference;
+
