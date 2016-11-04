58805155cb50ee3df3d5e0bbbe94c7be9f8ab0ea
hrosa <hdavidrosa@gmail.com>
2016-11-04 19:12:47 +0000

    #1526 Patched start-mediaserver.sh script to bootstrap media server correctly.
    Updated file permissions to make auto-config scripts configurable.

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/start-mediaserver.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/start-mediaserver.sh
@@ -3,8 +3,12 @@
 ## Description: Starts Media Server with auto-configuration.
 ## Author     : Henrique Rosa (henrique.rosa@telestax.com)
 
-local BASEDIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-local MS_CONF=$BASEDIR/mediaserver.conf
-local MS_HOME=$RESTCOMM_HOME/mediaserver
+startMediaServer() {
+    local basedir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+    local ms_conf=$basedir/mediaserver.conf
+    local ms_home=$RESTCOMM_HOME/mediaserver
 
-$MS_HOME/start-mediaserver.sh $MS_CONF
+    $ms_home/start-mediaserver.sh $ms_conf
+}
+
+startMediaServer
