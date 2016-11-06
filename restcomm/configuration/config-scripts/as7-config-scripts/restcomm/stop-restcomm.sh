a813a952fd4185d4d56457c9adf9847f05c50f55
hrosa <hdavidrosa@gmail.com>
2016-11-06 13:06:50 +0000

    #1526 stop-restcomm script now runs inside functions so that variables are declared locally so they dont affect execution of start-restcomm script.

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/stop-restcomm.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/stop-restcomm.sh
@@ -4,15 +4,20 @@
 ## Authors    : Henrique Rosa (henrique.rosa@telestax.com)
 ##
 
-# Stop Media Server
-local basedir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-$basedir/stop-mediaserver.sh
+stopMediaServer() {
+    local basedir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+    $basedir/stop-mediaserver.sh
+}
 
-# Stop RestComm
-echo 'Shutting down RestComm...'
-if tmux ls | grep -q 'restcomm'; then
-	tmux kill-session -t 'restcomm'
-	echo '...stopped RestComm instance running on terminal session "restcomm"!'
-else
-	echo '...restComm already stopped!'
-fi
+stopRestComm() {
+    echo 'Shutting down RestComm...'
+    if tmux ls | grep -q 'restcomm'; then
+        tmux kill-session -t restcomm
+        echo '...stopped RestComm instance running on terminal session "restcomm"!'
+    else
+        echo '...restComm already stopped!'
+    fi
+}
+
+stopMediaServer
+stopRestComm
