09eb12eb225230c0613b3695ce0dce6d30649d9a
Orestis <orestis.tsakiridis@telestax.com>
2016-11-21 12:23:38 +0200

    Mysql upgrade script that changes password size to 60
    Refers #1497

---
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/upgrade_scripts/patches_for_790/V7_9_11__For_790_increase_password_column_size_in_Accounts_Table.sql
@@ -0,0 +1,24 @@
+-- SQL Script for MySQL/MariaDB to update DB with the schema changes for issue #5
+-- #Author: Orestis Tsakiridis
+
+-- #To run the script use mysql client:
+-- #mysql -u yourusername -p yourpassword yourdatabase < sql_update_script.sql
+
+USE ${RESTCOMM_DBNAME};
+DELIMITER //
+CREATE PROCEDURE updateProcedure()
+ BEGIN
+ SELECT DISTINCTROW IFNULL(character_maximum_length, '') INTO @colSize7911
+ FROM information_schema.columns
+ WHERE table_schema='${RESTCOMM_DBNAME}'
+ AND table_name = 'restcomm_accounts'
+ AND column_name = 'password';
+
+IF @colSize7911 = 32 THEN
+ALTER TABLE restcomm_accounts CHANGE column password password varchar(60) NOT NULL;
+END IF;
+END //
+
+DELIMITER ;
+CALL updateProcedure();
+drop procedure updateProcedure;
