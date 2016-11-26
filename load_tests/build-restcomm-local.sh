35422d36e8ce036820a1d10f0fb6f2713c333201
George Vagenas <gvagenas@gmail.com>
2016-11-26 21:21:41 +0200

    Fixes for local build script

--- load_tests/build-restcomm-local.sh
+++ load_tests/build-restcomm-local.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 3 ]; then
     echo "No arguments provided. Usage: "
-    echo "build-restcomm-local.sh BRANCH_NAME WORKSPACE MAJOR_VERSION_NUMBER"
+    echo "build-restcomm-local.sh BRANCH_NAME BUILD_WORKSPACE MAJOR_VERSION_NUMBER"
     exit 1
 fi
 
@@ -10,7 +10,7 @@ CURRENT_DIR=`pwd`
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 RESTCOMM_BRANCH=$1
-WORKSPACE=$2
+BUILD_WORKSPACE=$2
 MAJOR_VERSION_NUMBER=$3
 
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m"
@@ -21,16 +21,16 @@ echo "RESTCOMM BRANCH: $RESTCOMM_BRANCH"
 echo "RUN TESTSUITE: $RUN_TESTSUITE"
 #export ANT_HOME=/opt/ant/apache-ant-1.8.3
 
-cp -ar ../* $WORKSPACE
-cd $WORKSPACE
+cp -ar ../* $BUILD_WORKSPACE
+cd $BUILD_WORKSPACE
 
 rm Mobicents-Restcomm*.zip
 rm dependencies -rf
 
-export DEPENDENCIES_HOME=$WORKSPACE/dependencies
+export DEPENDENCIES_HOME=$BUILD_WORKSPACE/dependencies
 mkdir -p $DEPENDENCIES_HOME
-export RESTCOMM_HOME=$WORKSPACE
-export RELEASE=$WORKSPACE/release
+export RESTCOMM_HOME=$BUILD_WORKSPACE
+export BUILD_RELEASE=$BUILD_WORKSPACE/release
 cd $RESTCOMM_HOME
 # git checkout -b restcomm-release-$MAJOR_VERSION_NUMBER.$BUILD_NUMBER
 # git rev-parse HEAD > git-info-restcomm.txt
@@ -40,12 +40,12 @@ echo "Workign directory: " `pwd`
 mvn versions:set -DnewVersion=$MAJOR_VERSION_NUMBER.$BUILD_NUMBER
 #git commit -a -m "New release candidate $MAJOR_VERSION_NUMBER.$BUILD_NUMBER"
 
-cd $RELEASE
+cd $BUILD_RELEASE
 FILE=$RESTCOMM_HOME/restcomm/configuration/mss-sip-stack.properties
 sed -e "s|MAJOR_VERSION_NUMBER.BUILD_NUMBER|$MAJOR_VERSION_NUMBER.$BUILD_NUMBER|g" $FILE > $FILE.bak
 mv $FILE.bak $FILE
 ant release -f $RESTCOMM_HOME/release/build.xml -Drestcomm.release.version=$MAJOR_VERSION_NUMBER.$BUILD_NUMBER -Drestcomm.branch.name=restcomm-release-$MAJOR_VERSION_NUMBER.$BUILD_NUMBER -Dcheckout.restcomm.dir=$RESTCOMM_HOME -Dworkspace.restcomm.dir=$RESTCOMM_HOME/restcomm -Dcheckout.dir=$DEPENDENCIES_HOME
-mv $RELEASE/Restcomm-*.zip $WORKSPACE/Restcomm-JBoss-AS7.zip
+mv $BUILD_RELEASE/Restcomm-*.zip $BUILD_WORKSPACE/Restcomm-JBoss-AS7.zip
 
 cd $RESTCOMM_HOME/restcomm
 #commenting the deploy command as it eats up storage on artifactory
@@ -58,7 +58,7 @@ else
 echo "Will not run test suite because variable is $RUN_TESTSUITE"
 fi
 
-echo "$MAJOR_VERSION_NUMBER.$BUILD_NUMBER" > $WORKSPACE/restcomm-version.txt
-ls -la $WORKSPACE/*.zip
-md5sum $WORKSPACE/*.zip
-sha1sum $WORKSPACE/*.zip
+echo "$MAJOR_VERSION_NUMBER.$BUILD_NUMBER" > $BUILD_WORKSPACE/restcomm-version.txt
+ls -la $BUILD_WORKSPACE/*.zip
+md5sum $BUILD_WORKSPACE/*.zip
+sha1sum $BUILD_WORKSPACE/*.zip
