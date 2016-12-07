7768f03c9e66107ea2a2253d16902b4bff553d44
Orestis <orestis.tsakiridis@telestax.com>
2016-12-07 19:03:19 +0200

    Moved RollingUpgradeStage interface to github repo
    Refers #1496

--- restcomm/restcomm.dao/src/main/java/org/restcomm/connect/dao/mybatis/rolling_upgrades/AccountAccessor.java
+++ restcomm/restcomm.dao/src/main/java/org/restcomm/connect/dao/mybatis/rolling_upgrades/AccountAccessor.java
@@ -1,5 +1,6 @@
 package org.restcomm.connect.dao.mybatis.rolling_upgrades;
 
+import org.restcomm.connect.commons.rollingupgrades.RollingUpgradeStage;
 import org.restcomm.connect.dao.entities.Account;
 
 import java.util.Map;
@@ -9,7 +10,7 @@ import java.util.Map;
  *
  * Created by gvagenas on 29/11/2016.
  */
-public interface AccountAccessor {
+public interface AccountAccessor extends RollingUpgradeStage {
     Map<String, Object> toMap(final Account account);
     Account toAccount(final Map<String, Object> map);
 }
