9e67d84e9a53e4112963e803af515f23ec4a83be
Fernando Mendioroz <fernando.mendioroz@telestax.com>
2017-01-15 17:08:34 -0300

    Restcomm Geolocation API recreated files in sync with master by 2017-01-15 and working with GMLC, stage I. Issue #637

--- restcomm/restcomm.application/src/main/webapp/WEB-INF/scripts/sql/init.sql
+++ restcomm/restcomm.application/src/main/webapp/WEB-INF/scripts/sql/init.sql
@@ -287,6 +287,38 @@ CREATE TABLE "restcomm_gateways" (
 "uri" LONGVARCHAR NOT NULL
 );
 
+CREATE TABLE "restcomm_geolocation"(
+"sid" VARCHAR(34) NOT NULL PRIMARY KEY,
+"date_created" DATETIME NOT NULL,
+"date_updated" DATETIME NOT NULL,
+"date_executed" DATETIME NOT NULL,
+"account_sid" VARCHAR(34) NOT NULL,
+"source" VARCHAR(30),
+"device_identifier" VARCHAR(30) NOT NULL,
+"geolocation_type" VARCHAR(15) NOT NULL,
+"response_status" VARCHAR(30),
+"cell_id" VARCHAR(10),
+"location_area_code" VARCHAR(10),
+"mobile_country_code" INTEGER,
+"mobile_network_code" VARCHAR(3),
+"network_entity_address" BIGINT,
+"age_of_location_info" INTEGER,
+"device_latitude" VARCHAR(15),
+"device_longitude" VARCHAR(15),
+"accuracy" BIGINT,
+"physical_address" VARCHAR(50),
+"internet_address" VARCHAR(50),
+"formatted_address" VARCHAR(200),
+"location_timestamp" DATETIME,
+"event_geofence_latitude" VARCHAR(15),
+"event_geofence_longitude" VARCHAR(15),
+"radius" BIGINT,
+"geolocation_positioning_type" VARCHAR(15),
+"last_geolocation_response" VARCHAR(15),
+"cause" VARCHAR(150),
+"api_version" VARCHAR(10) NOT NULL,
+"uri" LONGVARCHAR NOT NULL);
+
 CREATE TABLE "update_scripts" (
 "script" VARCHAR(255) NOT NULL,
 "date_executed" DATETIME NOT NULL
