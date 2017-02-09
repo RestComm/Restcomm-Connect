056eb88b81c55016a36284ebb710da44ab4310d7
George Vagenas <gvagenas@gmail.com>
2017-02-09 10:57:15 +0200

    Work in progress for properly prepare Amazon S3 public URL for recordings This refer to #1780

--- restcomm/restcomm.dao/src/main/java/org/restcomm/connect/dao/mybatis/MybatisRecordingsDao.java
+++ restcomm/restcomm.dao/src/main/java/org/restcomm/connect/dao/mybatis/MybatisRecordingsDao.java
@@ -64,7 +64,7 @@ public final class MybatisRecordingsDao implements RecordingsDao {
         if (s3AccessTool != null) {
             URI s3Uri = s3AccessTool.uploadFile(recordingPath+"/"+recording.getSid().toString()+".wav");
             if (s3Uri != null) {
-                recording = recording.updateFileUri(s3Uri);
+                recording = recording.setS3Uri(s3Uri);
             }
         } else {
             recording = recording.updateFileUri(generateLocalFileUri("/restcomm/recordings/" + recording.getSid()));
@@ -176,6 +176,11 @@ public final class MybatisRecordingsDao implements RecordingsDao {
         map.put("api_version", recording.getApiVersion());
         map.put("uri", DaoUtils.writeUri(recording.getUri()));
         map.put("file_uri", DaoUtils.writeUri(recording.getFileUri()));
+        if (recording.getS3Uri() != null) {
+            map.put("s3_uri", DaoUtils.writeUri(recording.getS3Uri()));
+        } else {
+            map.put("s3_uri", null);
+        }
         return map;
     }
 
@@ -194,6 +199,7 @@ public final class MybatisRecordingsDao implements RecordingsDao {
         if (fileUri == null || fileUri.isEmpty()) {
             fileUri = generateLocalFileUri("/restcomm/recordings/" + sid).toString();
         }
-        return new Recording(sid, dateCreated, dateUpdated, accountSid, callSid, duration, apiVersion, uri, DaoUtils.readUri(fileUri));
+        String s3Uri = (String) map.get("s3_uri");
+        return new Recording(sid, dateCreated, dateUpdated, accountSid, callSid, duration, apiVersion, uri, DaoUtils.readUri(fileUri), DaoUtils.readUri(s3Uri));
     }
 }
