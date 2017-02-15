04e2bc445c6b353bb79a83326e9e8f757c0b5ea4
Maria Farooq <maria-farooq@live.com>
2017-02-15 20:00:55 +0500

    added component comments

--- restcomm/restcomm.mrb.api/src/main/java/org/restcomm/connect/mrb/api/GetMediaGateway.java
+++ restcomm/restcomm.mrb.api/src/main/java/org/restcomm/connect/mrb/api/GetMediaGateway.java
@@ -33,6 +33,11 @@ public final class GetMediaGateway {
     private final String conferenceName;
     private final String msId;
 
+    /**
+     * @param callSid - restcomm callSid - if MediaGateway is required for a call
+     * @param conferenceName - friendly name of conference - if MediaGateway is required for a conference
+     * @param msId - mediaserver id - provide in case we need MediaGateway of a specific mediaserver in cluster. 
+     */
     public GetMediaGateway(final Sid callSid, final String conferenceName, final String msId) {
         super();
         this.callSid = callSid;
@@ -40,10 +45,16 @@ public final class GetMediaGateway {
         this.msId = msId;
     }
 
+    /**
+     * @param callSid - restcomm callSid - if MediaGateway is required for a call
+     */
     public GetMediaGateway(final Sid callSid){
         this(callSid, null, null);
     }
 
+    /**
+     * @param msId - mediaserver id - provide in case we need MediaGateway of a specific mediaserver in cluster.
+     */
     public GetMediaGateway(final String msId) {
         this(null, null, msId);
     }
