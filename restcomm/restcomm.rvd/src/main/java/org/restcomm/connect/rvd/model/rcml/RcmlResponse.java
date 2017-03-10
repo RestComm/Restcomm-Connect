98e499c904850a0796a788b9df93acd8825711e2
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2017-03-10 12:09:31 +0200

    Removed RVD from main restcomm-connect repo and updated build script.
    Fixes #1819, fixes #1884

--- restcomm/restcomm.rvd/src/main/java/org/restcomm/connect/rvd/model/rcml/RcmlResponse.java
+++
@@ -1,8 +0,0 @@
-package org.restcomm.connect.rvd.model.rcml;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class RcmlResponse {
-    public List<RcmlStep> steps = new ArrayList<RcmlStep>();
-}
--- restcomm/restcomm.rvd/src/main/java/org/restcomm/connect/rvd/model/steps/ussdsay/UssdSayRcml.java
+++
@@ -1,8 +0,0 @@
-package org.restcomm.connect.rvd.model.steps.ussdsay;
-
-import org.restcomm.connect.rvd.model.rcml.RcmlStep;
-
-public class UssdSayRcml extends RcmlStep {
-    String language;
-    String text;
-}
