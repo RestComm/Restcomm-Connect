cc7fe3709c036b6e9675944b93f9f14e414ee6d9
Maria Farooq <maria-farooq@live.com>
2017-03-13 19:18:56 +0500

    added default invalid callSid

--- restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/http/LiveCallModificationTest.java
+++ restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/http/LiveCallModificationTest.java
@@ -633,7 +633,9 @@ public class LiveCallModificationTest {
         System.out.println("\n ******************** \nAbout to redirect the call\n ********************\n");
         rcmlUrl = "http://127.0.0.1:8080/restcomm/dial-client-entry.xml";
 
-        String invalidCallSid = Sid.generate(Sid.Type.CALL).toString();
+        //String invalidCallSid = Sid.generate(Sid.Type.CALL).toString();
+        // restcomm-connect/1907
+        String invalidCallSid = "IDaaaaaaaaaaabbbbbbbbbbbcccccccddd-CAaaaaaabbbbbcccccdddddeeeeeffffgg";
 
         callResult = RestcommCallsTool.getInstance().modifyCall(deploymentUrl.toString(), adminAccountSid, adminAuthToken,
                 invalidCallSid, null, rcmlUrl);
