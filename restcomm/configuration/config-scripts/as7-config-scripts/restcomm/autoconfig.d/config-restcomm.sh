8c87d9dfa3606a53bdb32134fa91fa15fad22e8f
croufay <charles.roufay@telestax.com>
2017-03-21 10:08:25 -0600

    updated script to remove use of host to check entry in /etc/hosts

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-restcomm.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-restcomm.sh
@@ -52,13 +52,10 @@ configRestcomm() {
   		echo "HOSTNAME $RESTCOMM_HOSTNAME"
   		sed -i "s|<hostname>.*<\/hostname>|<hostname>${RESTCOMM_HOSTNAME}<\/hostname>|" $RESTCOMM_DEPLOY/WEB-INF/conf/restcomm.xml
 
-        if hash host 2>/dev/null; then
-		if ! grep "${BIND_ADDRESS}.*${RESTCOMM_HOSTNAME}" /etc/hosts ; then
+ 		if ! grep "${BIND_ADDRESS}.*${RESTCOMM_HOSTNAME}" /etc/hosts ; then
 		echo "${BIND_ADDRESS} ${RESTCOMM_HOSTNAME}" >> /etc/hosts
 		fi
-        else
-            echo "INFO: \"host\" programm does not exist ('dnsutils' package) please make sure that used hostname has a valid DNS resolution."
-            echo "INFO:IF not add the necessary hostname Ip resolution at /etc/hosts file: e.g  echo RestC0mm_BIND_IP RESTCOMM_HOSTNAME >> /etc/hosts "
+
         fi
 	else
   		sed -i "s|<hostname>.*<\/hostname>|<hostname>${PUBLIC_IP}<\/hostname>|" $RESTCOMM_DEPLOY/WEB-INF/conf/restcomm.xml
