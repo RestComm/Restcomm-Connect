3110300aabd76c5715dd32d708c9008923c55c16
Maria Farooq <maria-farooq@live.com>
2017-04-01 00:38:18 +0500

    Added test to put stressed requests on RestcommSupervisor to create actors

---
+++ restcomm/restcomm.commons/src/test/java/org/restcomm/connect/commons/faulttolerance/tool/SimpleActor.java
@@ -0,0 +1,19 @@
+package org.restcomm.connect.commons.faulttolerance.tool;
+
+import akka.actor.UntypedActor;
+
+/**
+ * SampleActor: a simple actor to be created
+ * 
+ * @author mariafarooq
+ *
+ */
+public class SimpleActor  extends UntypedActor {
+
+	public SimpleActor(){
+	}
+
+	@Override
+	public void onReceive(Object message) throws Exception {}
+
+}
