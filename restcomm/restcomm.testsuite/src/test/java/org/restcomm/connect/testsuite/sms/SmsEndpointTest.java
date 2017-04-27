4b2629f35c556b777d7e5046cbf8f27d61bac85a
George Vagenas <gvagenas@gmail.com>
2017-04-27 12:34:10 +0530

    Patch to support Sms messages with To=client:alice (currently we support To=alice) This close #2108

--- restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/sms/SmsEndpointTest.java
+++ restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/sms/SmsEndpointTest.java
@@ -24,8 +24,10 @@ import static org.junit.Assert.assertTrue;
 import gov.nist.javax.sip.header.SIPHeader;
 
 import java.net.URL;
+import java.text.ParseException;
 import java.util.HashMap;
 
+import javax.sip.address.SipURI;
 import javax.sip.message.Request;
 
 import org.apache.log4j.Logger;
@@ -62,26 +64,35 @@ public class SmsEndpointTest {
     private Deployer deployer;
     @ArquillianResource
     URL deploymentUrl;
-    
+
     private static SipStackTool tool1;
     private SipStack bobSipStack;
     private SipPhone bobPhone;
     private String bobContact = "sip:1213@127.0.0.1:5090";
 
+    private static SipStackTool tool2;
+    private SipStack aliceSipStack;
+    private SipPhone alicePhone;
+    private String aliceContact = "sip:alice@127.0.0.1:5091";
+
     private String adminAccountSid = "ACae6e420f425248d6a26948c17a9e2acf";
     private String adminAuthToken = "77f8c12cc7b8f8423e5c38b035249166";
-    
+
     @BeforeClass
     public static void beforeClass() throws Exception {
         tool1 = new SipStackTool("SmsTest1");
+        tool2 = new SipStackTool("SmsTest2");
     }
-    
+
     @Before
     public void before() throws Exception {
         bobSipStack = tool1.initializeSipStack(SipStack.PROTOCOL_UDP, "127.0.0.1", "5090", "127.0.0.1:5080");
         bobPhone = bobSipStack.createSipPhone("127.0.0.1", SipStack.PROTOCOL_UDP, 5080, bobContact);
+
+        aliceSipStack = tool2.initializeSipStack(SipStack.PROTOCOL_UDP, "127.0.0.1", "5091", "127.0.0.1:5080");
+        alicePhone = aliceSipStack.createSipPhone("127.0.0.1", SipStack.PROTOCOL_UDP, 5080, aliceContact);
     }
-    
+
     @After
     public void after() throws Exception {
         if (bobPhone != null) {
@@ -91,20 +102,20 @@ public class SmsEndpointTest {
             bobSipStack.dispose();
         }
     }
-    
+
     @Test
     public void sendSmsTest() {
         SipCall bobCall = bobPhone.createSipCall();
         bobCall.listenForMessage();
-        
+
         String from = "+15126002188";
         String to = "1213";
         String body = "Hello Bob!";
-        
+
         JsonObject callResult = SmsEndpointTool.getInstance().createSms(deploymentUrl.toString(), adminAccountSid,
                 adminAuthToken, from, to, body, null);
         assertNotNull(callResult);
-        
+
 //        bobPhone.setLoopback(true);
         assertTrue(bobCall.waitForMessage(10000));
         Request messageRequest = bobCall.getLastReceivedMessageRequest();
@@ -114,19 +125,71 @@ public class SmsEndpointTest {
     }
 
     @Test
+    public void sendSmsTestToClientAlice() throws ParseException {
+
+        SipURI uri = aliceSipStack.getAddressFactory().createSipURI(null, "127.0.0.1:5080");
+        assertTrue(alicePhone.register(uri, "alice", "1234", aliceContact, 3600, 3600));
+
+        // Prepare second phone to receive call
+        SipCall aliceCall = alicePhone.createSipCall();
+        aliceCall.listenForMessage();
+
+        String from = "+15126002188";
+        String to = "client:alice";
+        String body = "Hello Alice!";
+
+        JsonObject callResult = SmsEndpointTool.getInstance().createSms(deploymentUrl.toString(), adminAccountSid,
+                adminAuthToken, from, to, body, null);
+        assertNotNull(callResult);
+
+//        bobPhone.setLoopback(true);
+        assertTrue(aliceCall.waitForMessage(10000));
+        Request messageRequest = aliceCall.getLastReceivedMessageRequest();
+        assertTrue(aliceCall.sendMessageResponse(202, "Accepted", 3600));
+        String messageReceived = new String(messageRequest.getRawContent());
+        assertTrue(messageReceived.equals(body));
+    }
+
+    @Test
+    public void sendSmsTestToAlice() throws ParseException {
+
+        SipURI uri = aliceSipStack.getAddressFactory().createSipURI(null, "127.0.0.1:5080");
+        assertTrue(alicePhone.register(uri, "alice", "1234", aliceContact, 3600, 3600));
+
+        // Prepare second phone to receive call
+        SipCall aliceCall = alicePhone.createSipCall();
+        aliceCall.listenForMessage();
+
+        String from = "+15126002188";
+        String to = "alice";
+        String body = "Hello Alice!";
+
+        JsonObject callResult = SmsEndpointTool.getInstance().createSms(deploymentUrl.toString(), adminAccountSid,
+                adminAuthToken, from, to, body, null);
+        assertNotNull(callResult);
+
+//        bobPhone.setLoopback(true);
+        assertTrue(aliceCall.waitForMessage(10000));
+        Request messageRequest = aliceCall.getLastReceivedMessageRequest();
+        assertTrue(aliceCall.sendMessageResponse(202, "Accepted", 3600));
+        String messageReceived = new String(messageRequest.getRawContent());
+        assertTrue(messageReceived.equals(body));
+    }
+
+    @Test
     public void sendSmsTestGSMEncoding() {
         SipCall bobCall = bobPhone.createSipCall();
         bobCall.listenForMessage();
-        
+
         String from = "+15126002188";
         String to = "1213";
         String body = "Hello Bob!";
         String encoding = "GSM";
-        
+
         JsonObject callResult = SmsEndpointTool.getInstance().createSms(deploymentUrl.toString(), adminAccountSid,
                 adminAuthToken, from, to, body, null, encoding);
         assertNotNull(callResult);
-        
+
         assertTrue(bobCall.waitForMessage(10000));
         Request messageRequest = bobCall.getLastReceivedMessageRequest();
         assertTrue(bobCall.sendMessageResponse(202, "Accepted", 3600));
@@ -138,16 +201,16 @@ public class SmsEndpointTest {
     public void sendSmsTestUCS2Encoding() {
         SipCall bobCall = bobPhone.createSipCall();
         bobCall.listenForMessage();
-        
+
         String from = "+15126002188";
         String to = "1213";
         String body = " ̰Heo llb!Bo ͤb!";
         String encoding = "UCS-2";
-        
+
         JsonObject callResult = SmsEndpointTool.getInstance().createSms(deploymentUrl.toString(), adminAccountSid,
                 adminAuthToken, from, to, body, null, encoding);
         assertNotNull(callResult);
-        
+
         assertTrue(bobCall.waitForMessage(10000));
         Request messageRequest = bobCall.getLastReceivedMessageRequest();
         assertTrue(bobCall.sendMessageResponse(202, "Accepted", 3600));
@@ -161,40 +224,40 @@ public class SmsEndpointTest {
     public void sendSmsTestWithCustomHeaders() {
         String myFirstHeaderName = "X-Custom-Header-1";
         String myFirstHeaderValue = "X Custom Header Value 1";
-        
+
         String mySecondHeaderName = "X-Custom-Header-2";
         String mySecondHeaderValue = "X Custom Header Value 2";
-        
+
         SipCall bobCall = bobPhone.createSipCall();
         bobCall.listenForMessage();
-        
+
         String from = "+15126002188";
         String to = "1213";
         String body = "Hello Bob! This time I sent you the message and some additional headers.";
         HashMap<String, String> additionalHeaders = new HashMap<String, String>();
         additionalHeaders.put(myFirstHeaderName, myFirstHeaderValue);
         additionalHeaders.put(mySecondHeaderName, mySecondHeaderValue);
-        
+
         JsonObject callResult = SmsEndpointTool.getInstance().createSms(deploymentUrl.toString(), adminAccountSid,
                 adminAuthToken, from, to, body, additionalHeaders);
         assertNotNull(callResult);
-        
+
         assertTrue(bobCall.waitForMessage(10000));
         Request messageRequest = bobCall.getLastReceivedMessageRequest();
         assertTrue(bobCall.sendMessageResponse(202, "Accepted", 3600));
         String messageReceived = new String(messageRequest.getRawContent());
         assertTrue(messageReceived.equals(body));
-        
+
         SIPHeader myFirstHeader = (SIPHeader)messageRequest.getHeader(myFirstHeaderName);
         assertTrue(myFirstHeader != null);
         assertTrue(myFirstHeader.getValue().equalsIgnoreCase(myFirstHeaderValue));
-        
+
         SIPHeader mySecondHeader = (SIPHeader)messageRequest.getHeader(mySecondHeaderName);
         assertTrue(mySecondHeader != null);
         assertTrue(mySecondHeader.getHeaderValue().equalsIgnoreCase(mySecondHeaderValue));
-        
+
     }
-    
+
     @Deployment(name = "LiveCallModificationTest", managed = true, testable = false)
     public static WebArchive createWebArchiveNoGw() {
         logger.info("Packaging Test App");
