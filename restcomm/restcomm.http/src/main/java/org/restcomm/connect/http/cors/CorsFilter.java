b009ff46f6372acec4e894745223e96c3e5cc967
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2017-05-19 14:04:11 +0300

    Added allow-all CORS filter
    refers #2163

---
+++ restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/cors/CorsFilter.java
@@ -0,0 +1,44 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+package org.restcomm.connect.http.cors;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author otsakir@gmail.com - Orestis Tsakiridis
+ */
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public ContainerResponse filter(ContainerRequest cres, ContainerResponse response) {
+        response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        response.getHttpHeaders().add("Access-Control-Max-Age", "1209600");
+        return response;
+    }
+}
