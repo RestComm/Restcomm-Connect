2974321ea558f12fc0a25b5df31f11dfcf6a2522
Maria Farooq <maria-farooq@live.com>
2017-07-04 01:11:29 +0500

    Revert "patched #2312"
    This reverts commit 3ba284ce8afe4ede6248ec4238565d41c4d2bb62.

--- restcomm/restcomm.telephony.api/src/main/java/org/restcomm/connect/telephony/api/ConferenceStateChanged.java
+++ restcomm/restcomm.telephony.api/src/main/java/org/restcomm/connect/telephony/api/ConferenceStateChanged.java
@@ -25,7 +25,7 @@ package org.restcomm.connect.telephony.api;
  */
 public final class ConferenceStateChanged {
     public enum State {
-        RUNNING_INITIALIZING, RUNNING_MODERATOR_ABSENT, RUNNING_MODERATOR_PRESENT, STOPPING, COMPLETED, FAILED
+        RUNNING_INITIALIZING, RUNNING_MODERATOR_ABSENT, RUNNING_MODERATOR_PRESENT, COMPLETED, FAILED
     };
 
     private final String name;
@@ -67,10 +67,6 @@ public final class ConferenceStateChanged {
 
                 case "FAILED":
                     converetedState=State.FAILED;
-                    break;
-
-                case "STOPPING":
-                    converetedState=State.STOPPING;
                     break;
 
                 default:
