c2f63f8199b14a1f7012e50fb8f406e3ecafb090
agafox <agafox@qlabs.cc>
2017-07-05 18:36:55 +0300

    added ActorSupervisorStrategyTest

---
+++ restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/faultTolerance/ActorSupervisorStrategyTest.java
@@ -0,0 +1,104 @@
+package org.restcomm.connect.testsuite.faultTolerance;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.testkit.JavaTestKit;
+import akka.util.Timeout;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
+
+import static akka.pattern.Patterns.ask;
+
+/**
+ * @author oleg.agafonov@telestax.com (Oleg Agafonov)
+ */
+public class ActorSupervisorStrategyTest {
+
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        Config config = ConfigFactory.load("akka_fault_tolerance_application.conf");
+        system = ActorSystem.create("test", config);
+    }
+
+    @AfterClass
+    public static void teardown() {
+        system.shutdown();
+    }
+
+    @Test
+    public void parentActorThrowsExceptionTest() throws Exception {
+        new JavaTestKit(system) {{
+            final ActorRef parent = system.actorOf(new Props(Parent.class));
+            parent.tell("check exception", getRef());
+            expectMsgEquals(duration("1 second"), false);
+            parent.tell("throw exception", getRef());
+            Thread.sleep(5000);
+            parent.tell("check exception", getRef());
+            expectMsgEquals(duration("1 second"), true);
+        }};
+    }
+
+    @Test
+    public void childActorThrowsExceptionTest() throws Exception {
+        new JavaTestKit(system) {{
+            final ActorRef parent = system.actorOf(new Props(Parent.class));
+            final Future<Object> future = ask(parent, "create child", new Timeout(Duration.create(5, TimeUnit.SECONDS)));
+            final ActorRef child = (ActorRef) Await.result(future, Duration.create(10, TimeUnit.SECONDS));
+
+            child.tell("check exception", getRef());
+            expectMsgEquals(duration("1 second"), false);
+            child.tell("throw exception", getRef());
+            Thread.sleep(5000);
+            child.tell("check exception", getRef());
+            expectMsgEquals(duration("1 second"), true);
+        }};
+
+    }
+
+    public static class Parent extends UntypedActor {
+
+        private boolean receivedThrowException;
+
+        @SuppressWarnings("Duplicates")
+        @Override
+        public void onReceive(Object message) throws Exception {
+            if ("create child".equals(message)) {
+                ActorRef child = getContext().actorOf(new Props(Child.class));
+                sender().tell(child, self());
+            } else if ("throw exception".equals(message)) {
+                this.receivedThrowException = true;
+                throw new RuntimeException();
+            } else if ("check exception".equals(message)) {
+                sender().tell(receivedThrowException, self());
+            }
+        }
+    }
+
+    public static class Child extends UntypedActor {
+
+        private boolean receivedThrowException;
+
+        @SuppressWarnings("Duplicates")
+        @Override
+        public void onReceive(Object message) throws Exception {
+            if ("throw exception".equals(message)) {
+                this.receivedThrowException = true;
+                throw new RuntimeException();
+            } else if ("check exception".equals(message)) {
+                sender().tell(receivedThrowException, self());
+            }
+        }
+    }
+}
