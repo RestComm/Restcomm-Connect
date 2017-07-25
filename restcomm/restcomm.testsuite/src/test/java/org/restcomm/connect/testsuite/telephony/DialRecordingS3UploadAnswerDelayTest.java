85d91bfd8cadae7e1dae594a15a1ef9986b1e168
George Vagenas <gvagenas@gmail.com>
2017-07-25 10:52:13 +0300

    Fixes for testsuite

--- restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/telephony/DialRecordingS3UploadAnswerDelayTest.java
+++ restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/telephony/DialRecordingS3UploadAnswerDelayTest.java
@@ -231,6 +231,8 @@ public class DialRecordingS3UploadAnswerDelayTest {
 		assertTrue(duration==3.0);
 		assertTrue(recording.get(0).getAsJsonObject().get("file_uri").getAsString().startsWith("http://localhost:8080/restcomm/2012-04-24/Accounts/ACae6e420f425248d6a26948c17a9e2acf/Recordings/"));
 
+		Thread.sleep(7000);
+
 		//Verify S3 Upload
 		List<LoggedRequest> requests = findAll(putRequestedFor(urlMatching("/s3/.*")));
 		assertEquals(1, requests.size());
@@ -308,6 +310,8 @@ public class DialRecordingS3UploadAnswerDelayTest {
 		double duration = recording.get(0).getAsJsonObject().get("duration").getAsDouble();
 		assertTrue(duration==3.0);
 		assertTrue(recording.get(0).getAsJsonObject().get("file_uri").getAsString().startsWith("http://localhost:8080/restcomm/2012-04-24/Accounts/ACae6e420f425248d6a26948c17a9e2acf/Recordings/"));
+
+		Thread.sleep(7000);
 
 		//Verify S3 Upload
 		List<LoggedRequest> requests = findAll(putRequestedFor(urlMatching("/s3/.*")));
