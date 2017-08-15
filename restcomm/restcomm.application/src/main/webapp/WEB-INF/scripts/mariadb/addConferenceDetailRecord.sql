876d2c2d8e1ff4459d31194d17495109037f3812
Maria Farooq <maria-farooq@live.com>
2017-08-15 19:38:35 +0500

    fixed https://telestax.atlassian.net/browse/RESTCOMM-757

--- restcomm/restcomm.application/src/main/webapp/WEB-INF/scripts/mariadb/addConferenceDetailRecord.sql
+++ restcomm/restcomm.application/src/main/webapp/WEB-INF/scripts/mariadb/addConferenceDetailRecord.sql
@@ -11,9 +11,9 @@ CREATE PROCEDURE addConferenceDetailRecord(	IN in_sid VARCHAR(34),
 									IN in_master_ms_id VARCHAR(34),
 									IN master_present BOOLEAN )
 BEGIN
-	IF EXISTS(SELECT * FROM restcomm_conference_detail_records WHERE friendly_name=in_friendly_name AND status LIKE 'RUNNING%')
+	IF EXISTS(SELECT * FROM restcomm_conference_detail_records WHERE friendly_name=in_friendly_name AND account_sid=in_account_sid AND status LIKE 'RUNNING%')
 	THEN
-		SELECT * FROM restcomm_conference_detail_records WHERE friendly_name=in_friendly_name AND status LIKE 'RUNNING%';
+		SELECT * FROM restcomm_conference_detail_records WHERE friendly_name=in_friendly_name AND account_sid=in_account_sid AND status LIKE 'RUNNING%';
 	ELSE 
 		INSERT INTO restcomm_conference_detail_records (sid, date_created, date_updated, account_sid, status, friendly_name, api_version, uri, master_ms_id, master_present) VALUES (in_sid, in_date_created, in_date_updated, in_account_sid, in_status, in_friendly_name, in_api_version, in_uri, in_master_ms_id, master_present); 
 		
