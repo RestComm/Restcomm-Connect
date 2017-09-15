7994b0969aa81c603eb44b6fd1fb7de8042af758
George Vagenas <gvagenas@gmail.com>
2017-09-15 11:22:14 +0300

    Patch for Downloader support for TLSv1.1 and TLSv1.2 This close #2501 This close #RESTCOMM-1095 This close #RESTCOMM-1093

--- restcomm/restcomm.telephony/src/main/java/org/restcomm/connect/telephony/Call.java
+++ restcomm/restcomm.telephony/src/main/java/org/restcomm/connect/telephony/Call.java
@@ -1379,21 +1379,27 @@ public final class Call extends RestcommUntypedActor {
         @Override
         public void execute(final Object message) throws Exception {
             if (isInbound()) {
-                SipServletResponse resp = null;
-                if (message instanceof CallFail) {
-                    resp = invite.createResponse(500, "Problem to setup the call");
-                    String reason = ((CallFail) message).getReason();
-                    if (reason != null)
-                        resp.addHeader("Reason", reason);
-                } else {
-                    // https://github.com/RestComm/Restcomm-Connect/issues/1663
-                    // We use 569 only if there is a problem to reach RMS as LB can be configured to take out
-                    // nodes that send back 569. This is meant to protect the cluster from nodes where the RMS
-                    // is in bad state and not responding anymore
-                    resp = invite.createResponse(MEDIA_SERVER_FAILURE_RESPONSE_CODE, "Problem to setup services");
+                try {
+                    if (invite.getSession().isValid()) {
+                        SipServletResponse resp = null;
+                        if (message instanceof CallFail) {
+                            resp = invite.createResponse(500, "Problem to setup the call");
+                            String reason = ((CallFail) message).getReason();
+                            if (reason != null)
+                                resp.addHeader("Reason", reason);
+                        } else {
+                            // https://github.com/RestComm/Restcomm-Connect/issues/1663
+                            // We use 569 only if there is a problem to reach RMS as LB can be configured to take out
+                            // nodes that send back 569. This is meant to protect the cluster from nodes where the RMS
+                            // is in bad state and not responding anymore
+                            resp = invite.createResponse(MEDIA_SERVER_FAILURE_RESPONSE_CODE, "Problem to setup services");
+                        }
+                        addCustomHeaders(resp);
+                        resp.send();
+                    }
+                } catch (Exception e) {
+                    logger.error("Exception while trying to prepare failed response, exception: "+e);
                 }
-                addCustomHeaders(resp);
-                resp.send();
             } else {
                 if (message instanceof CallFail)
                     sendBye(new Hangup(((CallFail) message).getReason()));
