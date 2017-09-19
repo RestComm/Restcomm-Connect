84225771d4f27fdd9ea2d122f3b114192926821a
Maria Farooq <maria-farooq@live.com>
2017-09-19 14:59:30 +0500

    added test for different roles #2493

--- restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/http/OrganizationsEndpointTest.java
+++ restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/http/OrganizationsEndpointTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 import java.net.URL;
 
 import org.apache.log4j.Logger;
-import org.jboss.arquillian.container.mss.extension.SipStackTool;
 import org.jboss.arquillian.container.test.api.Deployer;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -20,6 +19,7 @@ import org.restcomm.connect.commons.Version;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.sun.jersey.api.client.ClientResponse;
 
 /**
  * @author maria
@@ -36,29 +36,23 @@ public class OrganizationsEndpointTest extends EndpointTest {
     @ArquillianResource
     URL deploymentUrl;
 
-    private String superAdminUsername = "administrator@company.com";
-    private String adminFriendlyName = "Default Administrator Account";
-    private String adminAccountSid = "ACae6e420f425248d6a26948c17a9e2acf";
+    //super admin account
+    private String superAdminAccountSid = "ACae6e420f425248d6a26948c17a9e2acf";
     private String superAdminAuthToken = "77f8c12cc7b8f8423e5c38b035249166";
-    private String adminPassword = "RestComm";
 
-    private String commonAuthToken = "77f8c12cc7b8f8423e5c38b035249166";
+    //admin account
+    private String adminAccountSid = "AC574d775522c96f9aacacc5ca60c8c74g";
+    private String adminAuthToken = "77f8c12cc7b8f8423e5c38b035249166";
 
-    static SipStackTool tool1;
+    //developer account
+    private String devAccountSid = "AC574d775522c96f9aacacc5ca60c8c74f";
+    private String devAuthToken = "77f8c12cc7b8f8423e5c38b035249166"; 
 
     private final String org1 = "ORafbe225ad37541eba518a74248f0ac4c";
-    private final String org2 = "ORafbe225ad37541eba518a74248f0ac4c";
+    private final String org2 = "ORafbe225ad37541eba518a74248f0ac4d";
     
     @BeforeClass
     public static void beforeClass() {
-    }
-
-    @Test
-    public void testGetAccount() {
-        // Get Account using admin email address and user email address
-        JsonObject adminAccount = RestcommAccountsTool.getInstance().getAccount(deploymentUrl.toString(), superAdminUsername,
-                superAdminAuthToken, superAdminUsername);
-        assertTrue(adminAccount.get("sid").getAsString().equals(adminAccountSid));
     }
 
     /**
@@ -67,18 +61,18 @@ public class OrganizationsEndpointTest extends EndpointTest {
      */
     @Test
     public void getOrganizationFromSuperAdminAccount(){
-    	JsonObject organizationJsonObject = RestcommOrganizationsTool.getInstance().getOrganization(deploymentUrl.toString(), superAdminUsername, superAdminAuthToken, org1);
+    	JsonObject organizationJsonObject = RestcommOrganizationsTool.getInstance().getOrganization(deploymentUrl.toString(), superAdminAccountSid, superAdminAuthToken, org1);
     	assertTrue(organizationJsonObject!=null);
     	logger.info("organization: "+organizationJsonObject);
     	
     	// only superadmin can read an org that does not affiliate with its account
     	organizationJsonObject = null;
-    	organizationJsonObject = RestcommOrganizationsTool.getInstance().getOrganization(deploymentUrl.toString(), superAdminUsername, superAdminAuthToken, org1);
+    	organizationJsonObject = RestcommOrganizationsTool.getInstance().getOrganization(deploymentUrl.toString(), superAdminAccountSid, superAdminAuthToken, org2);
     	assertTrue(organizationJsonObject!=null);
     	
     	//only superadmin can read the whole list of organizations
     	JsonArray jsonArray = null;
-    	jsonArray = RestcommOrganizationsTool.getInstance().getOrganizationList(deploymentUrl.toExternalForm(), superAdminUsername, superAdminAuthToken, null);
+    	jsonArray = RestcommOrganizationsTool.getInstance().getOrganizationList(deploymentUrl.toExternalForm(), superAdminAccountSid, superAdminAuthToken, null);
     	logger.info("organization list: "+jsonArray);
     	assertTrue(jsonArray!=null);
     	assertTrue(jsonArray.size() == 2);
@@ -89,13 +83,53 @@ public class OrganizationsEndpointTest extends EndpointTest {
      * this test will try to Read single organization and read list
      */
     @Test
-    public void getOrganizationFromAdministratorAccount(){}
+    public void getOrganizationFromAdministratorAccount(){
+    	ClientResponse clientResponse = RestcommOrganizationsTool.getInstance().getOrganizationResponse(deploymentUrl.toString(), adminAccountSid, adminAuthToken, org1);
+    	assertTrue(clientResponse!=null);
+    	logger.info("organization: "+clientResponse);
+    	assertTrue(clientResponse.getStatus() == 200);
+    	
+    	// only superadmin can read an org that does not affiliate with its account
+    	clientResponse = null;
+    	clientResponse = RestcommOrganizationsTool.getInstance().getOrganizationResponse(deploymentUrl.toString(), adminAccountSid, adminAuthToken, org2);
+    	assertTrue(clientResponse!=null);
+    	logger.info("organization: "+clientResponse);
+    	assertTrue(clientResponse.getStatus() == 401);
+    	
+    	//only superadmin can read the whole list of organizations
+    	clientResponse = null;
+    	clientResponse = RestcommOrganizationsTool.getInstance().getOrganizationsResponse(deploymentUrl.toExternalForm(), adminAccountSid, adminAuthToken);
+    	logger.info("organization list: "+clientResponse);
+    	assertTrue(clientResponse!=null);
+    	assertTrue(clientResponse.getStatus() == 403);
+    
+    }
     /**
      * Developers can not read organization
      * this test will try to Read single organization and read list
      */
     @Test
-    public void getOrganizationFromDeveloperAccount(){}
+    public void getOrganizationFromDeveloperAccount(){
+    	ClientResponse clientResponse = RestcommOrganizationsTool.getInstance().getOrganizationResponse(deploymentUrl.toString(), devAccountSid, devAuthToken, org1);
+    	assertTrue(clientResponse!=null);
+    	logger.info("organization: "+clientResponse);
+    	assertTrue(clientResponse.getStatus() == 401);
+    	
+    	// only superadmin can read an org that does not affiliate with its account
+    	clientResponse = null;
+    	clientResponse = RestcommOrganizationsTool.getInstance().getOrganizationResponse(deploymentUrl.toString(), devAccountSid, devAuthToken, org2);
+    	assertTrue(clientResponse!=null);
+    	logger.info("organization: "+clientResponse);
+    	assertTrue(clientResponse.getStatus() == 401);
+    	
+    	//only superadmin can read the whole list of organizations
+    	clientResponse = null;
+    	clientResponse = RestcommOrganizationsTool.getInstance().getOrganizationsResponse(deploymentUrl.toExternalForm(), devAccountSid, devAuthToken);
+    	logger.info("organization list: "+clientResponse);
+    	assertTrue(clientResponse!=null);
+    	assertTrue(clientResponse.getStatus() == 403);
+    
+    }
     /**
      * getOrganizationListByStatus
      */
