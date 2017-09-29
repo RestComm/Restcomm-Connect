15f06a542344d4485bac86b627b81bd1a5105f95
Maria Farooq <maria-farooq@live.com>
2017-09-29 15:54:42 +0500

    We want this file to be executed last since its contains xmlstarlet based config script https://telestax.atlassian.net/browse/RESTCOMM-1140

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfigure.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfigure.sh
@@ -5,17 +5,29 @@
 
 autoconfigure() {
     local BASEDIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+    
+    ## We want this file to be executed last since its contains xmlstarlet based config script
+    ## https://telestax.atlassian.net/browse/RESTCOMM-1140
+    local LAST_FILE_TO_BE_EXECUTED=$BASEDIR/autoconfig.d/config-restcomm.sh
 
     # load configuration values
     #source $BASEDIR/restcomm.conf
     echo ''
     echo 'RestComm automatic configuration started:'
+    echo "LAST_FILE_TO_BE_EXECUTED is: $LAST_FILE_TO_BE_EXECUTED"
     for f in $BASEDIR/autoconfig.d/*.sh; do
         echo "Executing configuration file $f..."
-        source $f
-        echo "Finished executing configuration file $f!"
-        echo ''
+        if [ "$f" != "$LAST_FILE_TO_BE_EXECUTED" ]; then
+                source $f
+                echo "Finished executing configuration file $f!"
+                echo ''
+        fi
     done
+
+    source $LAST_FILE_TO_BE_EXECUTED
+    echo "Finished executing configuration file $LAST_FILE_TO_BE_EXECUTED!"
+    echo ''
+
     echo 'RestComm automatic configuration finished!'
     echo ''
 }
