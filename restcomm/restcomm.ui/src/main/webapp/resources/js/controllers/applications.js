25415b82841dc8afe0c81ce91c82d1936e53a2a0
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2017-11-06 15:00:01 +0200

    Application creation and import (#2)
    * Added initial application list view and controller
    
    Refers Restcomm/Restcomm-Connect#2561
    
    * Client-side filtering for application listing view and 'Applications' navbar item
    
    Refers Restcomm/Restcomm-Connect#2561
    
    * Responsive tweaking for application listing
    
    Refers Restcomm/Restcomm-Connect#2592
    
    * More responsive tweaking for application listing table
    
    Refers Restcomm/Restcomm-Connect#2592
    
    * Introduced application details view - WIP
    
    Refers Restcomm/Restcomm-Connect#2562
    
    * Added 'external' application and application removal support to app details
    
    Refers Restcomm/Restcomm-Connect#2562
    
    * Persistense for external apps in app-details view
    
    Refers Restcomm/Restcomm-Connect#2562
    
    * RVD project export/download operation in app-details view
    
    - Added 'filesaver', an AJAX file downloader
    - Ported angular services and glue code from RVD
    - Added download operation in app-details view
    
    Refers Restcomm/Restcomm-Connect#2562
    
    * Minor UI fixes in app details view
    
    Refers Restcomm/Restcomm-Connect#2562
    
    * Added placeholder view for application creation wizzard
    
    Refers Restcomm/Restcomm-Connect#2563
    
    * Added application creation view and controller
    
    Refers Restcomm/Restcomm-Connect#2609
    
    * RVD project upload support for dashboard
    
    Restcomm/Restcomm-Connect#2609
    
    * Upgraded file-uploader and added filename override option
    
    Refers Restcomm/Restcomm-Connect#2562
    
    * 'kind' option in application creation view and narrower content
    
    Refers Refers Restcomm/Restcomm-Connect#2562, Refers Restcomm/Restcomm-Connect#2609
    
    * Blank project creation from dashboard
    
    Refers Restcomm/Restcomm-Connect#2609

--- restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/applications.js
+++ restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/applications.js
@@ -34,6 +34,83 @@ angular.module('rcApp.controllers').controller('ApplicationDetailsCtrl', functio
     }
 });
 
+angular.module('rcApp.controllers').controller('ApplicationCreationWizzardCtrl', function ($scope, $rootScope, $location) {
+    console.log("IN ApplicaitonCreationWizzardCtrl");
+
+    $scope.onFileDropped = function(files) {
+        $rootScope.droppedFiles = files;
+        $location.path("/applications/new");
+
+    }
+});
+
+angular.module('rcApp.controllers').controller('ApplicationCreationCtrl', function ($scope, $rootScope, $location, Notifications, RvdProjectImporter, RvdProjects) {
+    var appOptions = {}, droppedFiles; // all options the application needs to be created like name, kind ... anything else ?
+    if ( !!$rootScope.droppedFiles ) {
+        droppedFiles = $rootScope.droppedFiles;
+        delete $rootScope.droppedFiles;
+    }
+    if (!droppedFiles) {
+        appOptions.kind = "voice"; // by default create voice applications
+    }
+
+    $scope.setKind = function(options, kind) {
+        options.kind = kind;
+    }
+
+    $scope.createRvdApplication = function(options) {
+        RvdProjects.create({applicationSid: options.name, kind:options.kind}, null, function (data) { // RVD does not have an intuitive API :-( // NOTE 'null' is VERY IMPORTANT here as it makes $resource and the kind as a query parameter
+            Notifications.success("RVD application created");
+            window.open("/restcomm-rvd#/designer/" + data.sid + "=" + data.name);
+
+        });
+
+    }
+
+
+    // if we're importing, use imported filename to suggest a name for the newly created project
+    if (droppedFiles) {
+        var m = droppedFiles[0].name.match(RegExp("(.+)\\.zip$"));
+        if ( m && m[1] ) {
+            appOptions.name = m[1];
+        }
+    }
+
+    $scope.importProjectFromFile = function(files, nameOverride) {
+        if (files[0]) {
+            RvdProjectImporter.import(files[0], nameOverride).then(function () {
+                Notifications.success("Application imported successfully");
+                $location.path("/applications");
+            }, function (message) {
+                Notifications.error(message);
+            });
+        }
+
+        /*
+        $scope.upload = $upload.upload({
+            url: '/restcomm-rvd/services/projects',
+            file: file,
+        }).progress(function(evt) {
+            //console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+        }).success(function(data, status, headers, config) {
+            Notifications.success("Application imported successfully");
+            $location.path("/applications")
+        }).error(function(data, status, headers, config) {
+            if (status == 400 && data && data.exception && data.exception.className == "UnsupportedProjectVersion") {
+                console.log(data.exception.message);
+                Notifications.error("Cannot import application. " + data.exception.message, type:"danger");
+            } else {
+                console.log(data);
+                Notifications.error("Error importing application");
+            }
+        });
+        */
+    }
+
+    $scope.appOptions = appOptions;
+    $scope.droppedFiles = droppedFiles;
+});
+
 var confirmApplicationDelete = function(app, $dialog, $scope, Notifications, RCommApplications, RvdProjects, $location) {
   var title = 'Delete application \'' + app.friendly_name + '\'';
   var msg = 'Are you sure you want to delete application ' + app.sid + ' (' + app.friendly_name +  ') ? This action cannot be undone.';
