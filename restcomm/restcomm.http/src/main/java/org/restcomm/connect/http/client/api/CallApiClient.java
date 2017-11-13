0303a1f8fdc58c134cedfb52a718e30700cc7ef1
Maria Farooq <maria-farooq@live.com>
2017-11-13 18:36:06 +0500

    changed timeout unit

--- restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/client/api/CallApiClient.java
+++ restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/client/api/CallApiClient.java
@@ -72,7 +72,7 @@ public class CallApiClient extends RestcommUntypedActor {
         this.callSid = callSid;
         this.storage = storage;
         //actor will only live in memory for one hour
-        context().setReceiveTimeout(Duration.create(3600, TimeUnit.MILLISECONDS));
+        context().setReceiveTimeout(Duration.create(3600, TimeUnit.SECONDS));
     }
     @Override
     public void onReceive(Object message) throws Exception {
