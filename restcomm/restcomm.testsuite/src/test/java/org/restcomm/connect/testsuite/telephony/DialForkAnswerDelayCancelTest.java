69c5620871f54e156a40eaa07d50c73afd723a9d
George Vagenas <gvagenas@gmail.com>
2017-11-22 13:35:53 +0200

    Categorization for DialFok* tests

--- restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/telephony/DialForkAnswerDelayCancelTest.java
+++ restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/telephony/DialForkAnswerDelayCancelTest.java
@@ -19,6 +19,7 @@ import org.jboss.shrinkwrap.resolver.api.maven.archive.ShrinkWrapMaven;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.restcomm.connect.commons.Version;
+import org.restcomm.connect.commons.annotations.ParallelClassTests;
 import org.restcomm.connect.testsuite.http.RestcommCallsTool;
 
 import javax.sip.address.SipURI;
@@ -43,7 +44,7 @@ import org.restcomm.connect.commons.annotations.SequentialClassTests;
  * Created by gvagenas on 12/19/15.
  */
 @RunWith(Arquillian.class)
-@Category(value={FeatureAltTests.class, SequentialClassTests.class})
+@Category(value={FeatureAltTests.class, ParallelClassTests.class})
 public class DialForkAnswerDelayCancelTest {
 
     private final static Logger logger = Logger.getLogger(CallLifecycleTest.class.getName());
