4f3e72aaddaae50d867c768126eff81c17395f17
Maria Farooq <maria-farooq@live.com>
2017-12-01 14:02:07 +0500

    added conference timeout config

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-restcomm.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-restcomm.sh
@@ -836,6 +836,11 @@ configDnsProvisioningManager() {
 
 }
 
+configConferenceTimeout(){
+    echo "Configure conference timeout $CONFERENCE_TIMEOUT"
+	xmlstarlet ed --inplace -u "/restcomm/runtime-settings/conference-timeout" -v "$CONFERENCE_TIMEOUT" $FILE
+}
+
 # MAIN
 echo 'Configuring RestComm...'
 configRCJavaOpts
@@ -875,4 +880,5 @@ confRVD
 configRMSNetworking
 configAsrDriver
 configDnsProvisioningManager
+configConferenceTimeout
 echo 'Configured RestComm!'
