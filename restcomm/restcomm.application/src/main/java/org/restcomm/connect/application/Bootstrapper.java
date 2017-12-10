72f78cf61cdd216110b8263d458fd59cde4df509
agafox <oleg.agafonov@telestax.com>
2017-12-10 17:50:38 +0300

    isAssignableFrom() bugfix

--- restcomm/restcomm.application/src/main/java/org/restcomm/connect/application/Bootstrapper.java
+++ restcomm/restcomm.application/src/main/java/org/restcomm/connect/application/Bootstrapper.java
@@ -246,7 +246,7 @@ public final class Bootstrapper extends SipServlet implements SipServletListener
 
     private ActorRef sdrService(final Configuration configuration, final ClassLoader loader) throws Exception {
         final String className = configuration.getString("sdr-service[@class]");
-        if (className != null && loader.loadClass(className).isAssignableFrom(SdrService.class)) {
+        if (className != null && SdrService.class.isAssignableFrom(loader.loadClass(className))) {
             final Props props = new Props(new UntypedActorFactory() {
                 @Override
                 public Actor create() throws Exception {
