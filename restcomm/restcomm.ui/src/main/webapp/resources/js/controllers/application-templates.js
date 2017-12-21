5d403ca466540dfd3e8af94c419a5ee2adaecaa2
Orestis Tsakiridis <otsakir@gmail.com>
2017-12-21 14:19:30 +0200

    Progress on app templates list view
    * Added description
    * Added blank template
    
    Refers RestComm/Restcomm-Connect#2684

--- restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/application-templates.js
+++ restcomm/restcomm.ui/src/main/webapp/resources/js/controllers/application-templates.js
@@ -1,7 +1,8 @@
 angular.module('rcApp.controllers').controller('ApplicationTemplatesCtrl', function ($scope, SessionService, RvdProjectTemplates) {
     var accountSid = SessionService.get("sid");
 
+    $scope.filter = {};
     $scope.templateList = RvdProjectTemplates.query();
     //$scope.appsList = RCommApplications.query({accountSid: accountSid, includeNumbers: true});
 
-});
+ });
