0d3a4bdf835e4034ae2f41a8bee05ef9a4846abe
agafox <oleg.agafonov@telestax.com>
2018-01-05 15:05:06 +0300

    sms implementation

--- restcomm/restcomm.sms/src/main/java/org/restcomm/connect/sms/SmsService.java
+++ restcomm/restcomm.sms/src/main/java/org/restcomm/connect/sms/SmsService.java
@@ -204,7 +204,9 @@ public final class SmsService extends RestcommUntypedActor {
             final SipServletResponse messageAccepted = request.createResponse(SipServletResponse.SC_ACCEPTED);
             messageAccepted.send();
 
-            monitoringService.tell(new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.INBOUND_TO_APP), self);
+            TextMessage textMessage = new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.INBOUND_TO_APP);
+            monitoringService.tell(textMessage, self);
+            system.eventStream().publish(textMessage);
 
             return;
 
@@ -238,7 +240,9 @@ public final class SmsService extends RestcommUntypedActor {
                                     logger.info("P2P, Message from: " + client.getLogin() + " redirected to registered client: "
                                             + toClient.getLogin());
                                 }
-                                monitoringService.tell(new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.INBOUND_TO_CLIENT), self);
+                                TextMessage textMessage = new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.INBOUND_TO_CLIENT);
+                                monitoringService.tell(textMessage, self);
+                                system.eventStream().publish(textMessage);
                                 return;
                             } else {
                                 String errMsg = "Cannot Connect to Client: " + toClient.getFriendlyName()
@@ -291,7 +295,9 @@ public final class SmsService extends RestcommUntypedActor {
                 // Send the SMS.
                 TlvSet tlvSet = new TlvSet();
                 final SmsSessionRequest sms = new SmsSessionRequest(client.getLogin(), toUser, new String(request.getRawContent()), request, tlvSet, null);
-                monitoringService.tell(new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.INBOUND_TO_PROXY_OUT), self);
+                TextMessage textMessage = new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.INBOUND_TO_PROXY_OUT);
+                monitoringService.tell(textMessage, self);
+                system.eventStream().publish(textMessage);
                 session.tell(sms, self());
             }
         } else {
@@ -300,7 +306,9 @@ public final class SmsService extends RestcommUntypedActor {
             // We didn't find anyway to handle the SMS.
             String errMsg = "Restcomm cannot process this SMS because the destination number is not hosted locally. To: "+toUser;
             sendNotification(errMsg, 11005, "error", true);
-            monitoringService.tell(new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.NOT_FOUND), self);
+            TextMessage textMessage = new TextMessage(((SipURI)request.getFrom().getURI()).getUser(), ((SipURI)request.getTo().getURI()).getUser(), TextMessage.SmsState.NOT_FOUND);
+            monitoringService.tell(textMessage, self);
+            system.eventStream().publish(textMessage);
         }}
 
 
