daabcbf8e19c10b32e288eb6c3c8f1930cfd6d81
Maria Farooq <maria-farooq@live.com>
2018-01-17 19:48:48 +0500

    added profile dao interface

---
+++ restcomm/restcomm.dao/src/main/java/org/restcomm/connect/dao/ProfilesDao.java
@@ -0,0 +1,63 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+package org.restcomm.connect.dao;
+
+import java.util.List;
+
+import org.restcomm.connect.commons.dao.Sid;
+import org.restcomm.connect.dao.entities.Profile;
+
+/**
+ * @author maria.farooq@telestax.com (Maria Farooq)
+ */
+public interface ProfilesDao {
+
+    /**
+     * @param sid
+     * @return a single profile as per provided profile sid
+     */
+    Profile getProfile(Sid sid);
+
+    /**
+     * @return List of all profiles in the system
+     */
+    List<Profile> getAllProfiles();
+
+    /**
+     * @return number of total profiles in the system
+     */
+    Integer getTotalProfiles();
+
+    /**
+     * @param profile
+     * @return
+     */
+    int addProfile(Profile profile);
+
+    /**
+     * @param profile
+     */
+    void updateProfile(Profile profile);
+
+    /**
+     * @param profile
+     */
+    void deleteProfile(Sid sid);
+}
