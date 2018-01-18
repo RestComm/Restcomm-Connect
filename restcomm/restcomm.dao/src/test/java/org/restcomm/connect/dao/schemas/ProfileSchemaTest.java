040571a214d229aabd6d00636368f5e1ae79a59f
Jaime Casero <jaime.casero@telestax.com>
2018-01-18 07:09:23 -0500

    renamed to featureenablement

--- restcomm/restcomm.dao/src/test/java/org/restcomm/connect/dao/schemas/ProfileSchemaTest.java
+++ restcomm/restcomm.dao/src/test/java/org/restcomm/connect/dao/schemas/ProfileSchemaTest.java
@@ -74,7 +74,7 @@ public class ProfileSchemaTest {
     @Test
     public void testRetrieveAllowedPrefixes() throws Exception{
         final JsonNode good = JsonLoader.fromResource("/org/restcomm/connect/dao/schemas/freePlan.json");
-        JsonPointer pointer = JsonPointer.compile("/FACEnablement/destinations/allowedPrefixes");
+        JsonPointer pointer = JsonPointer.compile("/FeatureEnablement/destinations/allowedPrefixes");
         JsonNode at = good.at(pointer);
         Assert.assertNotNull(at);
         Assert.assertTrue(at.isArray());
