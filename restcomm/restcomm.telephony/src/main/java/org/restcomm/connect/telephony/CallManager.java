a37c1244a9ec5fa7ce42c7481628851edd75c616
George Vagenas <gvagenas@gmail.com>
2018-01-23 12:18:36 +0200

    Add more log statements during the method `redirectToHostedApp`

--- restcomm/restcomm.telephony/src/main/java/org/restcomm/connect/telephony/CallManager.java
+++ restcomm/restcomm.telephony/src/main/java/org/restcomm/connect/telephony/CallManager.java
@@ -1374,7 +1374,8 @@ public final class CallManager extends RestcommUntypedActor {
             sendNotification(fromClientAccountSid, errMsg, 11007, "error", false);
             logger.warning(errMsg);
             if (logger.isDebugEnabled()) {
-                logger.debug("Exception stack trace: \n");
+                logger.debug("Number that caused exception: "+number.toString());
+                logger.debug("Exception stack trace: ");
                 notANumber.printStackTrace();
             }
             isFoundHostedApp = false;
