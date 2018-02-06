94df2c58ecc9614c9d8c3f5ca69c69d3e6f0c278
Jaime Casero <jaime.casero@telestax.com>
2018-02-06 10:58:29 -0500

    allowed/blocked prefixes per service Voice/SMS/USSD

--- restcomm/restcomm.http/src/test/java/org/restcomm/connect/http/schemas/ProfileSchemaTest.java
+++ restcomm/restcomm.http/src/test/java/org/restcomm/connect/http/schemas/ProfileSchemaTest.java
@@ -72,7 +72,7 @@ public class ProfileSchemaTest {
     @Test
     public void testRetrieveAllowedPrefixes() throws Exception {
         final JsonNode good = JsonLoader.fromResource("/org/restcomm/connect/http/schemas/freePlan.json");
-        JsonPointer pointer = JsonPointer.compile("/featureEnablement/destinations/allowedPrefixes");
+        JsonPointer pointer = JsonPointer.compile("/featureEnablement/outboundPSTN/allowedPrefixes");
         JsonNode at = good.at(pointer);
         Assert.assertNotNull(at);
         Assert.assertTrue(at.isArray());
