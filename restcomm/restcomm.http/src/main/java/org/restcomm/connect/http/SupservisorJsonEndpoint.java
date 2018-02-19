320c4238d63cc38d4e186cb0014678f73b79bb03
Jaime Casero <jaime.casero@telestax.com>
2018-02-19 04:11:20 -0500

    fixed supervisor endpointto emet convention

--- restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/SupservisorJsonEndpoint.java
+++ restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/SupservisorJsonEndpoint.java
@@ -29,7 +29,6 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 import static org.restcomm.connect.http.security.AccountPrincipal.SUPER_ADMIN_ROLE;
 
 import javax.annotation.security.RolesAllowed;
@@ -39,7 +38,7 @@ import org.restcomm.connect.commons.annotations.concurrency.ThreadSafe;
  * @author <a href="mailto:gvagenas@gmail.com">gvagenas</a>
  *
  */
-@Path("/Accounts/{accountSid}/Supervisor.json")
+@Path("/Accounts/{accountSid}/Supervisor")
 @ThreadSafe
 @RolesAllowed(SUPER_ADMIN_ROLE)
 public class SupservisorJsonEndpoint extends SupervisorEndpoint{
@@ -51,34 +50,34 @@ public class SupservisorJsonEndpoint extends SupervisorEndpoint{
     //Simple PING/PONG message
     @GET
     public Response ping(@PathParam("accountSid") final String accountSid) {
-        return pong(accountSid, APPLICATION_JSON_TYPE);
+        return pong(accountSid, retrieveMediaType());
     }
 
     //Get statistics
     @Path("/metrics")
     @GET
     public Response getMetrics(@PathParam("accountSid") final String accountSid, @Context UriInfo info) {
-        return getMetrics(accountSid, info, APPLICATION_JSON_TYPE);
+        return getMetrics(accountSid, info, retrieveMediaType());
     }
 
     //Get live calls
     @Path("/livecalls")
     @GET
     public Response getLiveCalls(@PathParam("accountSid") final String accountSid) {
-        return getLiveCalls(accountSid, APPLICATION_JSON_TYPE);
+        return getLiveCalls(accountSid, retrieveMediaType());
     }
 
     //Register a remote location where Restcomm will send monitoring updates
     @Path("/remote")
     @POST
     public Response registerForMetricsUpdates(@PathParam("accountSid") final String accountSid, @Context UriInfo info) {
-        return registerForUpdates(accountSid, info, APPLICATION_JSON_TYPE);
+        return registerForUpdates(accountSid, info, retrieveMediaType());
     }
 
     //Register a remote location where Restcomm will send monitoring updates for a specific Call
     @Path("/remote/{sid}")
     @POST
     public Response registerForCallMetricsUpdates(@PathParam("accountSid") final String accountSid, @PathParam("sid") final String sid, final MultivaluedMap<String, String> data) {
-        return registerForCallUpdates(accountSid, sid, data, APPLICATION_JSON_TYPE);
+        return registerForCallUpdates(accountSid, sid, data, retrieveMediaType());
     }
 }
