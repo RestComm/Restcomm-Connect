88f90f67f4b057b06ee13a1940c9f586a3d09a27
Guilherme Humberto Jansen <guilherme.jansen@telestax.com>
2018-03-02 18:53:51 -0300

    RESTCOMM-1595 #Including missing import

--- restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/smpp/SmppTest.java
+++ restcomm/restcomm.testsuite/src/test/java/org/restcomm/connect/testsuite/smpp/SmppTest.java
@@ -31,6 +31,7 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.FixMethodOrder;
