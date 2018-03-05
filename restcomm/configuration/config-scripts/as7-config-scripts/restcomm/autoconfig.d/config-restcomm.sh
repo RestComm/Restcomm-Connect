49dac701aa3d655fa0187b6b46bbffb0fe3d2d93
leftyb <Lefty1978!>
2018-03-05 17:26:40 +0200

    Enable USESBC configuration option.

--- restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-restcomm.sh
+++ restcomm/configuration/config-scripts/as7-config-scripts/restcomm/autoconfig.d/config-restcomm.sh
@@ -687,6 +687,10 @@ otherRestCommConf(){
     echo "CACHE_NO_WAV $CACHE_NO_WAV"
     sed -i "s|<cache-no-wav>.*</cache-no-wav>|<cache-no-wav>${CACHE_NO_WAV}</cache-no-wav>|" $FILE
 
+    #Configure USESBC
+    echo "USESBC: $USESBC"
+    sed -i "s|<use-sbc>.*</use-sbc>|<use-sbc>${USESBC}</use-sbc>" $FILE
+
     echo "End Rest RestComm configuration"
 }
 
