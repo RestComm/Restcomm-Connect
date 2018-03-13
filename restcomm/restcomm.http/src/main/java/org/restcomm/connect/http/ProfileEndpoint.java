ce6956420c1d9af241d2332a4bcbefa4a27e1260
Jaime Casero <jaime.casero@telestax.com>
2018-03-13 08:18:29 -0400

    fixed wrong accoutn sid

--- restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/ProfileEndpoint.java
+++ restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/ProfileEndpoint.java
@@ -359,8 +359,8 @@ public class ProfileEndpoint {
     private void checkProfileAccess(String profileSid, SecurityContext secCtx) {
         AccountPrincipal userPrincipal = (AccountPrincipal) secCtx.getUserPrincipal();
         if (!userPrincipal.isSuperAdmin()) {
-
-            Profile effectiveProfile = profileService.retrieveEffectiveProfileByAccountSid(userPrincipal.getIdentityContext().getAccountKey().getAccount().toString());
+            String accountSid = userPrincipal.getIdentityContext().getAccountKey().getAccount().getSid().toString();
+            Profile effectiveProfile = profileService.retrieveEffectiveProfileByAccountSid(accountSid);
             if (!effectiveProfile.getSid().equals(profileSid)) {
                 CustomReasonPhraseType stat = new CustomReasonPhraseType(Response.Status.FORBIDDEN, "Profile not linked");
                 throw new WebApplicationException(status(stat).build());
