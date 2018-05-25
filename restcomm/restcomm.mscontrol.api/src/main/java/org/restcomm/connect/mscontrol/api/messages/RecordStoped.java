bd7427abdd9edc133a91c6f086088b74c4aca8e7
Hoan HL <hoan.h.luu@telestax.com>
2018-05-25 14:40:48 +0700

    jira-2165 RC does not send BYE to XMS when stop recording.

---
+++ restcomm/restcomm.mscontrol.api/src/main/java/org/restcomm/connect/mscontrol/api/messages/RecordStoped.java
@@ -0,0 +1,14 @@
+package org.restcomm.connect.mscontrol.api.messages;
+
+import org.restcomm.connect.commons.annotations.concurrency.Immutable;
+
+/**
+ * @author Hoan Luu (hoan.h.luu@telestax.com)
+ */
+
+@Immutable
+public class RecordStoped {
+    public RecordStoped() {
+        super();
+    }
+}
