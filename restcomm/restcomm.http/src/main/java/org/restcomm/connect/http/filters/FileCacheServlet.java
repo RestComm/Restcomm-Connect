0d2726178c091fba1da74cc45c7fae682b3e0a1c
Jaime Casero <jaime.casero@telestax.com>
2018-06-07 21:26:18 +0200

    fixes BS-181 added servlet to allow cache directory separation from jboss installation

---
+++ restcomm/restcomm.http/src/main/java/org/restcomm/connect/http/filters/FileCacheServlet.java
@@ -0,0 +1,267 @@
+
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+package org.restcomm.connect.http.filters;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Arrays;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.configuration.Configuration;
+
+/**
+ * Serves media resources to other third-prty systems like RMS.
+ *
+ * The content is saved in a local FileSystem directory which location is coming
+ * from RC configuration.
+ *
+ * The urlpattern needs to be synced with "cache-uri" conf
+ */
+public class FileCacheServlet extends HttpServlet {
+
+    // Constants ----------------------------------------------------------------------------------
+    private static final int DEFAULT_BUFFER_SIZE = 10240; // ..bytes = 10KB.
+    private static final long DEFAULT_EXPIRE_TIME = 604800000L; // ..ms = 1 week.
+
+    /**
+     * Process HEAD request. This returns the same headers as GET request, but
+     * without content.
+     *
+     * @see HttpServlet#doHead(HttpServletRequest, HttpServletResponse).
+     */
+    protected void doHead(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Process request without content.
+        processRequest(request, response, false);
+    }
+
+    /**
+     * Process GET request.
+     *
+     * @see HttpServlet#doGet(HttpServletRequest, HttpServletResponse).
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Process request with content.
+        processRequest(request, response, true);
+    }
+
+    /**
+     * Process the actual request.
+     *
+     * @param request The request to be processed.
+     * @param response The response to be created.
+     * @param content Whether the request body should be written (GET) or not
+     * (HEAD).
+     * @throws IOException If something fails at I/O level.
+     */
+    private void processRequest(HttpServletRequest request, HttpServletResponse response, boolean content)
+            throws IOException {
+        // Validate the requested file ------------------------------------------------------------
+
+        // Get requested file by path info.
+        String requestedFile = request.getPathInfo();
+
+        // Check if file is actually supplied to the request URL.
+        if (requestedFile == null) {
+            // Do your thing if the file is not supplied to the request URL.
+            // Throw an exception, or send 404, or show default/warning page, or just ignore it.
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Configuration rootConfiguration = (Configuration) request.getServletContext().getAttribute(Configuration.class.getName());
+        Configuration runtimeConfiguration = rootConfiguration.subset("runtime-settings");
+
+        String basePath = runtimeConfiguration.getString("cache-path");
+        int bufferSize = runtimeConfiguration.getInteger("cache-buffer-size", DEFAULT_BUFFER_SIZE);
+        long expireTime = runtimeConfiguration.getLong("cache-expire-time", DEFAULT_EXPIRE_TIME);
+
+        // URL-decode the file name (might contain spaces and on) and prepare file object.
+        File file = new File(basePath, URLDecoder.decode(requestedFile, "UTF-8"));
+
+        // Check if file actually exists in filesystem.
+        if (!file.exists()) {
+            // Do your thing if the file appears to be non-existing.
+            // Throw an exception, or send 404, or show default/warning page, or just ignore it.
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // Prepare some variables. The ETag is an unique identifier of the file.
+        String fileName = file.getName();
+        long length = file.length();
+        long lastModified = file.lastModified();
+        String eTag = fileName + "_" + length + "_" + lastModified;
+        long expires = System.currentTimeMillis() + expireTime;
+
+        // Validate request headers for caching ---------------------------------------------------
+        // If-None-Match header should contain "*" or ETag. If so, then return 304.
+        String ifNoneMatch = request.getHeader("If-None-Match");
+        if (ifNoneMatch != null && matches(ifNoneMatch, eTag)) {
+            response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+            response.setHeader("ETag", eTag); // Required in 304.
+            response.setDateHeader("Expires", expires); // Postpone cache with 1 week.
+            return;
+        }
+
+        // If-Modified-Since header should be greater than LastModified. If so, then return 304.
+        // This header is ignored if any If-None-Match header is specified.
+        long ifModifiedSince = request.getDateHeader("If-Modified-Since");
+        if (ifNoneMatch == null && ifModifiedSince != -1 && ifModifiedSince + 1000 > lastModified) {
+            response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+            response.setHeader("ETag", eTag); // Required in 304.
+            response.setDateHeader("Expires", expires); // Postpone cache with 1 week.
+            return;
+        }
+
+        // Validate request headers for resume ----------------------------------------------------
+        // If-Match header should contain "*" or ETag. If not, then return 412.
+        String ifMatch = request.getHeader("If-Match");
+        if (ifMatch != null && !matches(ifMatch, eTag)) {
+            response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED);
+            return;
+        }
+
+        // If-Unmodified-Since header should be greater than LastModified. If not, then return 412.
+        long ifUnmodifiedSince = request.getDateHeader("If-Unmodified-Since");
+        if (ifUnmodifiedSince != -1 && ifUnmodifiedSince + 1000 <= lastModified) {
+            response.sendError(HttpServletResponse.SC_PRECONDITION_FAILED);
+            return;
+        }
+
+        // Prepare and initialize response --------------------------------------------------------
+        // Get content type by file name and set default GZIP support and content disposition.
+        String contentType = getServletContext().getMimeType(fileName);
+        String disposition = "inline";
+
+        // If content type is unknown, then set the default value.
+        // For all content types, see: http://www.w3schools.com/media/media_mimeref.asp
+        // To add new content types, add new mime-mapping entry in web.xml.
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        // If content type is text, then determine whether GZIP content encoding is supported by
+        // the browser and expand content type with the one and right character encoding.
+        if (contentType.startsWith("text")) {
+            contentType += ";charset=UTF-8";
+        } // Else, expect for images, determine content disposition. If content type is supported by
+        // the browser, then set to inline, else attachment which will pop a 'save as' dialogue.
+        else if (!contentType.startsWith("image")) {
+            String accept = request.getHeader("Accept");
+            disposition = accept != null && accepts(accept, contentType) ? "inline" : "attachment";
+        }
+
+        // Initialize response.
+        response.reset();
+        response.setBufferSize(bufferSize);
+        response.setHeader("Content-Disposition", disposition + ";filename=\"" + fileName + "\"");
+        response.setHeader("Accept-Ranges", "bytes");
+        response.setHeader("ETag", eTag);
+        response.setDateHeader("Last-Modified", lastModified);
+        response.setDateHeader("Expires", expires);
+
+        // Send requested file (part(s)) to client ------------------------------------------------
+        // Prepare streams.
+        FileInputStream input = null;
+        OutputStream output = null;
+
+        if (content) {
+            // Open streams.
+            input = new FileInputStream(file);
+            output = response.getOutputStream();
+            stream(input, output, bufferSize);
+        }
+
+    }
+
+    // Helpers (can be refactored to public utility class) ----------------------------------------
+    /**
+     * Returns true if the given accept header accepts the given value.
+     *
+     * @param acceptHeader The accept header.
+     * @param toAccept The value to be accepted.
+     * @return True if the given accept header accepts the given value.
+     */
+    private static boolean accepts(String acceptHeader, String toAccept) {
+        String[] acceptValues = acceptHeader.split("\\s*(,|;)\\s*");
+        Arrays.sort(acceptValues);
+        return Arrays.binarySearch(acceptValues, toAccept) > -1
+                || Arrays.binarySearch(acceptValues, toAccept.replaceAll("/.*$", "/*")) > -1
+                || Arrays.binarySearch(acceptValues, "*/*") > -1;
+    }
+
+    /**
+     * Returns true if the given match header matches the given value.
+     *
+     * @param matchHeader The match header.
+     * @param toMatch The value to be matched.
+     * @return True if the given match header matches the given value.
+     */
+    private static boolean matches(String matchHeader, String toMatch) {
+        String[] matchValues = matchHeader.split("\\s*,\\s*");
+        Arrays.sort(matchValues);
+        return Arrays.binarySearch(matchValues, toMatch) > -1
+                || Arrays.binarySearch(matchValues, "*") > -1;
+    }
+
+
+    /**
+     * Stream the given input to the given output via NIO {@link Channels} and a
+     * directly allocated NIO {@link ByteBuffer}. Both the input and output
+     * streams will implicitly be closed after streaming, regardless of whether
+     * an exception is been thrown or not.
+     *
+     * @param input The input stream.
+     * @param output The output stream.
+     * @param bufferSize
+     * @return The length of the written bytes.
+     * @throws IOException When an I/O error occurs.
+     */
+    public static long stream(InputStream input, OutputStream output, int bufferSize) throws IOException {
+        try (ReadableByteChannel inputChannel = Channels.newChannel(input);
+                WritableByteChannel outputChannel = Channels.newChannel(output)) {
+            ByteBuffer buffer = ByteBuffer.allocateDirect(bufferSize);
+            long size = 0;
+
+            while (inputChannel.read(buffer) != -1) {
+                buffer.flip();
+                size += outputChannel.write(buffer);
+                buffer.clear();
+            }
+
+            return size;
+        }
+    }
+
+}
