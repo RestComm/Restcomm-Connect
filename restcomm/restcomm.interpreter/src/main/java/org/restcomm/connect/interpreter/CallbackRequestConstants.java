ee565b4746c1483dbf4f04282c3017f8158b75d3
Jaime Casero <jaime.casero@telestax.com>
2018-06-14 15:45:00 +0200

    added SMSnotifier actor

---
+++ restcomm/restcomm.interpreter/src/main/java/org/restcomm/connect/interpreter/CallbackRequestConstants.java
@@ -0,0 +1,29 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2014, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+package org.restcomm.connect.interpreter;
+
+public class CallbackRequestConstants {
+
+    private static final String ACCOUT_SID_PARAM = "AccountSid";
+    private static final String FROM_PARAM = "From";
+    private static final String TO_PARAM = "To";
+    private static final String BODY_PARAM = "Body";
+    private static final String NUM_MEDIA_PARAM = "NumMedia";
+}
